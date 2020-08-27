4220b67c2645d25d77c0562fe90c553c9ff5a190
yaniryos <58386254+yaniryos@users.noreply.github.com>
2020-08-27 10:01:30 +0300

    fix(): add describe in validation test

--- client/src/Utils/Validations/Validator.test.ts
+++ client/src/Utils/Validations/Validator.test.ts
@@ -1,84 +1,86 @@
 import Validator from './Validator';
 
-describe('PhoneValidation Tests', () => {
+describe('Validation Tests', () => {
+    describe('PhoneValidation Tests', () => {
 
-    describe('CellPhone Numbers: ', () => {
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('050-1234561')).toBeTruthy();
+        describe('CellPhone Numbers: ', () => {
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('050-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('051-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('052-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('053-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('054-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('055-1234561')).toBeTruthy();
+            });
+
+            it('Should be true (wrong beginning)', () => {
+                expect(Validator.phoneValidation('056-1234561')).toBeFalsy();
+            });
+
+            it('Should be false (not long enough)', () => {
+                expect(Validator.phoneValidation('050-12561')).toBeFalsy();
+            });
+
+            it('Should be false (too long)', () => {
+                expect(Validator.phoneValidation('050-123456123')).toBeFalsy();
+            });
+
+            it('Should be false (no heifen)', () => {
+                expect(Validator.phoneValidation('0501234561')).toBeFalsy();
+            });
         });
 
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('051-1234561')).toBeTruthy();
-        });
+        describe('HomePhone Numbers: ', () => {
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('02-1234561')).toBeTruthy();
+            });
 
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('052-1234561')).toBeTruthy();
-        });
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('03-1234561')).toBeTruthy();
+            });
 
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('053-1234561')).toBeTruthy();
-        });
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('04-1234561')).toBeTruthy();
+            });
 
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('054-1234561')).toBeTruthy();
-        });
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('08-1234561')).toBeTruthy();
+            });
 
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('055-1234561')).toBeTruthy();
-        });
+            it('Should be true (correct start & correct number of digits)', () => {
+                expect(Validator.phoneValidation('09-1234561')).toBeTruthy();
+            });
 
-        it('Should be true (wrong beginning)', () => {
-            expect(Validator.phoneValidation('056-1234561')).toBeFalsy();
-        });
+            it('Should be false (not long enough)', () => {
+                expect(Validator.phoneValidation('02-12561')).toBeFalsy();
+            });
 
-        it('Should be false (not long enough)', () => {
-            expect(Validator.phoneValidation('050-12561')).toBeFalsy();
-        });
+            it('Should be false (too long)', () => {
+                expect(Validator.phoneValidation('03-123456123')).toBeFalsy();
+            });
 
-        it('Should be false (too long)', () => {
-            expect(Validator.phoneValidation('050-123456123')).toBeFalsy();
-        });
+            it('Should be false (wrong beginning)', () => {
+                expect(Validator.phoneValidation('07-1234561')).toBeFalsy();
+            });
 
-        it('Should be false (no heifen)', () => {
-            expect(Validator.phoneValidation('0501234561')).toBeFalsy();
-        });
-    });
-
-    describe('HomePhone Numbers: ', () => {
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('02-1234561')).toBeTruthy();
-        });
-
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('03-1234561')).toBeTruthy();
-        });
-
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('04-1234561')).toBeTruthy();
-        });
-
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('08-1234561')).toBeTruthy();
-        });
-
-        it('Should be true (correct start & correct number of digits)', () => {
-            expect(Validator.phoneValidation('09-1234561')).toBeTruthy();
-        });
-
-        it('Should be false (not long enough)', () => {
-            expect(Validator.phoneValidation('02-12561')).toBeFalsy();
-        });
-
-        it('Should be false (too long)', () => {
-            expect(Validator.phoneValidation('03-123456123')).toBeFalsy();
-        });
-
-        it('Should be false (wrong beginning)', () => {
-            expect(Validator.phoneValidation('07-1234561')).toBeFalsy();
-        });
-
-        it('Should be false (no heifen)', () => {
-            expect(Validator.phoneValidation('03123456123')).toBeFalsy();
+            it('Should be false (no heifen)', () => {
+                expect(Validator.phoneValidation('03123456123')).toBeFalsy();
+            });
         });
     });
 });
