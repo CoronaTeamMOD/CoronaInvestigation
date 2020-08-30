43a747701f4f2b7f74c78b43e4a5d1dc68725100
Emily Lipkin <emily7975@gmail.com>
2020-08-30 18:00:37 +0300

    feat(Add Redux): added redux (#34)
    * feat(Add Redux): added redux
    
    * feat(Add Redux): fix tab
    
    * feat(Add Redux): import order
    
    Co-authored-by: EmilyLip <mily7975@gmail.com>

---
+++ client/src/redux/User/userActionTypes.ts
@@ -0,0 +1,10 @@
+import User from 'models/User';
+
+export const SET_USER = 'SET_USER';
+
+interface SetUser {
+    type: typeof SET_USER,
+    payload: {user : User}
+}
+
+export type UserAction = SetUser;
