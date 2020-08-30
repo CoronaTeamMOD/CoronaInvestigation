88ab386c2c055e6ee9c2e2669b4056408818bfc8
MayaSasson <mayokit22@gmail.com>
2020-08-30 14:54:41 +0300

    feat(exit_investigation): added the button and swal

--- client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/useInvestigatedPersonInfo.ts
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/useInvestigatedPersonInfo.ts
@@ -1,28 +1,28 @@
 import Swal from 'sweetalert2';
+import theme from 'styles/theme';
 import { useHistory } from "react-router-dom";
 
-import theme from 'styles/theme';
 import {timeout} from 'Utils/Timeout/Timeout';
+
+import useStyles from './InvestigatedPersonInfoStyles';
 import {landingPageRoute} from 'Utils/Routes/Routes';
+import { InvestigatedPersonInfoOutcome } from './InvestigatedPersonInfoInterfaces';
 
-import useStyles from './InvestigationFormStyles';
-import { useInvestigationFormOutcome } from './InvestigationFormInterfaces';
-
-const useInvestigationForm = (): useInvestigationFormOutcome => {
+const useInvestigatedPersonInfo = (): InvestigatedPersonInfoOutcome => {
     let history = useHistory();
     const classes = useStyles({});
 
-    const confirmFinishInvestigation = () => {
+    const confirmExitUnfinishedInvestigation = () => {
         Swal.fire({
             icon: 'warning',
-            title: 'האם אתה בטוח שאתה רוצה לסיים ולשמור את החקירה?',
+            title: 'האם אתה בטוח שתרצה לצאת מהחקירה ולחזור אליה מאוחר יותר?',
             showCancelButton: true,
             cancelButtonText: 'בטל',
             cancelButtonColor: theme.palette.error.main,
             confirmButtonColor: theme.palette.primary.main,
             confirmButtonText: 'כן, המשך',
             customClass: {
-                title: classes.swalTitle
+                title: classes.swalTitle,
             }
           }).then((result) => {
             if (result.value) {
@@ -34,9 +34,9 @@ const useInvestigationForm = (): useInvestigationFormOutcome => {
     const handleInvestigationFinish = () => {
         Swal.fire({
             icon: 'success',
-            title: 'החקירה הסתיימה! הנך מועבר לעמוד הנחיתה',
+            title: 'בחרת לצאת מהחקירה לפני השלמתה! הנך מועבר לעמוד הנחיתה',
             customClass: {
-                title: classes.swalTitle
+                title: classes.swalTitle,
             },
             timer: 1750,
             showConfirmButton: false
@@ -47,8 +47,8 @@ const useInvestigationForm = (): useInvestigationFormOutcome => {
     };
 
     return {
-        confirmFinishInvestigation
+        confirmExitUnfinishedInvestigation
     }
 };
 
-export default useInvestigationForm;
+export default useInvestigatedPersonInfo;
