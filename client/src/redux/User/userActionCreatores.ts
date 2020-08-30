9666b73d499d23ac17a7aa6c1e00aff182aa9c5f
EmilyLip <mily7975@gmail.com>
2020-08-30 17:43:21 +0300

    feat(Add Redux): import order

--- client/src/redux/User/userActionCreatores.ts
+++ client/src/redux/User/userActionCreatores.ts
@@ -1,6 +1,7 @@
-import * as actionTypes from './userActionTypes';
-import {store} from '../store';
 import User from 'models/User';
+
+import {store} from '../store';
+import * as actionTypes from './userActionTypes';
 
 export const setUser = (user: User): void => {
     store.dispatch({
