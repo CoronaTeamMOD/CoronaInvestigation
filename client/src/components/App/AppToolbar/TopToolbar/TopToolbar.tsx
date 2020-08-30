719474ac22f729fb1c8972389e3b8e40a67847aa
Emily Lipkin <emily7975@gmail.com>
2020-08-30 18:00:37 +0300

    feat(Add Redux): added redux (#34)
    * feat(Add Redux): added redux
    
    * feat(Add Redux): fix tab
    
    * feat(Add Redux): import order
    
    Co-authored-by: EmilyLip <mily7975@gmail.com>

--- client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
+++ client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import { Toolbar } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 
 import useStyles from './TopToolbarStyles';
+import StoreStateType from 'redux/storeStateType';
 
-const TopToolbar: React.FC = (): JSX.Element => {
+const TopToolbar: React.FC<Props> = (props: Props): JSX.Element => {
     const classes = useStyles({});
   
     return (
@@ -13,9 +15,17 @@ const TopToolbar: React.FC = (): JSX.Element => {
                 <img alt='logo' className={classes.logo} src='./assets/img/logo.png'></img>
                 <Typography variant='h4'><div className={classes.systemName}>שם המערכת</div></Typography>
             </div>
-            <Typography>שלום, שם שלך</Typography>
+            <Typography>שלום, {props.userName}</Typography>
         </Toolbar>
     )
 }
 
-export default TopToolbar;
+const mapStateToProps = (state: StoreStateType) => ({
+    userName: state.user.name
+});
+
+interface Props {
+    userName: string
+}
+
+export default connect(mapStateToProps)(TopToolbar);
---
+++ client/src/redux/User/userActionCreatores.ts
@@ -0,0 +1,11 @@
+import User from 'models/User';
+
+import {store} from '../store';
+import * as actionTypes from './userActionTypes';
+
+export const setUser = (user: User): void => {
+    store.dispatch({
+        type: actionTypes.SET_USER,
+        payload: {user}
+    })
+}
