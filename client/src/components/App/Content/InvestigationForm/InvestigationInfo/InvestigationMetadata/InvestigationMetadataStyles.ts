37a5512d63bf5485a8decface80bdcd2b4430bea
sandymash <42100027+sandymash@users.noreply.github.com>
2020-08-30 11:07:00 +0300

    Feat/static info (#28)
    * feat(investigation info)
    - added static info bars
    - deleted unused create-react-app leftovers
    - added bold font weights
    - moved jss-rtl to App component
    - changed styling a bit to fit new bar
    - changed bg.colour to match mockup better

---
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigationMetadata/InvestigationMetadataStyles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    metadata: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        whiteSpace: 'pre',
+        padding: '1vh 0 1vh 0',
+        background: '#E5E5E5',
+    },
+});
+
+export default useStyles;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import rtl from 'jss-rtl';
+import {create} from 'jss';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 import {BrowserRouter} from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks';
+import {MuiThemeProvider, StylesProvider} from '@material-ui/core/styles';
+import {jssPreset} from '@material-ui/styles';
+import {ApolloProvider as ApolloHooksProvider} from '@apollo/react-hooks';
 
 import './index.css';
 import App from './components/App/App';
@@ -15,15 +18,19 @@ const client = new ApolloClient({
     uri: '/graphql'
 });
 
+const jss = create({plugins: [...jssPreset().plugins, rtl()]});
+
 ReactDOM.render(
     <MuiThemeProvider theme={theme}>
         <ApolloProvider client={client}>
             <ApolloHooksProvider client={client}>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
+                <StylesProvider jss={jss}>
+                    <BrowserRouter>
+                        <App/>
+                    </BrowserRouter>
+                </StylesProvider>
             </ApolloHooksProvider>
         </ApolloProvider>
     </MuiThemeProvider>
-,    document.getElementById('root') as HTMLElement
+    , document.getElementById('root') as HTMLElement
 );
