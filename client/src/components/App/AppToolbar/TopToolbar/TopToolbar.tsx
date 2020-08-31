ca49a0030561639473a7e4ebd75f97990c4c6f4b
Emily Lipkin <emily7975@gmail.com>
2020-08-31 14:43:11 +0300

    feat(Landing Page Table): Added UI table (#40)
    Co-authored-by: EmilyLip <mily7975@gmail.com>

--- client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
+++ client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import { Toolbar } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
+import {useSelector} from 'react-redux';
+import {Toolbar} from '@material-ui/core';
+import {Typography} from '@material-ui/core';
 
 import useStyles from './TopToolbarStyles';
 import StoreStateType from 'redux/storeStateType';
 
-const TopToolbar: React.FC<Props> = (props: Props): JSX.Element => {
+const TopToolbar: React.FC = (): JSX.Element => {
     const classes = useStyles({});
+    const userName = useSelector<StoreStateType, string>(state => state.user.name);
   
     return (
         <Toolbar className={classes.toolbar}>
@@ -15,17 +16,9 @@ const TopToolbar: React.FC<Props> = (props: Props): JSX.Element => {
                 <img alt='logo' className={classes.logo} src='./assets/img/logo.png'></img>
                 <Typography variant='h4'><div className={classes.systemName}>שם המערכת</div></Typography>
             </div>
-            <Typography>שלום, {props.userName}</Typography>
+            <Typography>שלום, {userName}</Typography>
         </Toolbar>
     )
 }
 
-const mapStateToProps = (state: StoreStateType) => ({
-    userName: state.user.name
-});
-
-interface Props {
-    userName: string
-}
-
-export default connect(mapStateToProps)(TopToolbar);
+export default TopToolbar;
