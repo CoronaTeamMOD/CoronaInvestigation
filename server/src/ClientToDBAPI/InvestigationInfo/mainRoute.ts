eaf8d6b68d402e9846113322880ed260c601d4bf
Sigwoman <edensigman@gmail.com>
2020-09-01 11:35:03 +0300

    fix(): remove console log

--- server/src/ClientToDBAPI/InvestigationInfo/mainRoute.ts
+++ server/src/ClientToDBAPI/InvestigationInfo/mainRoute.ts
@@ -6,7 +6,6 @@ import { GET_INVESTIGATION_INFO } from '../../DBService/InvestigationInfo/Query'
 const investigationInfo = Router();
 
 investigationInfo.post('/staticInfo', (request: Request, response: Response) => {
-    console.log(request.body)
     graphqlRequest(GET_INVESTIGATION_INFO, { id: request.body.id })
     .then((result: any) => response.send(result));
 })
