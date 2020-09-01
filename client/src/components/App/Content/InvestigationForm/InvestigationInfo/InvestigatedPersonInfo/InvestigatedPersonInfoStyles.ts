cf862fdeffe47d684a95578767e08d2c373ee8ec
sandymash <42100027+sandymash@users.noreply.github.com>
2020-09-01 15:28:42 +0300

    fix(static info bar) (#43)
    * fix(static info bar)
    - fixed look on ipad
    - divided button and checkbox into different divs
    
    Co-authored-by: sandy <>

--- client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/InvestigatedPersonInfoStyles.ts
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/InvestigatedPersonInfoStyles.ts
@@ -5,6 +5,12 @@ const useStyles = makeStyles({
         padding: '0.7vw',
         borderRadius: 0
     },
+    headerTopPart:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        paddingLeft: '3vh',
+        flip:false,
+    },
     investigationHeaderInfo: {
         display: 'flex',
         alignItems: 'center',
@@ -16,10 +22,7 @@ const useStyles = makeStyles({
     },
     informationBar: {
         display: 'flex',
-        flexWrap: 'wrap',
-        whiteSpace: 'pre',
         alignItems: 'center',
-        paddingTop: '1vh',
         justifyContent: 'space-between'
     },
     divider: {
@@ -28,13 +31,14 @@ const useStyles = makeStyles({
     },
     additionalInfo: {
         display: 'flex',
+        flexWrap: 'wrap',
+        whiteSpace: 'pre',
     },
     exitInvestigationButton: {
-        marginTop: '1vh',
         color: 'white',
         borderRadius: '10vw',
         height: '4vh',
-        width: '7vw',
+        width: '10vw',
     }, 
     managementControllers: {
         display: 'flex',
@@ -45,9 +49,6 @@ const useStyles = makeStyles({
         fontSize: '1.5vw',
         fontFamily: 'Assistant',
     },
-    checkbox: {
-        marginTop: '0.25vh',
-    }
 });
 
 export default useStyles;
