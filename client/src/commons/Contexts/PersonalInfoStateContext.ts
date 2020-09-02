0aaf5424e0052cc8be01217e8c533a494453f9dd
shiran eyal <shiran.eyal@gmail.com>
2020-09-02 14:04:24 +0300

    pause

--- client/src/commons/Contexts/PersonalInfoStateContext.ts
+++ client/src/commons/Contexts/PersonalInfoStateContext.ts
@@ -1,3 +1,37 @@
 import {createContext, useContext} from 'react';
+
+import Gender from 'models/enums/Gender';
+import identificationType from 'models/enums/IdentificationTypes';
+import RelevantOccupations from 'models/enums/RelevantOccupations';
 import {personalInfoContextData} from 'models/Contexts/personalInfoContextData'
 
+const initialPersonalInfoContext: personalInfoContextData = {
+    phoneNumber: '',
+    isInvestigatedPersonsNumber: true,
+    selectReasonNumberIsNotRelated: '',
+    writeReasonNumberIsNotRelated: '',
+    additionalPhoneNumber: '',
+    gender: Gender.MALE,
+    identificationType: identificationType.ID,
+    identificationNumber: '',
+    age: -1,
+    motherName: '',
+    fatherName: '',
+    insuranceCompany: '',
+    HMO: '',
+    adress: {
+        city: '',
+        neighborhood: '',
+        street: '',
+        houseNumber: -1,
+        entrance: '',
+        floor: -1
+    },
+    relevantOccupation: RelevantOccupations.MOH_Worker,
+    institutionName: ''
+} 
+
+export const personalInfoContext = createContext<personalInfoContextData>(initialPersonalInfoContext);
+
+export const personalInfoContextConsumer = personalInfoContext.Consumer;
+export const personalInfoContextProvider = personalInfoContext.Provider;
