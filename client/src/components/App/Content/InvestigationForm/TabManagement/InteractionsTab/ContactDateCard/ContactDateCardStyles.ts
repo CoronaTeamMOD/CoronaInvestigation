c4969b382899f83717584bbbdc7e22607fbfdf74
Roei Rom <52863381+RoeiRom@users.noreply.github.com>
2020-09-06 12:14:16 +0300

    feat(InteractionsTab): add the Interactions display (ONLY UI) (#64)

---
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/ContactDateCard/ContactDateCardStyles.ts
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    investigatedDateCard: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        backgroundColor: 'lightgray',
+        margin: '1vh 0',
+        padding: '1vh 1vw'
+    },
+    dateInfo: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    dateSection: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    infoSection: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    createContactButton: {
+        marginRight: 'auto'
+    }
+});
+
+export default useStyles;
