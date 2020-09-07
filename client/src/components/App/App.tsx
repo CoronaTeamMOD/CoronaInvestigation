9ceb0c624aa678d624fa6cfa5b0c37d3aa04a5c3
Roei Rom <52863381+RoeiRom@users.noreply.github.com>
2020-09-07 17:29:23 +0300

    feat(authentication): add client side authentication (#72)

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,52 @@
+import axios from 'axios'; 
 import React from 'react';
+import { config } from 'dotenv';
 
-import AppToolbar from './AppToolbar/AppToolbar';
+import { Environment } from 'models/enums/Environments';
+import { setUser } from 'redux/User/userActionCreatores';
+
 import Content from './Content/Content';
+import AppToolbar from './AppToolbar/AppToolbar';
+
+config();
+
+type AuthenticationReturn = [{
+    access_token: string;
+    expires_on: Date;
+    id_token: string;
+    provider_name: string;
+    refresh_token: string;
+    user_clamis: [{
+        typ: string;
+        val: string;
+    }];
+    user_id: string;
+}];
+
+const userNameClaimType = 'name';
 
 const App: React.FC = (): JSX.Element => {
+    
+    React.useEffect(() => {
+        if (process.env.REACT_APP_ENVIRONMENT === Environment.PROD) {
+            axios.get<AuthenticationReturn>(`${process.env.REACT_APP_CLIENT_URL}/.auth/me`)
+            .then((response) => {
+                const { data } = response;
+                setUser({
+                    id: data[0].user_id,
+                    name: data[0].user_clamis.find(claim => claim.typ === userNameClaimType)?.val as string,
+                    token: data[0].id_token
+                });
+            })
+        } else {
+            setUser({
+                id: '7',
+                name: 'חוקר פיקטיבי',
+                token: 'fake token!'
+            });
+        }
+    }, [])
+
     return (
         <>
             <AppToolbar/>
