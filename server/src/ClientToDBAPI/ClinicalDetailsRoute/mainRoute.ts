cc7f7cf9129374fb54ce940eebefd03ef14abeba
Sigwoman <60707882+Sigwoman@users.noreply.github.com>
2020-09-07 18:22:35 +0300

    feat(): clinical details db acsses (#58)
    * feat(clinical details db): add connection
    
    * fix(): mr fixes

--- server/src/ClientToDBAPI/ClinicalDetailsRoute/mainRoute.ts
+++ server/src/ClientToDBAPI/ClinicalDetailsRoute/mainRoute.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from 'express';
 
 import { graphqlRequest } from '../../GraphqlHTTPRequest';
+import { CreateAddressResposne } from '../../Models/ClinicalDetails/CreateAddress';
 import { GET_SYMPTOMS, GET_BACKGROUND_DISEASES } from '../../DBService/ClinicalDetails/Query';
+import { CREATE_ADDRESS, CREATE_INVESTIGATION, ADD_BACKGROUND_DESEASES, ADD_SYMPTOMS } from '../../DBService/ClinicalDetails/Mutation';
 
 const clinicalDetailsRoute = Router();
 
@@ -15,6 +17,34 @@ clinicalDetailsRoute.post('/symptoms', (request: Request, response: Response) =>
 
 clinicalDetailsRoute.post('/backgroundDiseases', (request: Request, response: Response) => {
     graphqlRequest(GET_BACKGROUND_DISEASES).then((result: any) => response.send(result));
+});
+
+clinicalDetailsRoute.post('/saveClinicalDetails', (request: Request, response: Response) => {
+    
+    const requestAddress = request.body.input;
+
+    const requestInvestigation = request.body.investigation;
+    
+    graphqlRequest(CREATE_ADDRESS, { input: {
+        ...requestAddress
+    }}).then((result: CreateAddressResposne) => {
+        graphqlRequest(CREATE_INVESTIGATION, { investigation: {
+            ...requestInvestigation,
+            isolationAddress: result.data.insertAndGetAddressId.integer,
+        }}).then(() => {
+            graphqlRequest(ADD_BACKGROUND_DESEASES, {
+                backgroundDeseases: requestInvestigation.backgroundIllnesses,
+                investigatedPatientId: requestInvestigation.investigatedPatientId
+            }).then(() => {
+                graphqlRequest(ADD_SYMPTOMS, {
+                    investigationIdValue: requestInvestigation.epidemioligyNumber,
+                    symptomNames: requestInvestigation.symptoms
+                }).then(() => {
+                    response.send('Added clinical details');
+                });
+            });
+        });
+    });
 });
 
 export default clinicalDetailsRoute;
