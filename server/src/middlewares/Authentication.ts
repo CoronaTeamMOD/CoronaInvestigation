558ffaa080603e7a936f3dee54c9d253985018ce
sandymash <42100027+sandymash@users.noreply.github.com>
2020-09-09 00:06:34 +0300

    added future code in comment
    added code in comment for when debug gets resolved

--- server/src/middlewares/Authentication.ts
+++ server/src/middlewares/Authentication.ts
@@ -6,7 +6,7 @@ const authMiddleware = (request:Request, response:Response, next: NextFunction) 
 
     if(!token)
         return response.status(401).json({error: 'unauthorized'});
-   
+
     if (!(process.env.ENVIRONMENT === 'prod')) {
         switch (token) {
             case 'fake token!':
@@ -20,15 +20,19 @@ const authMiddleware = (request:Request, response:Response, next: NextFunction) 
         const claimRoute = '/.auth/me';
         console.log('request cookies:', request.cookies);
         const headers = {Cookie: request.cookies};
-        axios.get(request.baseUrl + claimRoute, {headers})
+        
+        return axios.get(request.baseUrl + claimRoute, {headers})
             .then(result => {
                 const {data} = result;
                 console.log('auth/me result', data);
                 Array.isArray(data) && console.log(data[0], data[0]?.user_id, data[0]?.user_claims);
+                //  const id = data[0].user_id;
+                //  const name = data[0].user_clamis.find(claim => claim.typ === userNameClaimType)?.val as string;
+                //  response.locals.user = {id: , name };
+                //  return next();
             })
-            .catch(e => console.error(e));
+            .catch(e => console.error(e)); // return response.status(403).json({error: 'forrbidden'});
 
-        next();
     }
 };
 
