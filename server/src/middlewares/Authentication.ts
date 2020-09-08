1f5b015110b234fb44076689a51529bcf2a006f6
sandymash <42100027+sandymash@users.noreply.github.com>
2020-09-09 00:01:32 +0300

    fix(auth middleware) added stub middleware for dev

--- server/src/middlewares/Authentication.ts
+++ server/src/middlewares/Authentication.ts
@@ -6,19 +6,30 @@ const authMiddleware = (request:Request, response:Response, next: NextFunction) 
 
     if(!token)
         return response.status(401).json({error: 'unauthorized'});
+   
+    if (!(process.env.ENVIRONMENT === 'prod')) {
+        switch (token) {
+            case 'fake token!':
+                response.locals.user = {id: '7', name: 'חוקר פיקטיבי'};
+                next();
+                break;
+            default:
+                return response.status(403).json({error: 'forrbidden'});
+        }
+    } else {
+        const claimRoute = '/.auth/me';
+        console.log('request cookies:', request.cookies);
+        const headers = {Cookie: request.cookies};
+        axios.get(request.baseUrl + claimRoute, {headers})
+            .then(result => {
+                const {data} = result;
+                console.log('auth/me result', data);
+                Array.isArray(data) && console.log(data[0], data[0]?.user_id, data[0]?.user_claims);
+            })
+            .catch(e => console.error(e));
 
-    const claimRoute = '/.auth/me';
-    console.log('request cookies:', request.cookies);
-    const headers = {Cookie: request.cookies};
-    axios.get(request.baseUrl + claimRoute, {headers})
-        .then(result => {
-            const {data} = result;
-            console.log('auth/me result', data);
-            Array.isArray(data) && console.log(data[0], data[0]?.user_id, data[0]?.user_claims);
-        })
-        .catch(e => console.error(e));
-
-    next();
+        next();
+    }
 };
 
 export default authMiddleware;
