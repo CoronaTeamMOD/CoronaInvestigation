cf6436d969ebe50736204053da571ae61f927172
Roy Maniv <roymaniv@gmail.com>
2020-09-08 16:19:55 +0300

    fix(Queries): fix names

--- server/src/DBService/InvestigationInfo/Mutation.ts
+++ server/src/DBService/InvestigationInfo/Mutation.ts
@@ -2,7 +2,7 @@ import { gql } from "postgraphile";
 
 export const UPDATE_INVESTIGATION_STATUS = gql`
   mutation UpdateInvestigationStatus($investigationStatus: String!, $epidemiologyNumber: Int!) {
-    updateInvestigationByEpidemiologyNumber(input: {investigationPatch: {investigationStatus: $investigationStatus}, epidemioligyNumber: $epidemiologyNumber}) {
+    updateInvestigationByEpidemiologyNumber(input: {investigationPatch: {investigationStatus: $investigationStatus}, epidemiologyNumber: $epidemiologyNumber}) {
       clientMutationId
     }
   }
