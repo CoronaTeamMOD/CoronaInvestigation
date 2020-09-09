a94c6edf07d6d23b9ab71071ebc6a1e99ca2b0dc
roymaniv <62545113+roymaniv@users.noreply.github.com>
2020-09-09 17:04:01 +0300

    feat(ClinicalDetails): show is pregnant toggle only if the investigated patient is a female (#89)

---
+++ client/src/redux/Gender/GenderActionTypes.ts
@@ -0,0 +1,8 @@
+export const SET_GENDER = 'SET_GENDER';
+
+interface SetGender {
+    type: typeof SET_GENDER,
+    payload: {gender : string}
+}
+
+export type GenderAction = SetGender;
--- client/src/redux/Investigation/investigationActionCreators.ts
+++ client/src/redux/Investigation/investigationActionCreators.ts
@@ -6,11 +6,32 @@ export const setEpidemiologyNum = (epidemiologyNumber : number): void => {
         type: actionTypes.SET_EPIDEMIOLOGY_NUM,
         payload: {epidemiologyNumber}
     })
-}
+};
 
 export const setCantReachInvestigated = (cantReachInvestigated : boolean): void => {
     store.dispatch({
         type: actionTypes.SET_CANT_REACH_INVESTIGATED,
         payload: {cantReachInvestigated}
     })
-}
+};
+
+export const setInvestigatedPatientId = (investigatedPatientId: number) => {
+    store.dispatch({
+        type: actionTypes.SET_INVESTIGATED_PATIENT_ID,
+        payload: {investigatedPatientId}
+    })
+};
+
+export const setCreator = (creator: string) => {
+    store.dispatch({
+        type: actionTypes.SET_CREATOR,
+        payload: {creator}
+    })
+};
+
+export const SetLastUpdator = (lastUpdator: string) => {
+    store.dispatch({
+        type: actionTypes.SET_LAST_UPDATOR,
+        payload: {lastUpdator}
+    })
+};
