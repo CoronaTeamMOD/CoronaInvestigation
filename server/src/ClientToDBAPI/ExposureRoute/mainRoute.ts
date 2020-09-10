2b4d1babdfd8fc64271961cb6880aa0d1aa0ca4a
Elad Yashar <eladyashar1104@gmail.com@gmail.com>
2020-09-10 18:40:49 +0300

    fix(exposure-db-conection): create api for reading exposure detailes

--- server/src/ClientToDBAPI/ExposureRoute/mainRoute.ts
+++ server/src/ClientToDBAPI/ExposureRoute/mainRoute.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express';
+import { graphqlRequest } from '../../GraphqlHTTPRequest';
+import { GET_EXPOSURE_INFO } from '../../DBService/Exposure/Query';
 
-const exosureRoute = Router();
+const exposureRoute = Router();
 
-exosureRoute.get('/', (request: Request, response: Response) => {
-    response.send('Hello from Exposure route');
+exposureRoute.get('/:investigationId', (request: Request, response: Response) => {
+    graphqlRequest(GET_EXPOSURE_INFO, {investigationId : parseInt(request.params.investigationId)})
+    .then((result: any) => response.send(result));
 })
 
-export default exosureRoute;
+export default exposureRoute;
