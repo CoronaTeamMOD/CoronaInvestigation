b157b5457251ea17d0c2191922047f61206ccfeb
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-09-13 13:45:16 +0300

    Loading Contacts and events from DB
    Added get query for events and contacts by investigation id

--- server/src/DBService/ContactEvent/Query.ts
+++ server/src/DBService/ContactEvent/Query.ts
@@ -1,7 +1,7 @@
 import { gql } from 'postgraphile';
 
 export const GET_LOACTIONS_SUB_TYPES_BY_TYPES = gql`
-query getLocationsSubTypesByTypes {
+query getPlacesSubTypesByTypes {
     allPlaceTypes {
       nodes {
         displayName
@@ -14,4 +14,58 @@ query getLocationsSubTypesByTypes {
       }
     }
   }
-`;  
+`;
+
+export const GET_FULL_CONTACT_EVENT_BY_INVESTIGATION_ID = gql`
+query getEventAndPeopleByInvestigationID($currInvestigation: Int!) {
+  allContactEvents(condition: {investigationId: $currInvestigation}) {
+    nodes {
+      airline
+      allowsHamagenData
+      boardingStation
+      busCompany
+      cityDestination
+      cityOrigin
+      busLine
+      contactPersonFirstName
+      contactPersonLastName
+      contactPersonPhoneNumber
+      contactPhoneNumber
+      endStation
+      endTime
+      externalizationApproval
+      flightDestinationAirport
+      flightDestinationCity
+      flightDestinationCountry
+      flightNum
+      flightOriginAirport
+      flightOriginCity
+      flightOriginCountry
+      grade
+      investigationId
+      isolationStartDate
+      locationAddress
+      numberOfContacted
+      placeName
+      placeSubType
+      placeType
+      startTime
+      trainLine
+      contactedPeopleByContactEvent {
+        nodes {
+          contactEvent
+          doesNeedIsolation
+          extraInfo
+          personByPersonInfo {
+            firstName
+            identificationNumber
+            lastName
+            gender
+            phoneNumber
+          }
+        }
+      }
+    }
+  }
+}
+`;
--- server/src/Models/ContactEvent/GetPlacesSubTypesByTypes.ts
+++ server/src/Models/ContactEvent/GetPlacesSubTypesByTypes.ts
@@ -1,3 +1,4 @@
+
 export interface GetPlaceSubTypesByTypesResposne {
     data: {
         allPlaceTypes: {
