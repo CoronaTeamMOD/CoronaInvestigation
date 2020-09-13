6f041f3c419ded6e53f685d0220c4e7aeb1b29bd
eladyashar <eladyashar1104@gmail.com>
2020-09-13 12:42:42 +0300

    Fix/exposure db conection (#124)
    * fix(exposure-db-conection):
    create api for reading exposure detailes
    
    * fix(exposure-db-connection): change query name
    
    * fix(exposure-db-connection): add id to response
    
    * fix(exposure-db-connection): add route for creat or update exposure
    
    * fix(exposure-db-connection): add placesubtype
    
    * fix(exposure-db-connection): change field on get expouse info query
    
    * fix(exposure-db-conection):
    create api for reading exposure detailes
    
    * fix condition for update or create exposure
    
    * split update and create to seprate routes
    
    * change update route from post to put
    
    Co-authored-by: Elad Yashar <eladyashar1104@gmail.com@gmail.com>

--- server/src/ClientToDBAPI/ExposureRoute/mainRoute.ts
+++ server/src/ClientToDBAPI/ExposureRoute/mainRoute.ts
@@ -1,9 +1,23 @@
 import { Router, Request, Response } from 'express';
+import { graphqlRequest } from '../../GraphqlHTTPRequest';
+import { GET_EXPOSURE_INFO } from '../../DBService/Exposure/Query';
+import { UPDATE_EXPOSURE, CREATE_EXPOSURE } from '../../DBService/Exposure/Mutation';
 
-const exosureRoute = Router();
+const exposureRoute = Router();
 
-exosureRoute.get('/', (request: Request, response: Response) => {
-    response.send('Hello from Exposure route');
+exposureRoute.get('/:investigationId', (request: Request, response: Response) => {
+    graphqlRequest(GET_EXPOSURE_INFO, request.headers, {investigationId : parseInt(request.params.investigationId)})
+    .then((result: any) => response.send(result));
 })
 
-export default exosureRoute;
+exposureRoute.post('/', (request: Request, response: Response) => {
+        graphqlRequest(CREATE_EXPOSURE, request.headers, { data: request.body.data })
+        .then((result: any) => response.send(result));
+})
+
+exposureRoute.put('/', (request: Request, response: Response) => {
+        graphqlRequest(UPDATE_EXPOSURE, request.headers, {exposureId : parseInt(request.body.exposureId), data: request.body.data})
+        .then((result: any) => response.send(result));
+})
+
+export default exposureRoute;
