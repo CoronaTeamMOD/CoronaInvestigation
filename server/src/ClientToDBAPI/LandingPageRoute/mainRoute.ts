890f8e04ba85b6a7152d36e916531fe0cee680bf
Daniel <dani.aizenberg47@gmail.com>
2020-09-14 11:17:12 +0300

    fix(): cr changes

--- server/src/ClientToDBAPI/LandingPageRoute/mainRoute.ts
+++ server/src/ClientToDBAPI/LandingPageRoute/mainRoute.ts
@@ -10,10 +10,7 @@ landingPageRoute.get('/', (request: Request, response: Response) => {
 
 landingPageRoute.post('/investigations', (request: Request, response: Response) => {
     graphqlRequest(GET_USER_INVESTIGATIONS, response.locals, { userName: response.locals.user.name })
-    .then((result: any) => {
-        console.log(result);
-        response.send(result)
-    })
+    .then((result: any) => response.send(result))
     .catch(err => response.status(500).send('error in fetching data: ' + err));
 })
 
