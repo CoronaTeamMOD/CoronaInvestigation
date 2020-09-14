40e28baf0476115d41f51978bc0c674fdf0702cf
ItayBenMoshe <71003295+ItayBenMoshe@users.noreply.github.com>
2020-09-14 14:56:36 +0300

    feat(exposuresAndFlights): done (#161)
    * feat all cahgnes  from our
    exposureAndFlights branch
    
    * fix(InteractionsTab.tsx): investigation dates are now loaded (#160)
    
    * feat all cahgnes  from our
    exposureAndFlights branch

--- client/src/components/App/Content/InvestigationForm/InvestigationForm.tsx
+++ client/src/components/App/Content/InvestigationForm/InvestigationForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
 import StoreStateType from 'redux/storeStateType';
+
 import PrimaryButton from 'commons/Buttons/PrimaryButton/PrimaryButton';
 import ClinicalDetailsData from 'models/Contexts/ClinicalDetailsContextData';
 import { personalInfoContextData } from 'models/Contexts/personalInfoContextData';
@@ -12,12 +12,15 @@ import {
     PersonalInfoDataAndSet
 } from 'commons/Contexts/PersonalInfoStateContext';
 import { ClinicalDetailsDataContextProvider, ClinicalDetailsDataAndSet, initialClinicalDetails } from 'commons/Contexts/ClinicalDetailsContext';
+import {ExposureAndFlightsContextProvider, ExposureAndFlightsDetails,
+        initialExposuresAndFlightsData, ExposureAndFlightsDetailsAndSet} from 'commons/Contexts/ExposuresAndFlights';
 
 import useStyles from './InvestigationFormStyles';
 import useInvestigationForm from './useInvestigationForm';
 import TabManagement from './TabManagement/TabManagement';
 import InvestigationInfoBar from './InvestigationInfo/InvestigationInfoBar';
 import { StartInvestigationDateVariablesProvider } from './StartInvestiationDateVariables/StartInvestigationDateVariables';
+
 
 export const LAST_TAB_ID = 3;
 const END_INVESTIGATION = 'סיים חקירה';
@@ -34,11 +37,13 @@ const InvestigationForm: React.FC = (): JSX.Element => {
         () => ({
             personalInfoData,
             setPersonalInfoData
+
         }),
         [personalInfoData, setPersonalInfoData]
     );
 
     const [exposureDate, setExposureDate] = React.useState<Date>();
+    const [exposureAndFlightsData, setExposureDataAndFlights] = React.useState<ExposureAndFlightsDetails>(initialExposuresAndFlightsData)
     const [symptomsStartDate, setSymptomsStartDate] = React.useState<Date>();
     const [hasSymptoms, setHasSymptoms] = React.useState<boolean>(false);
     const [endInvestigationDate, setEndInvestigationDate] = React.useState<Date>(new Date());
@@ -49,6 +54,13 @@ const InvestigationForm: React.FC = (): JSX.Element => {
         setClinicalDetailsData
     }),
         [clinicalDetailsData, setClinicalDetailsData]
+    );
+
+    const exposuresAndFlightsVariables: ExposureAndFlightsDetailsAndSet = React.useMemo(() => ({
+        exposureAndFlightsData,
+        setExposureDataAndFlights
+    }),
+        [exposureAndFlightsData, setExposureDataAndFlights]
     );
 
     const startInvestigationDateVariables: StartInvestigationDateVariables = React.useMemo(() => ({
@@ -65,34 +77,36 @@ const InvestigationForm: React.FC = (): JSX.Element => {
             setSymptomsStartDate, setExposureDate, setHasSymptoms, setEndInvestigationDate]
     );
 
-    const { currentTab, setCurrentTab, confirmFinishInvestigation, handleSwitchTab, saveCurrentTab } = useInvestigationForm({ clinicalDetailsVariables, personalInfoData });
+    const { currentTab, setCurrentTab, confirmFinishInvestigation, handleSwitchTab, saveCurrentTab } = useInvestigationForm({ clinicalDetailsVariables, personalInfoData, exposuresAndFlightsVariables });
 
     return (
         <div className={classes.content}>
-            <PersonalInfoContextProvider value={personalInfoValue}>
-                <ClinicalDetailsDataContextProvider value={clinicalDetailsVariables}>
-                    <StartInvestigationDateVariablesProvider value={startInvestigationDateVariables}>
-                        <InvestigationInfoBar
-                            onExitInvestigation={saveCurrentTab}
-                        />
-                        <div className={classes.interactiveForm}>
-                            <TabManagement
-                                currentTab={currentTab}
-                                setCurrentTab={setCurrentTab}
-                                onTabClicked={saveCurrentTab}
+            <ExposureAndFlightsContextProvider value={exposuresAndFlightsVariables}>
+                <PersonalInfoContextProvider value={personalInfoValue}>
+                    <ClinicalDetailsDataContextProvider value={clinicalDetailsVariables}>
+                        <StartInvestigationDateVariablesProvider value={startInvestigationDateVariables}>
+                            <InvestigationInfoBar
+                                onExitInvestigation={saveCurrentTab}
                             />
-                            <div className={classes.buttonSection}>
-                                <PrimaryButton test-id={currentTab.id === LAST_TAB_ID ? 'endInvestigation' : 'continueToNextStage'}
-                                    onClick={() => {
-                                        currentTab.id === LAST_TAB_ID ? confirmFinishInvestigation(epidemiologyNumber) : handleSwitchTab();
-                                    }}>
-                                    {currentTab.id === LAST_TAB_ID ? END_INVESTIGATION : CONTINUE_TO_NEXT_TAB}
-                                </PrimaryButton>
-                            </div>
-                        </div>
-                    </StartInvestigationDateVariablesProvider>
-                </ClinicalDetailsDataContextProvider>
-            </PersonalInfoContextProvider>
+                                <div className={classes.interactiveForm}>
+                                    <TabManagement
+                                        currentTab={currentTab}
+                                        setCurrentTab={setCurrentTab}
+                                        onTabClicked={saveCurrentTab}
+                                    />
+                                    <div className={classes.buttonSection}>
+                                        <PrimaryButton test-id={currentTab.id === LAST_TAB_ID ? 'endInvestigation' : 'continueToNextStage'}
+                                            onClick={() => {
+                                                currentTab.id === LAST_TAB_ID ? confirmFinishInvestigation(epidemiologyNumber) : handleSwitchTab();
+                                            }}>
+                                            {currentTab.id === LAST_TAB_ID ? END_INVESTIGATION : CONTINUE_TO_NEXT_TAB}
+                                        </PrimaryButton>
+                                    </div>
+                                </div>
+                            </StartInvestigationDateVariablesProvider>
+                        </ClinicalDetailsDataContextProvider>
+                    </PersonalInfoContextProvider>
+            </ExposureAndFlightsContextProvider>
         </div>
     )
 }
--- client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/FlightsForm/FlightsForm.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/FlightsForm/FlightsForm.tsx
@@ -1,58 +1,106 @@
-import React, {useContext} from 'react';
-import {Grid, Typography} from '@material-ui/core';
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+
 import FormRowWithInput from 'commons/FormRowWithInput/FormRowWithInput';
 import DatePick from 'commons/DatePick/DatePick';
 import CircleTextField from 'commons/CircleTextField/CircleTextField';
-import AirportInput from './AirportInput/AirportInput';
 import useFormStyles from 'styles/formStyles';
-import {exposuresContext} from "commons/Contexts/ExposuresAndFlights";
-import {format} from "date-fns";
 
-const FlightsForm = () => {
-    const classes = useFormStyles();
-    const {exposureData, setExposureData} = useContext(exposuresContext);
-    const {fromAirport, toAirport, departureDate, arrivalDate, airline,flightNum} = exposureData;
-    const {fromAirport: setFromAirport, toAirport: setToAirport, departureDate: setDepartureDate,
-        arrivalDate: setArrivalDate, airline: setAirline, flightNum:setFlightNumber} = setExposureData;
+import AirportInput from './AirportInput/AirportInput';
 
-    const handleFlightNumberInput = (event: React.ChangeEvent<HTMLInputElement>) => setFlightNumber(event.target.value);
-    const handleAirlineInput = (event: React.ChangeEvent<HTMLInputElement>) => setAirline(event.target.value);
-    const handleStartDateInput = (event: React.ChangeEvent<HTMLInputElement>) => setDepartureDate(new Date(event.target.value));
-    const handleEndDateInput = (event: React.ChangeEvent<HTMLInputElement>) => setArrivalDate(new Date(event.target.value));
+const FlightsForm = (props: any) => {
+  const {
+    exposureAndFlightsData,
+    fieldsNames,
+    handleChangeExposureDataAndFlightsField,
+  } = props;
 
-    const dateFormat = 'yyyy-MM-dd';
-    const formattedDate = (date: Date | undefined) => date ? format(date, dateFormat) : dateFormat;
+  const classes = useFormStyles();
 
-    return (
-        <Grid className={classes.form} container justify='flex-start'>
-            <FormRowWithInput fieldName='טיסה הלוך:' testId='fromFlight'>
-                <AirportInput airport={fromAirport} setAirport={setFromAirport}/>
-            </FormRowWithInput>
+  return (
+    <Grid className={classes.form} container justify="flex-start">
+      <FormRowWithInput fieldName="יעד:">
+        <AirportInput
+          country={exposureAndFlightsData[fieldsNames.destinationCountry]}
+          countryFieldName={fieldsNames.destinationCountry}
+          city={exposureAndFlightsData[fieldsNames.destinationCity]}
+          cityFieldName={fieldsNames.destinationCity}
+          airport={exposureAndFlightsData[fieldsNames.destinationAirport]}
+          airportFieldName={fieldsNames.destinationAirport}
+          handleChangeExposureDataAndFlightsField={
+            handleChangeExposureDataAndFlightsField
+          }
+        />
+      </FormRowWithInput>
 
-            <FormRowWithInput fieldName='טיסה חזור:' testId='toFlight'>
-                <AirportInput airport={toAirport} setAirport={setToAirport}/>
-            </FormRowWithInput>
+      <FormRowWithInput fieldName="מוצא:">
+        <AirportInput
+          country={exposureAndFlightsData[fieldsNames.originCountry]}
+          countryFieldName={fieldsNames.originCountry}
+          city={exposureAndFlightsData[fieldsNames.originCity]}
+          cityFieldName={fieldsNames.originCity}
+          airport={exposureAndFlightsData[fieldsNames.originAirport]}
+          airportFieldName={fieldsNames.originAirport}
+          handleChangeExposureDataAndFlightsField={
+            handleChangeExposureDataAndFlightsField
+          }
+        />
+      </FormRowWithInput>
 
-            <FormRowWithInput fieldName='תאריך טיסה:'>
-                <div className={classes.formRow}>
-                    <Typography variant='caption'>מתאריך</Typography>
-                    <DatePick value={formattedDate(departureDate)} onChange={handleStartDateInput} type='date' test-id='flightFromDate'/>
-                    <Typography variant='caption'>עד תאריך</Typography>
-                    <DatePick value={formattedDate(arrivalDate)} onChange={handleEndDateInput} type='date' test-id='flightUntilDate'/>
-                </div>
-            </FormRowWithInput>
+      <FormRowWithInput fieldName="תאריך טיסה:">
+        <div className={classes.formRow}>
+          <Typography variant="caption">מתאריך</Typography>
+          <DatePick
+            type="date"
+            value={exposureAndFlightsData[fieldsNames.flightStartDate]}
+            onChange={(e) =>
+              handleChangeExposureDataAndFlightsField(
+                fieldsNames.flightStartDate,
+                e.target.value
+              )
+            }
+          />
+          <Typography variant="caption">עד תאריך</Typography>
+          <DatePick
+            type="date"
+            value={exposureAndFlightsData[fieldsNames.flightEndDate]}
+            onChange={(e) =>
+              handleChangeExposureDataAndFlightsField(
+                fieldsNames.flightEndDate,
+                e.target.value
+              )
+            }
+          />
+        </div>
+      </FormRowWithInput>
 
-            <FormRowWithInput fieldName='חברת תעופה:'>
-                <CircleTextField value={airline} onChange={handleAirlineInput} test-id='airline'
-                                 placeholder='הזן חברת תעופה'/>
-            </FormRowWithInput>
+      <FormRowWithInput fieldName="חברת תעופה:">
+        <CircleTextField
+          value={exposureAndFlightsData[fieldsNames.airline]}
+          onChange={(e) =>
+            handleChangeExposureDataAndFlightsField(
+              fieldsNames.airline,
+              e.target.value
+            )
+          }
+          placeholder="הזן חברת תעופה"
+        />
+      </FormRowWithInput>
 
-            <FormRowWithInput fieldName='מספר טיסה:'>
-                <CircleTextField value={flightNum} onChange={handleFlightNumberInput} test-id='flightNumber'
-                                 placeholder='הזן מספר טיסה'/>
-            </FormRowWithInput>
-        </Grid>
-    );
+      <FormRowWithInput fieldName="מספר טיסה:">
+        <CircleTextField
+          value={exposureAndFlightsData[fieldsNames.flightNumber]}
+          onChange={(e) =>
+            handleChangeExposureDataAndFlightsField(
+              fieldsNames.flightNumber,
+              e.target.value
+            )
+          }
+          placeholder="הזן מספר טיסה"
+        />
+      </FormRowWithInput>
+    </Grid>
+  );
 };
 
 export default FlightsForm;
