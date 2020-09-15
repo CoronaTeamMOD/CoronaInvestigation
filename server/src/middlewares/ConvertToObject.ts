2199321388d903d39883bfd1144a9a5b37011046
MayaSasson <mayokit22@gmail.com>
2020-09-15 23:55:37 +0300

    fix(): add logs to server

--- server/src/middlewares/ConvertToObject.ts
+++ server/src/middlewares/ConvertToObject.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 const convertToJson = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body)
+    
     if (req.body && Object.keys(req.body).length > 0) {
         req.body = JSON.parse(req.body);
+        console.log(req.body)
     }
     next();
 }
