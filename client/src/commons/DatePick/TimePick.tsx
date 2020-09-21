f366b62d329ef6e64f85cdd268894e229b812b8d
Nevo Bonneh <nevo.bonneh@gmail.com>
2020-09-21 11:12:16 +0300

    use date pickers

--- client/src/commons/DatePick/TimePick.tsx
+++ client/src/commons/DatePick/TimePick.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-
 import { Variant } from '@material-ui/core/styles/createTypography';
 import { StandardTextFieldProps } from '@material-ui/core';
-import { KeyboardDatePicker } from '@material-ui/pickers';
+import { KeyboardTimePicker } from '@material-ui/pickers';
 import { ParsableDate } from '@material-ui/pickers/constants/prop-types';
 
 import { useStyles } from './DatePickStyles';
 
-const DatePick: React.FC<Props> = (props: Props): JSX.Element => {
+const TimePick: React.FC<Props> = (props: Props): JSX.Element => {
   const classes = useStyles({});
 
   const { labelText, value, onChange } = props;
 
   return (
-    <KeyboardDatePicker
+    <KeyboardTimePicker
       className={classes.dateText}
-      disableToolbar
-      variant="inline"
-      format="dd/MM/yyyy"
-      placeholder="dd/MM/yyyy"
+      format="HH:mm"
+      placeholder="HH:mm"
       margin="normal"
       label={labelText}
       value={value}
@@ -33,7 +30,7 @@ const DatePick: React.FC<Props> = (props: Props): JSX.Element => {
   );
 };
 
-export default DatePick;
+export default TimePick;
 
 interface Props extends StandardTextFieldProps {
   labelText?: string;
