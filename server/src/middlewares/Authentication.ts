5148df1545829ce5f79d175f3d690c4ed3b83f5e
Roei Rom <roeirom6@gmail.com>
2020-09-21 13:03:32 +0300

    fix(Authentication.ts): change middleware id key to env variable

--- server/src/middlewares/Authentication.ts
+++ server/src/middlewares/Authentication.ts
@@ -23,7 +23,7 @@ const handleConfidentialAuth = (
 
     const decoded = jwt_decode(token);
     const user = {
-        id: (decoded.upn as string).split('@')[0],
+        id: (decoded[process.env.CLIENT_ID_FIELD] as string).split('@')[0],
         name: decoded.name,
     };
 
