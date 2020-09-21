81a2b5780b7c860d47d479696fca094ca856028b
sandymash <42100027+sandymash@users.noreply.github.com>
2020-09-21 14:20:37 +0300

    fix(fix(contacts)) fixed address grid (#230)
    - fixed address grid typeError
    - removed entrance and floor display ing grid since they arent saved in db

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/ContactDateCard/InteractionCard/AddressGrid/AddressGrid.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/ContactDateCard/InteractionCard/AddressGrid/AddressGrid.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
-
-import { initAddress } from 'models/Address';
 import useFormStyles from 'styles/formStyles';
 import FormInput from 'commons/FormInput/FormInput';
 import InteractionEventDialogData from 'models/Contexts/InteractionEventDialogData';
 
 const AddressGrid : React.FC<Props> = (props: Props) : JSX.Element => {
-    
-    const { removeFloor, removeEntrance, interaction } = props;
+    const { interaction } = props;
         
     const formClasses = useFormStyles();
     
@@ -18,32 +15,12 @@ const AddressGrid : React.FC<Props> = (props: Props) : JSX.Element => {
                 <Grid item xs={6}>
                     <FormInput fieldName='כתובת'>
                         <Typography variant='caption'>
-                            {interaction.locationAddress.description}
+                            {interaction.locationAddress ? (interaction.locationAddress as any).description : 'לא הוזן מיקום'}
                         </Typography>
                     </FormInput>
                 </Grid>
                 <Grid item xs={6}/>
             </Grid>
-            {
-                interaction.locationAddress !== null &&
-                <Grid className={formClasses.formRow} container justify='flex-start'>
-                    <Grid item xs={6}>
-                        { !removeEntrance && <FormInput fieldName='כניסה'>
-                                <Typography variant='caption'>
-                                    {interaction.locationAddress.entrance}
-                                </Typography>
-                            </FormInput>
-                        }
-                    </Grid>
-                    <Grid item xs={6}>
-                        { !removeFloor && <FormInput fieldName='קומה'>
-                            <Typography variant='caption'>
-                                {interaction.locationAddress.floor}
-                            </Typography>
-                        </FormInput> }
-                    </Grid>
-                </Grid>
-            }
         </>
     );
 };
