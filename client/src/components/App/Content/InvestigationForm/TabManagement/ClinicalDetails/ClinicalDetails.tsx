e105c622e33efc9e59e84a92ff1ec43cfa548363
Naaman Tsur <naaman71@gmail.com>
2020-09-24 17:32:25 +0300

    feat(form) move hospital fields to component

--- client/src/components/App/Content/InvestigationForm/TabManagement/ClinicalDetails/ClinicalDetails.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ClinicalDetails/ClinicalDetails.tsx
@@ -23,6 +23,7 @@ import IsolationDatesFields from './IsolationDatesFields';
 import IsolationProblemFields from './IsolationProblemFields'
 import SymptomsFields from './SymptomsFields';
 import BackgroundDiseasesFields from './BackgroundDiseasesFields';
+import HospitalFields from './HospitalFields';
 
 const isInIsolationDateSchema = yup.date().when(
     ClinicalDetailsFields.IS_IN_ISOLATION, {
@@ -276,71 +277,15 @@ const ClinicalDetails: React.FC = (): JSX.Element => {
                 updateClinicalDetails={updateClinicalDetails}
             />
             <Grid spacing={3} container className={classes.containerGrid} justify='flex-start' alignItems='center'>
-                <Grid item xs={2} className={classes.fieldLabel}>
-                    <Typography>
-                        <b>
-                            האם אושפז:
-                        </b>
-                    </Typography>
-                </Grid>
-                <Grid item xs={10}>
-                    <Toggle
-                        test-id='wasHospitalized'
-                        value={context.clinicalDetailsData.wasHospitalized}
-                        onChange={() => updateClinicalDetails(ClinicalDetailsFields.WAS_HOPITALIZED, !context.clinicalDetailsData.wasHospitalized)}
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <Collapse in={context.clinicalDetailsData.wasHospitalized}>
-                        <div className={classes.dates}>
-                            <Typography>
-                                <b>
-                                    בית חולים:
-                                </b>
-                            </Typography>
-                            <TextField
-                                className={classes.hospitalInput}
-                                required
-                                label='בית חולים'
-                                test-id='hospitalInput'
-                                value={context.clinicalDetailsData.hospital}
-                                onChange={(event: React.ChangeEvent<{ value: unknown }>) => (
-                                    updateClinicalDetails(ClinicalDetailsFields.HOSPITAL, event.target.value)
-                                )}
-                            />
-                        </div>
-                        <div className={classes.hospitalizationDates}>
-                            <div className={classes.spacedDates}>
-                                <DatePick
-                                    required
-                                    label='מתאריך'
-                                    test-id='wasHospitalizedFromDate'
-                                    labelText='מתאריך'
-                                    value={context.clinicalDetailsData.hospitalizationStartDate}
-                                    onChange={(newDate: Date) =>
-                                        updateClinicalDetails(
-                                            ClinicalDetailsFields.HOSPITALIZATION_START_DATE,
-                                            newDate
-                                        )
-                                    }
-                                />
-                            </div>
-                            <DatePick
-                                required
-                                label='עד'
-                                testId='wasHospitalizedUntilDate'
-                                labelText='עד'
-                                value={context.clinicalDetailsData.hospitalizationEndDate}
-                                onChange={(newDate: Date) =>
-                                    updateClinicalDetails(
-                                        ClinicalDetailsFields.HOSPITALIZATION_END_DATE,
-                                        newDate
-                                    )
-                                }
-                            />
-                        </div>
-                    </Collapse>
-                </Grid>
+                <HospitalFields
+                    classes={classes}
+                    control={control}
+                    setError={setError}
+                    clearErrors={clearErrors}
+                    errors={errors}
+                    context={context}
+                    updateClinicalDetails={updateClinicalDetails}
+                />
                 {patientGender === Gender.FEMALE ?
                     <>
                         <Grid item xs={2}>
