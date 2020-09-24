acb262ca89ec75382a94ccfa178ff2b1621678e3
Naaman Tsur <naaman71@gmail.com>
2020-09-24 16:20:01 +0300

    feat(form) move some of fields to seperate components

---
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ClinicalDetails/IsolationProblemFields.tsx
@@ -0,0 +1,74 @@
+import { Collapse, Grid, Typography } from '@material-ui/core';
+import AlphanumericTextField from 'commons/AlphanumericTextField/AlphanumericTextField';
+import Toggle from 'commons/Toggle/Toggle';
+import ClinicalDetailsFields from 'models/enums/ClinicalDetailsFields';
+import React from 'react';
+import { Controller } from 'react-hook-form';
+
+const IsolationProblemFields: React.FC<Props> = (props: Props): JSX.Element => {
+    const { classes, control, watchIsIsolationProblem, setError, clearErrors, errors } = props;
+
+    return (
+        <Grid spacing={3} container className={classes.containerGrid} justify='flex-start' alignItems='center'>
+                <Grid item xs={2} className={classes.fieldLabel}>
+                    <Typography>
+                        <b>
+                            האם בעייתי לקיים בידוד:
+                        </b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Controller
+                        name={ClinicalDetailsFields.IS_ISOLATION_PROBLEM}
+                        control={control}
+                        render={(props) => (
+                            <Toggle
+                                test-id='isQuarantineProblematic'
+                                value={props.value}
+                                onChange={(e, value) => {
+                                    if (value !== null) {
+                                        props.onChange(value)
+                                    }
+                                }}
+                            />
+                        )}
+                    />
+                </Grid>
+                <Grid item xs={2}>
+                    <Collapse in={watchIsIsolationProblem}>
+                        <Controller
+                            name={ClinicalDetailsFields.IS_ISOLATION_PROBLEM_MORE_INFO}
+                            control={control}
+                            render={(props) => (
+                                <AlphanumericTextField
+                                    test-id='problematicQuarantineReason'
+                                    name={ClinicalDetailsFields.IS_ISOLATION_PROBLEM_MORE_INFO}
+                                    value={props.value}
+                                    onChange={(newValue: string) => (
+                                        props.onChange(newValue)
+                                    )}
+                                    setError={setError}
+                                    clearErrors={clearErrors}
+                                    errors={errors}
+                                    placeholder='הכנס סיבה:'
+                                    className={classes.isolationProblemTextField}
+                                />
+                            )}
+                        />
+                    </Collapse>
+                </Grid>
+            </Grid>
+    );
+};
+
+interface Props {
+    classes: any;
+    watchIsIsolationProblem: boolean;
+    control: any;
+    setError: any;
+    clearErrors: any;
+    errors: any;
+};
+
+export default IsolationProblemFields;
+
