e39bf48eda6b70c51b5836ee99821538e9d6c5bb
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-09-30 10:06:50 +0300

    Using Daniel's function for saving the contacts

--- server/src/ClientToDBAPI/ContactedPeople/mainRoute.ts
+++ server/src/ClientToDBAPI/ContactedPeople/mainRoute.ts
@@ -1,8 +1,7 @@
 import { Router, Request, Response } from 'express';
 
 import { graphqlRequest } from '../../GraphqlHTTPRequest';
-import { ContactedPerson } from '../../Models/ContactedPerson/ContactedPerson';
-import { UPDATE_CONTACTED_PERSON, SAVE_ALL_CONTACTS} from '../../DBService/ContactedPeople/Mutation';
+import { SAVE_LIST_OF_CONTACTS } from '../../DBService/ContactedPeople/Mutation';
 import { GET_ALL_FAMILY_RELATIONSHIPS, GET_CONTACTED_PEOPLE } from '../../DBService/ContactedPeople/Query';
 
 const ContactedPeopleRoute = Router();
@@ -17,29 +16,8 @@ ContactedPeopleRoute.post('/familyRelationships', (request: Request, response: R
     graphqlRequest(GET_ALL_FAMILY_RELATIONSHIPS, response.locals).then((result: any) => response.send(result))
 );
 
-ContactedPeopleRoute.post('/updateContactedPerson', (request: Request, response: Response) => {
-    const updatedPersonDetails: ContactedPerson = request.body;
-    graphqlRequest(UPDATE_CONTACTED_PERSON, response.locals, {
-        currContactedPerson: updatedPersonDetails.id,
-        cantReachContact: updatedPersonDetails.cantReachContact,
-        cityOfContacted: updatedPersonDetails.contactedPersonCity.displayName,
-        currCondition: updatedPersonDetails.doesFeelGood,
-        doesLiveWithConfirmed: updatedPersonDetails.doesLiveWithConfirmed,
-        doesNeedHelpInIsolation: updatedPersonDetails.doesNeedHelpInIsolation,
-        relationShip: updatedPersonDetails.relationship,
-        repeatingOccuranceWithConfirmed: updatedPersonDetails.repeatingOccuranceWithConfirmed,
-        familyRelationship: updatedPersonDetails.familyRelationship,
-        doesWorkWithCrowd: updatedPersonDetails,
-        doesHaveBackgroundDiseases: updatedPersonDetails.doesHaveBackgroundDiseases,
-        occupation: updatedPersonDetails.occupation,
-    })
-        .then((result: any) => response.send(result))
-        .catch(error => response.status(500).json({error: 'failed to save contacted person'}))
-
-});
-
 ContactedPeopleRoute.post('/saveAllContacts', (request: Request, response: Response) =>
-    graphqlRequest(SAVE_ALL_CONTACTS, response.locals, { unSavedContacts: request.body})
+    graphqlRequest(SAVE_LIST_OF_CONTACTS, response.locals, { unSavedContacts: request.body})
         .then((result: any) => response.send(result))
         .catch(error => response.status(500).json({error: 'failed to save all the contacts'}))
 );
--- server/src/DBService/ContactedPeople/Mutation.ts
+++ server/src/DBService/ContactedPeople/Mutation.ts
@@ -1,31 +1,6 @@
 import { gql } from 'postgraphile';
 
-export const UPDATE_CONTACTED_PERSON = gql`
-mutation updatedContactedPerson($currContactedPerson: Int!, $cantReachContact: Boolean!, $cityOfContacted: String!, $currCondition: Boolean!, 
-  $doesLiveWithConfirmed: Boolean!, $doesNeedHelpInIsolation: Boolean!, $relationShip: String!, $repeatingOccuranceWithConfirmed: Boolean!,
-  $familyRelationship: Int!, $doesWorkWithCrowd: Boolean!, $doesHaveBackgroundDiseases: Boolean!, $contacted_person_occupation: String!) {
-  updateContactedPersonById(input: {
-    contactedPersonPatch: {
-      cantReachContact: $cantReachContact, 
-      contactedPersonCity: $cityOfContacted, 
-      doesFeelGood: $currCondition, 
-      doesLiveWithConfirmed: $doesLiveWithConfirmed, 
-      doesNeedHelpInIsolation: $doesNeedHelpInIsolation, 
-      relationship: $relationShip, 
-      repeatingOccuranceWithConfirmed: $repeatingOccuranceWithConfirmed, 
-      familyRelationship: $familyRelationship, 
-      crowd: $doesWorkWithCrowd, 
-      doesHaveBackgroundDiseases: $doesHaveBackgroundDiseases,
-      occupation: $contacted_person_occupation
-    }, 
-    id: $currContactedPerson
-  }) {
-    clientMutationId
-  }
-}
-`;
-
-export const SAVE_ALL_CONTACTS = gql`
+export const SAVE_LIST_OF_CONTACTS = gql`
 mutation updateAllUnSavedContacts($unSavedContacts: JSON!) {
   updateContactPersons(input: {contactedPersons: $unSavedContacts}) {
     clientMutationId
