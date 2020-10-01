a5f0eba5072389ef6f45ea65ae1104bb65c65080
Itay Ben Moshe <itaybenm@gmail.com>
2020-10-01 23:36:45 +0300

    Add nullable to schema

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionEventForm/useInteractionsForm.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionEventForm/useInteractionsForm.ts
@@ -17,7 +17,7 @@ const useInteractionsForm = (props : Props): outCome => {
     const epidemiologyNumber = useSelector<StoreStateType, number>(state => state.investigation.epidemiologyNumber);
 
     const schema = yup.object().shape({
-        [InteractionEventDialogFields.PLACE_TYPE]: yup.string().required('סוג אתר חובה'),
+        [InteractionEventDialogFields.PLACE_TYPE]: yup.string().nullable().required('סוג אתר חובה'),
         [InteractionEventDialogFields.PLACE_SUB_TYPE]: yup.number().when(
           InteractionEventDialogFields.PLACE_TYPE, {
             is: placeType => placeType !== placeTypesCodesHierarchy.geriatric.code &&
@@ -26,7 +26,8 @@ const useInteractionsForm = (props : Props): outCome => {
             otherwise: yup.number().nullable()
           }
         ),
-        [InteractionEventDialogFields.CONTACT_PERSON_PHONE_NUMBER]: yup.string().matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/, 'מספר טלפון לא תקין'),
+        [InteractionEventDialogFields.CONTACT_PERSON_PHONE_NUMBER]: yup.string().nullable().
+          matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/, 'מספר טלפון לא תקין'),
         [InteractionEventDialogFields.START_TIME]: yup.date().required(),
         [InteractionEventDialogFields.END_TIME]: yup.date().required().when(
           InteractionEventDialogFields.START_TIME, (startTime: Date) => {
@@ -34,11 +35,11 @@ const useInteractionsForm = (props : Props): outCome => {
         }),
         [InteractionEventDialogFields.EXTERNALIZATION_APPROVAL]: yup.boolean().required('שדה חובה'),
         [InteractionEventDialogFields.CONTACTS]: yup.array().of(yup.object().shape({
-            [InteractionEventContactFields.FIRST_NAME]: yup.string().required('שם פרטי חובה'),
-            [InteractionEventContactFields.LAST_NAME]: yup.string().required('שם משפחה חובה'),
-            [InteractionEventContactFields.PHONE_NUMBER]: yup.string().required('מספר טלפון חובה')
+            [InteractionEventContactFields.FIRST_NAME]: yup.string().nullable().required('שם פרטי חובה'),
+            [InteractionEventContactFields.LAST_NAME]: yup.string().nullable().required('שם משפחה חובה'),
+            [InteractionEventContactFields.PHONE_NUMBER]: yup.string().nullable().required('מספר טלפון חובה')
               .matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/, 'מספר טלפון לא תקין'),
-              [InteractionEventContactFields.ID]: yup.string().matches(/^\d+$/, 'ת.ז חייבת להכיל מספרים בלבד')
+              [InteractionEventContactFields.ID]: yup.string().nullable().matches(/^\d+|^$/, 'ת.ז חייבת להכיל מספרים בלבד')
               .length(9, 'ת.ז מכילה 9 מספרים בלבד')
               .test('isValid', "ת.ז לא תקינה", (id: any) => {
                 let sum = 0;
