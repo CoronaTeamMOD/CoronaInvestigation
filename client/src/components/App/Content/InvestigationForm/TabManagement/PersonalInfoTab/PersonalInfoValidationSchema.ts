82d032107a12081e3f6f68b99ba098dc643cd0ac
Nevo Bonneh <nevo.bonneh@gmail.com>
2020-10-04 11:18:42 +0300

    Move herbrew text to consts

--- client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
@@ -1,29 +1,34 @@
 import * as yup from 'yup';
 import PersonalInfoDataContextFields from 'models/enums/PersonalInfoDataContextFields';
 
+const occupationsWithInstitution = ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון"]
+const occupationsWithoutExtraInfo = ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון","לא עובד"]
+const requiredText = 'שגיאה: שדה חובה'
+const numberValidationText = 'שגיאה: מספר אינו תקין'
+
 const schema = yup.object().shape({
-    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required('שגיאה: שדה חובה').matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,'שגיאה: מספר אינו תקין'),
-    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/,'שגיאה: מספר אינו תקין'),
-    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().required('שגיאה: שדה חובה').matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,'שגיאה: מספר אינו תקין'),
-    [PersonalInfoDataContextFields.INSURANCE_COMPANY]: yup.string().nullable().required('שגיאה: שדה חובה'),
-    [PersonalInfoDataContextFields.CITY]: yup.string().nullable().required('שגיאה: שדה חובה'),
-    [PersonalInfoDataContextFields.CONTACT_INFO]: yup.string().nullable().required('שגיאה: שדה חובה'),
+    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,numberValidationText),
+    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/,numberValidationText),
+    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,numberValidationText),
+    [PersonalInfoDataContextFields.INSURANCE_COMPANY]: yup.string().nullable().required(requiredText),
+    [PersonalInfoDataContextFields.CITY]: yup.string().nullable().required(requiredText),
+    [PersonalInfoDataContextFields.CONTACT_INFO]: yup.string().nullable().required(requiredText),
     [PersonalInfoDataContextFields.EDUCATION_OCCUPATION_CITY]:  yup.string().when(
         PersonalInfoDataContextFields.RELEVANT_OCCUPATION, {
             is: "מערכת החינוך",
-            then: yup.string().nullable().required('שדה זה הינו שדה חובה'),
+            then: yup.string().nullable().required(requiredText),
             else: yup.string().nullable()
         }
     ),
     [PersonalInfoDataContextFields.INSTITUTION_NAME]:  yup.string().when("relevantOccupation", (relevantOccupation:any, schema:any) => {
-        return ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון"].find(element => element === relevantOccupation)? 
-        schema.nullable().required('שדה זה הינו שדה חובה') : 
+        return occupationsWithInstitution.find(element => element === relevantOccupation)? 
+        schema.nullable().required(requiredText) : 
         schema.nullable()
     }),
     [PersonalInfoDataContextFields.OTHER_OCCUPATION_EXTRA_INFO]:  yup.string().when("relevantOccupation", (relevantOccupation:any, schema:any) => {
-        return ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון","לא עובד"].find(element => element === relevantOccupation)? 
+        return occupationsWithoutExtraInfo.find(element => element === relevantOccupation)? 
         schema.nullable() :
-        schema.nullable().required('שדה זה הינו שדה חובה')  
+        schema.nullable().required(requiredText)  
     }),
 });
 
