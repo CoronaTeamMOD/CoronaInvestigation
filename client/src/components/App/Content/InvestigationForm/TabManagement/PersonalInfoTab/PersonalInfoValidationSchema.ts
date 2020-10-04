3ce0795be56cc0c1cfd33c8f97f0c84b675aad96
Nevo Bonneh <nevo.bonneh@gmail.com>
2020-10-04 14:43:17 +0300

    Fix phone number regex

--- client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
@@ -1,15 +1,16 @@
 import * as yup from 'yup';
 import PersonalInfoDataContextFields from 'models/enums/PersonalInfoDataContextFields';
-
 const occupationsWithInstitution = ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון"]
 const occupationsWithoutExtraInfo = ["מערכת הבריאות", "מערכת החינוך","כוחות הביטחון","לא עובד"]
 const requiredText = 'שגיאה: שדה חובה'
 const numberValidationText = 'שגיאה: מספר אינו תקין'
+const phoneNumberRegex = /^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/
+const notRequiredPhoneNumberRegex = /^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/
 
 const schema = yup.object().shape({
-    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,numberValidationText),
-    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))|^$/,numberValidationText),
-    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(/^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(\d{7}))$/,numberValidationText),
+    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(phoneNumberRegex, numberValidationText),
+    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(notRequiredPhoneNumberRegex, numberValidationText),
+    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(phoneNumberRegex, numberValidationText),
     [PersonalInfoDataContextFields.INSURANCE_COMPANY]: yup.string().nullable().required(requiredText),
     [PersonalInfoDataContextFields.CITY]: yup.string().nullable().required(requiredText),
     [PersonalInfoDataContextFields.CONTACT_INFO]: yup.string().nullable().required(requiredText),
