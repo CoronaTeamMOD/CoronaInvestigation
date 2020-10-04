7b35582e85849e059c2d9797bf452471e73c710c
Itay Ben Moshe <itaybenm@gmail.com>
2020-10-04 23:09:22 +0300

    Change get function

--- client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
+++ client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
@@ -1,9 +1,8 @@
 const get = (obj: any, path: string, defaultValue = undefined) => {
     const travel = (regexp: RegExp) =>
-      String.prototype.split
-        .call(path, regexp)
-        .filter(Boolean)
-        .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+      path.split(regexp)
+          .filter(Boolean)
+          .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
     const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
     return result === undefined || result === obj ? defaultValue : result;
   };
