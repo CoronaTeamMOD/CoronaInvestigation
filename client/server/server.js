69e61b9dd4345e769bd6afe11b44573a17c64b44
Roei Rom <roeirom6@gmail.com>
2020-10-06 10:04:21 +0300

    fix(client/server): node rp instead of nginx

---
+++ client/server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(
+    '/db',
+    createProxyMiddleware({
+        target: process.env.DB_API,
+        pathRewrite: {
+            '^/db': ''
+        },
+        changeOrigin: true
+    })
+)
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+app.listen(9090, () => console.log('client access server started on port 8080'));
