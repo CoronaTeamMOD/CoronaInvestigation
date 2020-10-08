0f503467c16c58c8cc11fbc0dd92adade1bacd8d
MayaSasson <mayokit22@gmail.com>
2020-10-08 12:06:48 +0300

    fix(): changed validation regex

--- client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoValidationSchema.ts
@@ -12,7 +12,7 @@ const notRequiredPhoneNumberRegex = /^(0(?:[23489]|5[0-689]|7[2346789])(?![01])(
 const schema = yup.object().shape({
     [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(phoneNumberRegex, numberValidationText),
     [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(notRequiredPhoneNumberRegex, numberValidationText),
-    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().matches(phoneNumberRegex, numberValidationText),
+    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().matches(notRequiredPhoneNumberRegex, numberValidationText),
     [PersonalInfoDataContextFields.INSURANCE_COMPANY]: yup.string().nullable().required(requiredText),
     [PersonalInfoDataContextFields.CITY]: yup.string().nullable().required(requiredText),
     [PersonalInfoDataContextFields.CONTACT_INFO]: yup.string().nullable(),
