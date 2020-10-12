1952a5e98289d905c6580d679c0c4fb9a559de77
Naaman Tsur <naaman71@gmail.com>
2020-10-12 11:28:39 +0300

    Feat/phone text field (#382)
    * feat(textfield) create numeric text field
    
    * feat(phone) change phone text fields to numeric text fields
    
    * feat(textfield) change another phone and id field to numeric
    
    * feat(phone) return phone labels
    
    * feat(phone) phone number label

---
+++ client/src/commons/NumericTextField/NumericTextField.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as yup from 'yup';
+
+import NumericTextFieldType from './NumericTextFieldTypes';
+import TypePreventiveTextField from '../TypingPreventionTextField/TypingPreventionTextField';
+
+const stringAlphabet = yup
+  .string()
+  .required()
+  .matches(/^[0-9]*$/);
+
+const errorMessage = 'השדה יכול להכיל רק מספרים';
+
+const NumericTextField: NumericTextFieldType = (props) => {
+  return (
+    <TypePreventiveTextField
+        {...props}
+        validationSchema={stringAlphabet}
+        errorMessage={errorMessage}
+    />
+  );
+};
+
+export default NumericTextField;
