a18ca10b2959eb985b52e020f433d9a9b45064b0
Itay Ben Moshe <itaybenm@gmail.com>
2020-10-13 20:34:25 +0300

    fix(SignUpFormStyles): delete unnecessary theme

--- client/src/components/App/Content/SignUp/SignUpForm/SignUpFormStyles.ts
+++ client/src/components/App/Content/SignUp/SignUpForm/SignUpFormStyles.ts
@@ -1,7 +1,6 @@
-import { Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles(() => ({
     formRow: {
         margin: '3vh 0',
     },
