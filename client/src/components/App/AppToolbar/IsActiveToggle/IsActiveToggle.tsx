07da49fae94095fd4e00ce140e17cd4f8ee25941
MayaSasson <mayokit22@gmail.com>
2020-10-14 01:06:06 +0300

    fix: fixed isActive functionality

--- client/src/components/App/AppToolbar/IsActiveToggle/IsActiveToggle.tsx
+++ client/src/components/App/AppToolbar/IsActiveToggle/IsActiveToggle.tsx
@@ -24,13 +24,13 @@ const IsActiveToggle: React.FC<Props> = React.forwardRef((props: Props, ref): JS
         <ToggleButtonGroup {...rest} ref={ref} value={value} exclusive size='small' className={classes.isActiveToggle}>
             <ToggleButton className={classes.toggle}
                           style={value ? activeButtonStyle : {}}
-                          onClick={()=> setUserActivityStatus(!value)}
+                          onClick={()=> setUserActivityStatus(true)}
                           value={value}>
                 פעיל
             </ToggleButton>
             <ToggleButton className={classes.toggle}
                           style={!value ? notActiveButtonStyle : {}}
-                          onClick={()=> setUserActivityStatus(!value)}
+                          onClick={()=> setUserActivityStatus(false)}
                           value={value}>
                 לא פעיל
             </ToggleButton>
