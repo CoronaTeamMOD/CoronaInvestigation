7311d01dfbdfb47fbcc8996454d164df4c253156
MayaSasson <62448339+MayaSasson@users.noreply.github.com>
2020-10-13 09:45:28 +0300

    fix(bugs): fixed refresh, place holder and hmo bug (#395)
    * fix(routing when refresh): getting back the error
    
    * fix: place holder
    
    * fix(): hmo isnt mandatory

--- server/src/DBService/PersonalDetails/Mutation.ts
+++ server/src/DBService/PersonalDetails/Mutation.ts
@@ -1,7 +1,7 @@
 import { gql } from "postgraphile";
 
 export const UPDATE_INVESTIGATED_PERSON_PERSONAL_INFO = gql`
-mutation updateInvestigatedPersonPersonalInfo($id: Int!, $hmo: String!, $otherOccupationExtraInfo: String, $occupation: String, $patientContactPhoneNumber: String, $subOccupation: String, $patientContactInfo: String, $additionalPhoneNumber: String) {
+mutation updateInvestigatedPersonPersonalInfo($id: Int!, $hmo: String, $otherOccupationExtraInfo: String, $occupation: String, $patientContactPhoneNumber: String, $subOccupation: String, $patientContactInfo: String, $additionalPhoneNumber: String) {
     updateInvestigatedPatientById(input: {investigatedPatientPatch: {hmo: $hmo, otherOccupationExtraInfo: $otherOccupationExtraInfo, occupation: $occupation, patientContactPhoneNumber: $patientContactPhoneNumber, subOccupation: $subOccupation, patientContactInfo: $patientContactInfo, additionalPhoneNumber: $additionalPhoneNumber}, id: $id}) {
         covidPatientByCovidPatient {
             epidemiologyNumber
