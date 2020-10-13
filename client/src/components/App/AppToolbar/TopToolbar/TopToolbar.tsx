4584d48202569a55ddc3f7187537921e3eb3577a
Orel Karavani <orel1192@gmail.com>
2020-10-13 18:13:47 +0300

    fix(active): remove active toggle from UI for now

--- client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
+++ client/src/components/App/AppToolbar/TopToolbar/TopToolbar.tsx
@@ -2,34 +2,16 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import {Toolbar} from '@material-ui/core';
 import {Typography} from '@material-ui/core';
-import Tooltip from '@material-ui/core/Tooltip';
 
 import User from 'models/User';
 import StoreStateType from 'redux/storeStateType';
 
 import useStyles from './TopToolbarStyles';
-import useTopToolbar from './useTopToolbar';
-import IsActiveToggle from './IsActiveToggle/IsActiveToggle';
-
-const toggleMessage ='מה הסטטוס שלך?';
 
 const TopToolbar: React.FC = (): JSX.Element => {
     const classes = useStyles({});
-    const firstUserUpdate = React.useRef(true);
 
     const user = useSelector<StoreStateType, User>(state => state.user);
-
-    const [isActive, setIsActive] = React.useState<boolean>(false);
-
-    const {getUserActivityStatus, setUserActivityStatus} = useTopToolbar({ setIsActive });
-
-    React.useEffect( () => {
-        if (firstUserUpdate.current) {
-            firstUserUpdate.current = false;
-        } else {
-            getUserActivityStatus();
-        }
-    }, [user])
 
     return (
         <Toolbar className={classes.toolbar} >
@@ -38,9 +20,6 @@ const TopToolbar: React.FC = (): JSX.Element => {
                 <Typography variant='h4' className={classes.centering}><div className={classes.systemName}>אבן יסוד</div></Typography>
             </div>
             <div className={classes.leftToolbarSection}>
-                <Tooltip title={toggleMessage} arrow>
-                    <IsActiveToggle value={isActive} setUserActivityStatus={setUserActivityStatus} exclusive/>
-                </Tooltip>
                 <Typography>שלום, {user.userName}</Typography>
             </div>
         </Toolbar>
