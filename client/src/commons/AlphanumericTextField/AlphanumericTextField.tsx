cd90f72fc90eed31839d26ea4431e39570d494f6
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-10-14 19:35:05 +0300

    added another error message

--- client/src/commons/AlphanumericTextField/AlphanumericTextField.tsx
+++ client/src/commons/AlphanumericTextField/AlphanumericTextField.tsx
@@ -5,12 +5,13 @@ import AlphanumericTextFieldType from './AlphanumericTextFieldTypes';
 import TypePreventiveTextField from '../TypingPreventionTextField/TypingPreventionTextField';
 
 const errorMessage = 'השדה יכול להכיל רק אותיות ומספרים';
+const maxLengthErrorMessage = 'השדה יכול להכיל 50 תוים בלבד';
 
 const stringAlphanum = yup
   .string()
   .required()
   .matches(/^[a-zA-Z\u0590-\u05fe0-9\s]*$/, errorMessage)
-  .max(50);
+  .max(50, maxLengthErrorMessage);
 
 
 const AlphanumericTextField: AlphanumericTextFieldType = (props) => {
