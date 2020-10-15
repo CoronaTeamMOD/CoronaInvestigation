1f35fcf55d19ce2c9f0b33133e1619809315822f
Roy Maniv <roymaniv@gmail.com>
2020-10-15 15:36:43 +0300

    fix(TabNames): changes tab name

--- client/src/models/enums/TabNames.ts
+++ client/src/models/enums/TabNames.ts
@@ -1,7 +1,7 @@
 enum TabNames {
     PERSONAL_INFO = 'פרטים אישיים',
     CLINICAL_DETAILS = 'בידוד ופרטים קליניים',
-    EXPOSURES_AND_FLIGHTS = 'חשיפה אפשרית וחו"ל',
+    EXPOSURES_AND_FLIGHTS = 'חשיפה אפשרית',
     INTERACTIONS = 'מקומות ומגעים',
     CONTACT_QUESTIONING = 'תשאול מגעים',
 }
