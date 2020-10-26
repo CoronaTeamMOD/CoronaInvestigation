915a489335a310a67fa64da891179ff2dfedb45c
Naaman Tsur <naaman71@gmail.com>
2020-10-26 13:51:39 +0200

    feat(sort) change sort to accept single 'order by' parameter

--- server/src/ClientToDBAPI/UsersRoute/mainRoute.ts
+++ server/src/ClientToDBAPI/UsersRoute/mainRoute.ts
@@ -401,7 +401,7 @@ usersRoute.post('', (request: Request, response: Response) => {
         {
             offset: (request.body.page.number - 1) * request.body.page.size,
             size: request.body.page.size,
-            orderBy: request.body.orderBy
+            orderBy: [request.body.orderBy? request.body.orderBy : 'NATURAL']
         }
     )
         .then((result: any) => {
