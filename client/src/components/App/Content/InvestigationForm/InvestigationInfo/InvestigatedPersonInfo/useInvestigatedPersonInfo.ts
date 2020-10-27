324a02820a046ad3c55ec1bd1893ee072eac9351
yaniryos <58386254+yaniryos@users.noreply.github.com>
2020-10-27 14:32:39 +0200

    feat(): complexity by status on investigation end (#603)
    * feat(): complexity by status on end
    
    * fix(): leave investigation bug
    
    * fix(): mr fixes

--- client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/useInvestigatedPersonInfo.ts
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigatedPersonInfo/useInvestigatedPersonInfo.ts
@@ -5,15 +5,14 @@ import { useHistory } from 'react-router-dom';
 import axios from 'Utils/axios';
 import theme from 'styles/theme';
 import logger from 'logger/logger';
-import {timeout} from 'Utils/Timeout/Timeout';
+import { timeout } from 'Utils/Timeout/Timeout';
 import StoreStateType from 'redux/storeStateType';
 import { Service, Severity } from 'models/Logger';
-import InvestigatedPatient from 'models/InvestigatedPatient';
+import useStatusUtils from 'Utils/StatusUtils/useStatusUtils';
 import { InvestigationStatus } from 'models/InvestigationStatus';
 import InvestigationMainStatus from 'models/enums/InvestigationMainStatus';
 import { setInvestigationStatus } from 'redux/Investigation/investigationActionCreators';
 import InvestigationComplexityByStatus from 'models/enums/InvestigationComplexityByStatus';
-import useComplexitySwal from 'commons/InvestigationComplexity/ComplexityUtils/ComplexitySwal';
 import { setIsInInvestigation } from 'redux/IsInInvestigations/isInInvestigationActionCreators';
 
 import useStyles from './InvestigatedPersonInfoStyles';
@@ -24,62 +23,26 @@ const useInvestigatedPersonInfo = (): InvestigatedPersonInfoOutcome => {
     let history = useHistory();
     const classes = useStyles({});
 
-    const investigationStatus = useSelector<StoreStateType, InvestigationStatus>(state => state.investigation.investigationStatus);
-    const investigatedPatient = useSelector<StoreStateType, InvestigatedPatient>(state => state.investigation.investigatedPatient);
-    const epidemiologyNumber = useSelector<StoreStateType, number>(state => state.investigation.epidemiologyNumber);
+    const { updateIsDeceased, updateIsCurrentlyHospitialized } = useStatusUtils();
+
     const userId = useSelector<StoreStateType, string>(state => state.user.id);
+    const investigationStatus = useSelector<StoreStateType, InvestigationStatus>(state => state.investigation.investigationStatus);
 
-    const { complexityErrorAlert } = useComplexitySwal();
-
-    const updateIsDeceased = () => {
-        !investigatedPatient.isDeceased && axios.post('/clinicalDetails/isDeceased/'+investigatedPatient.investigatedPatientId + '/'+true)
-        .then((result: any) => {
-            logger.info({
-                service: Service.CLIENT,
-                severity: Severity.LOW,
-                workflow: 'Update isDeceased',
-                step: `launching isDeceased request succssesfully ${result}`,
-                user: userId,
-                investigation: epidemiologyNumber
-            });
-            handleInvestigationFinish();
-        }).catch((error: any) => {
-            logger.info({
-                service: Service.CLIENT,
-                severity: Severity.LOW,
-                workflow: 'Update isDeceased',
-                step: `launching isDeceased request unsuccssesfully ${error}`,
-                user: userId,
-                investigation: epidemiologyNumber
-            });
-            complexityErrorAlert(error);
+    const handleInvestigationFinish = async () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'בחרת לצאת מהחקירה לפני השלמתה! הנך מועבר לעמוד הנחיתה',
+            customClass: {
+                title: classes.swalTitle,
+            },
+            timer: 1750,
+            showConfirmButton: false
         })
-    }
-
-    const updateIsCurrentlyHospitialized = () => {
-        !investigatedPatient.isCurrentlyHospitialized && axios.post('/clinicalDetails/isCurrentlyHospitialized/'+investigatedPatient.investigatedPatientId + '/'+true)
-        .then((result: any) => {
-            logger.info({
-                service: Service.CLIENT,
-                severity: Severity.LOW,
-                workflow: 'Update isCurrentlyHospitialized',
-                step: `launching isCurrentlyHospitialized request succssesfully ${result}`,
-                user: userId,
-                investigation: epidemiologyNumber
-            });
-            handleInvestigationFinish();
-        }).catch((error: any) => {
-            logger.info({
-                service: Service.CLIENT,
-                severity: Severity.LOW,
-                workflow: 'Update isCurrentlyHospitialized',
-                step: `launching isCurrentlyHospitialized request unsuccssesfully ${error}`,
-                user: userId,
-                investigation: epidemiologyNumber
-            });
-            complexityErrorAlert(error);
-        })
-    }
+        timeout(1500).then(() => {
+            setIsInInvestigation(false);
+            window.close();
+        });
+    };
 
     const confirmExitUnfinishedInvestigation = (epidemiologyNumber: number) => {
         Swal.fire({
@@ -117,8 +80,12 @@ const useInvestigatedPersonInfo = (): InvestigatedPersonInfoOutcome => {
                         user: userId,
                         investigation: epidemiologyNumber
                     });
-                    investigationStatus.subStatus === InvestigationComplexityByStatus.IS_DECEASED && updateIsDeceased();
-                    investigationStatus.subStatus === InvestigationComplexityByStatus.IS_CURRENTLY_HOSPITIALIZED && updateIsCurrentlyHospitialized();
+                    investigationStatus.subStatus === InvestigationComplexityByStatus.IS_DECEASED && updateIsDeceased(handleInvestigationFinish);
+                    investigationStatus.subStatus === InvestigationComplexityByStatus.IS_CURRENTLY_HOSPITIALIZED && updateIsCurrentlyHospitialized(handleInvestigationFinish);
+                    if (investigationStatus.subStatus !== InvestigationComplexityByStatus.IS_DECEASED &&
+                        investigationStatus.subStatus !== InvestigationComplexityByStatus.IS_CURRENTLY_HOSPITIALIZED) {
+                        handleInvestigationFinish();
+                    }
                 }).catch((error) => {
                     logger.error({
                         service: Service.CLIENT,
@@ -134,29 +101,12 @@ const useInvestigatedPersonInfo = (): InvestigatedPersonInfoOutcome => {
         });
     };
 
-    const handleInvestigationFinish = async () => {
-            Swal.fire({
-                icon: 'success',
-                title: 'בחרת לצאת מהחקירה לפני השלמתה! הנך מועבר לעמוד הנחיתה',
-                customClass: {
-                    title: classes.swalTitle,
-                },
-                timer: 1750,
-                showConfirmButton: false
-            })
-            timeout(1500).then(()=> {
-                setIsInInvestigation(false);
-                window.close();
-            });
-    };
-
     const getPersonAge = (birthDate: Date) => {
 
         const currentDate = new Date();
         let personAge = currentDate.getFullYear() - birthDate.getFullYear();
         const monthDelta = currentDate.getMonth() - birthDate.getMonth();
-        if (monthDelta < 0 || (monthDelta === 0 && currentDate.getDate() < birthDate.getDate())) 
-        {
+        if (monthDelta < 0 || (monthDelta === 0 && currentDate.getDate() < birthDate.getDate())) {
             personAge--;
         }
         return String(personAge);
@@ -172,10 +122,10 @@ const useInvestigatedPersonInfo = (): InvestigatedPersonInfoOutcome => {
         })
     };
 
-    const handleCannotCompleteInvestigationCheck = (cannotCompleteInvestigation: boolean) => {      
+    const handleCannotCompleteInvestigationCheck = (cannotCompleteInvestigation: boolean) => {
         setInvestigationStatus({
             mainStatus: cannotCompleteInvestigation ? InvestigationMainStatus.CANT_COMPLETE : InvestigationMainStatus.IN_PROCESS,
-            subStatus:  investigationStatus.subStatus
+            subStatus: investigationStatus.subStatus
         })
     };
 
