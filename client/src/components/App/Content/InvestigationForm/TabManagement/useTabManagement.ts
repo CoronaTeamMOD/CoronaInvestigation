75e30ce63dfd37f0e3609cbe3d7fcd5ef29a5dfc
Roei Rom <roeirom6@gmail.com>
2020-11-01 17:39:32 +0200

    feat(useTabManagement.ts): add redux data to the hook

--- client/src/components/App/Content/InvestigationForm/TabManagement/useTabManagement.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/useTabManagement.ts
@@ -1,9 +1,16 @@
+import { useSelector } from 'react-redux';
 import { useState, useRef, RefObject } from 'react';
 
+import StoreStateType from 'redux/storeStateType';
+
+const CONTACTS_QUESTIONING_TAB_ID = 4;
+const exportToMabarTabs = [CONTACTS_QUESTIONING_TAB_ID];
 const useTabManagement = () => {
 
     const [currentTab, setCurrentTab] = useState<number>(0);
     const nextTab: RefObject<number> = useRef<number>(0);
+    const epidemiologyNumber = useSelector<StoreStateType, number>(store => store.investigation.epidemiologyNumber);
+    const currentInvestigationsValidations = useSelector<StoreStateType, (boolean | null)[]>(store => store.formsValidations[epidemiologyNumber]);
 
     const moveToNextTab = () => {
         setCurrentTab(nextTab.current as number);
