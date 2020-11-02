d0c28da203b761ea2b83b471fb29395461e1391b
MayaSasson <mayokit22@gmail.com>
2020-11-02 21:21:18 +0200

    using only one var

--- server/src/Utils/InterfacesIntegration.ts
+++ server/src/Utils/InterfacesIntegration.ts
@@ -2,10 +2,8 @@ import { httpRequest } from '../HTTPRequest';
 
 require('dotenv').config();
 
-const apiUrl = process.env.ENVIRONMENT === 'prod' ? process.env.INTERFACES_INTEGRATION_API_PROD : process.env.INTERFACES_INTEGRATION_API_TEST;
-
 export const sendSavedInvestigationToIntegration = (epidemiologyNumber: number) =>
-    httpRequest(apiUrl, 'POST', {
+    httpRequest(process.env.INTERFACES_INTEGRATION_API, 'POST', {
         variables: {
             idARR: [epidemiologyNumber]
         }
