2a78e457ee85799910ed5ca4f1f37d9fafd038b1
Roei Rom <roeirom6@gmail.com>
2020-11-04 13:43:27 +0200

    feat(InterfacesIntegration.ts): add logs

--- server/src/Utils/InterfacesIntegration.ts
+++ server/src/Utils/InterfacesIntegration.ts
@@ -4,9 +4,24 @@ import { Severity, Service } from '../Models/Logger/types';
 
 require('dotenv').config();
 
-export const sendSavedInvestigationToIntegration = (epidemiologyNumber: number, workflow: string, userId: string) =>
-{
+export const sendSavedInvestigationToIntegration = (epidemiologyNumber: number, workflow: string, userId: string) => {
+    logger.info({
+        service: Service.SERVER,
+        severity: Severity.LOW,
+        workflow,
+        step: 'checking if there is need to send the integration request',
+        user: userId,
+        investigation: epidemiologyNumber
+    });
     if (process.env.ENVIRONMENT === 'prod' || process.env.ENVIRONMENT === 'test' || (process.env.ENVIRONMENT === 'dev' && process.env.INTERFACES_INTEGRATION_API !== undefined)) {
+        logger.info({
+            service: Service.SERVER,
+            severity: Severity.LOW,
+            workflow,
+            step: 'sending the http request to integration API',
+            user: userId,
+            investigation: epidemiologyNumber
+        });
         httpRequest(process.env.INTERFACES_INTEGRATION_API, 'POST', {
             variables: {
                 idARR: [epidemiologyNumber]
