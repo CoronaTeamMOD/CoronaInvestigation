2e005b4b5dbaa76be15766ddcec08e8b80eecb55
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-11-08 15:36:31 +0200

    update db scripts

--- db scripts/check_duplicates_ids.sql
+++ db scripts/check_duplicates_ids.sql
@@ -1,7 +1,11 @@
+-- FUNCTION: public.check_duplicates_ids(character varying, integer)
+
+-- DROP FUNCTION public.check_duplicates_ids(character varying, integer);
+
 CREATE OR REPLACE FUNCTION public.check_duplicates_ids(
 	curridentificationnumber character varying,
 	investigationid integer,
-	interactedContactId integer)
+	interactedcontactid integer)
     RETURNS boolean
     LANGUAGE 'plpgsql'
 
@@ -16,7 +20,7 @@ declare
 				join contacted_person cp on p.id = cp.person_info join contact_event ce on ce.id = cp.contact_event
 				where ce.investigation_id = investigationId and  p.identification_number is not null
 				and p.identification_number = currIdentificationNumber
-				and p.id <> interactedContactId
+				and p.id <> interactedcontactid
 			) then
 				foundDuplicates = true;
 			else
