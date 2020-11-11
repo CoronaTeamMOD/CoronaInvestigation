872717235ea70ca3e2b47c54ba990e1c51525289
Itay Ben Moshe <71003295+ItayBenMoshe@users.noreply.github.com>
2020-11-11 13:12:11 +0200

    move onClick function to IconButton (#727)

--- client/src/components/App/Content/UsersManagement/UsersFilter/UsersFilter.tsx
+++ client/src/components/App/Content/UsersManagement/UsersFilter/UsersFilter.tsx
@@ -123,8 +123,8 @@ const UsersFilter:React.FC<Props> = ( props : Props ) => {
                     className={classes.autoComplete}
                 />
             </FormInput>
-            <IconButton>
-                <Close onClick={handleCloseFitler}/>
+            <IconButton onClick={handleCloseFitler}>
+                <Close/>
             </IconButton>
         </div>
     );
