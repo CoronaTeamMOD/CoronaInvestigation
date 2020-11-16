4416fe9dba6b8e8c0cd22aaa8f9689be08446a44
eladyashar <eladyashar1104@gmail.com>
2020-11-16 20:17:13 +0200

    fix(finish-without-contacts): bug fix from test branch (#766)
    * fix(finish-without-contacts): bug fix from test bracnh
    
    * fix(finish-without-contacts): remove unnecessry console.log
    
    Co-authored-by: Elad Yashar <eladyashar1104@gmail.com@gmail.com>

--- client/src/components/App/Content/InvestigationForm/InvestigationForm.tsx
+++ client/src/components/App/Content/InvestigationForm/InvestigationForm.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import StoreStateType from 'redux/storeStateType';
 import MuiAlert from '@material-ui/lab/Alert';
 import Snackbar from '@material-ui/core/Snackbar';
 
+import { setFormState } from 'redux/Form/formActionCreators';
 import PrimaryButton from 'commons/Buttons/PrimaryButton/PrimaryButton';
 import StartInvestigationDateVariables from 'models/StartInvestigationDateVariables';
 import {ExposureAndFlightsContextProvider, ExposureAndFlightsDetails,
@@ -9,7 +12,7 @@ import {ExposureAndFlightsContextProvider, ExposureAndFlightsDetails,
 
 import useStyles from './InvestigationFormStyles';
 import useInvestigationForm from './useInvestigationForm';
-import useTabManagement from './TabManagement/useTabManagement';
+import useTabManagement ,{ LAST_TAB_ID } from './TabManagement/useTabManagement';
 import InvestigationInfoBar from './InvestigationInfo/InvestigationInfoBar';
 import TabManagement from './TabManagement/TabManagement';
 import { StartInvestigationDateVariablesProvider } from './StartInvestiationDateVariables/StartInvestigationDateVariables';
@@ -26,6 +29,8 @@ const InvestigationForm: React.FC = (): JSX.Element => {
     const [symptomsStartDate, setSymptomsStartDate] = React.useState<Date>();
     const [hasSymptoms, setHasSymptoms] = React.useState<boolean>(false);
     const [endInvestigationDate, setEndInvestigationDate] = React.useState<Date>(new Date());
+    const [lastTabDisplayedId, setLastTabDisplayedId] = React.useState<number>(LAST_TAB_ID - 1);
+    const investigationId = useSelector<StoreStateType, number>((state) => state.investigation.epidemiologyNumber);
 
     const exposuresAndFlightsVariables: ExposureAndFlightsDetailsAndSet = React.useMemo(() => ({
         exposureAndFlightsData,
@@ -51,9 +56,13 @@ const InvestigationForm: React.FC = (): JSX.Element => {
     const {
         currentTab,
         setNextTab,
-        lastTabDisplayedId
-    } = useTabManagement();
+    } = useTabManagement({lastTabDisplayedId});
 
+    useEffect(() => {
+        setLastTabDisplayedId(areThereContacts ? LAST_TAB_ID : LAST_TAB_ID - 1);
+        !areThereContacts && setFormState(investigationId, LAST_TAB_ID, true);
+    }, [areThereContacts]);
+    
     return (
         <div className={classes.content}>
             <ExposureAndFlightsContextProvider value={exposuresAndFlightsVariables}>
