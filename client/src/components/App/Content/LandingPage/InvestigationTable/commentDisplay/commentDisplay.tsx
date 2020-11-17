4eb03fcb03e5d24fd805a4da598256b74515440c
sandymash <heyitssandym@gmail.com>
2020-11-17 19:06:29 +0200

    fix scroll fix :(

--- client/src/components/App/Content/LandingPage/InvestigationTable/commentDisplay/commentDisplay.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/commentDisplay/commentDisplay.tsx
@@ -8,6 +8,7 @@ const noCommentMessage = 'אין הערה';
 
 const CommentDisplay = ({comment}: Props) => {
     const [isTooltipOpen, setIsTooltipOpen] = React.useState<boolean>(false);
+
     const handleTooltipClose = () => setIsTooltipOpen(false);
     const handleTooltipToggle = (event:any) => {
         event.stopPropagation();
@@ -18,26 +19,25 @@ const CommentDisplay = ({comment}: Props) => {
 
     return (
         <ClickAwayListener onClickAway={handleTooltipClose}>
-            <Tooltip classes={{tooltip: classes.lightTooltip, popper: classes.popper}}
-                     PopperProps={{
-                         disablePortal: true,
-                         placement: 'bottom-start',
-                         modifiers: {
-                             offset: {
-                                 enabled: true,
-                                 offset: '20px, 0',
-                             },
-                         },
-                     }}
-                     onClose={handleTooltipClose}
-                     open={isTooltipOpen}
-                     disableHoverListener
-                     title={comment || noCommentMessage}>
-                <IconButton onClick={handleTooltipToggle}>
-                    <Comment color={comment ? 'primary' : 'disabled'}/>
-                </IconButton>
-            </Tooltip>
-        </ClickAwayListener>
+          <Tooltip classes={{tooltip:classes.lightTooltip, popper: classes.popper}}
+            PopperProps={{
+              disablePortal: true,
+              placement: 'bottom-start',
+              modifiers: {
+                offset: {
+                  enabled: true,
+                  offset: '20px, 0',
+                },
+              },
+            }}
+            onClose={handleTooltipClose}
+            open={isTooltipOpen}
+            title={comment || noCommentMessage}>
+                  <IconButton onClick={handleTooltipToggle}>
+                      <Comment color={comment ? 'primary' : 'disabled'}/>
+                  </IconButton>
+          </Tooltip>
+      </ClickAwayListener>
     )
 }
 
