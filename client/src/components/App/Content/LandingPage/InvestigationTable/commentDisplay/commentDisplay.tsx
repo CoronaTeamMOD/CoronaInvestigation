b2f697945471ac3042d737dce167d907b4666596
sandymash <42100027+sandymash@users.noreply.github.com>
2020-11-18 11:55:15 +0200

    fix(investigation table) (#790)
    - made comment tooltip disappear on scroll

--- client/src/components/App/Content/LandingPage/InvestigationTable/commentDisplay/commentDisplay.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/commentDisplay/commentDisplay.tsx
@@ -6,13 +6,19 @@ import useStyles from './commentDisplayStyles';
 
 const noCommentMessage = 'אין הערה';
 
-const CommentDisplay = ({comment}: Props) => {
+const CommentDisplay = ({comment, scrollableRef}: Props) => {
     const [isTooltipOpen, setIsTooltipOpen] = React.useState<boolean>(false);
+
     const handleTooltipClose = () => setIsTooltipOpen(false);
     const handleTooltipToggle = (event:any) => {
         event.stopPropagation();
         setIsTooltipOpen(isOpen => !isOpen);
     };
+
+    React.useEffect(() => {
+        scrollableRef?.addEventListener('scroll', handleTooltipClose)
+        return () => scrollableRef?.removeEventListener('scroll', handleTooltipClose)
+    }, []);
 
     const classes = useStyles();
 
@@ -31,12 +37,11 @@ const CommentDisplay = ({comment}: Props) => {
             }}
             onClose={handleTooltipClose}
             open={isTooltipOpen}
-            disableHoverListener
-            disableTouchListener
+                   disableHoverListener
             title={comment || noCommentMessage}>
-            <IconButton onClick={handleTooltipToggle}>
-            <Comment color={comment ? 'primary' : 'disabled'} />
-            </IconButton>
+                  <IconButton onClick={handleTooltipToggle}>
+                      <Comment color={comment ? 'primary' : 'disabled'}/>
+                  </IconButton>
           </Tooltip>
       </ClickAwayListener>
     )
@@ -44,6 +49,7 @@ const CommentDisplay = ({comment}: Props) => {
 
 interface Props {
     comment: string | null;
+    scrollableRef?: HTMLElement;
 }
 
 export default CommentDisplay;
