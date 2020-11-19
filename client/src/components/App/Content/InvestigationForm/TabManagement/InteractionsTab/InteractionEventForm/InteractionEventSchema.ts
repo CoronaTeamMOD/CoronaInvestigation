92b9983f6d681d32db6fb230b66707375bb25b02
Itay Ben Moshe <itaybenm@gmail.com>
2020-11-19 12:51:00 +0200

    fix(interactionSchema): undisabling externalization input when placeType is transportation and make it required

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionEventForm/InteractionEventSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionEventForm/InteractionEventSchema.ts
@@ -43,7 +43,8 @@ const interactionEventSchema = yup.object().shape({
        InteractionEventDialogFields.PLACE_DESCRIPTION], {
         is: (placeType, isUnknownTime, locationAddress, placeName, placeDescription) => {
           if (placeType === placeTypesCodesHierarchy.privateHouse.code ||
-              isUnknownTime || !(locationAddress && (placeName || placeDescription))) {
+              isUnknownTime || (placeType !== placeTypesCodesHierarchy.transportation.code
+                && !(locationAddress && (placeName || placeDescription)))) {
             return false;
           } else {
             return true;
