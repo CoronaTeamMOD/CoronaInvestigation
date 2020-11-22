26d31cdef0bc82e6841703e8eaff3f61a8d7bc0a
Roei Rom <roeirom6@gmail.com>
2020-11-22 11:57:00 +0200

    fix(redux/store.ts): making blacklist for complex object

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,17 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist'
-import storageSession from 'redux-persist/lib/storage/session'
+import storageSession from 'redux-persist/lib/storage/session';
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist'
 
 import reducers from './rootReducers';
+import StoreStateType from './storeStateType';
 
 
-const persistConfig = {
+const persistConfig: PersistConfig<StoreStateType> = {
     key: 'root',
     storage: storageSession,
+    // We used the blacklist prop in order to avoid saving maps on session storage
+    blacklist: ['cities', 'countries', 'contactTypes']
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers)
