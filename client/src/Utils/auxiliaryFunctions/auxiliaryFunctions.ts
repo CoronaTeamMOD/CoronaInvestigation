6877c355e6dd1e0da2c14b8b51688a11d37cc8fc
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-11-23 13:35:28 +0200

    fix(id and time validations in contact event creation):
    1042
    1044

--- client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
+++ client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
@@ -1,27 +1,31 @@
 const get = (obj: any, path: string, defaultValue = undefined) => {
     const travel = (regexp: RegExp) =>
-      path.split(regexp)
-          .filter(Boolean)
-          .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+        path.split(regexp)
+            .filter(Boolean)
+            .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
     const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
     return result === undefined || result === obj ? defaultValue : result;
 };
 
 const isIdValid = (id: string | null | undefined) => {
-  let sum = 0;
-  if (id?.length === 9) {
-    Array.from(id)?.forEach((digit: string, index: number) => {
-      let digitMul = parseInt(digit) * ((index % 2) + 1);
-      if (digitMul > 9) {
-        digitMul -= 9;
-      }
-      sum += digitMul;
-    })
-  }
-  return sum % 10 === 0 && sum > 0;
+    let sum = 0;
+    if (Boolean(id)) {
+        if (id?.length === 9) {
+            Array.from(id)?.forEach((digit: string, index: number) => {
+                let digitMul = parseInt(digit) * ((index % 2) + 1);
+                if (digitMul > 9) {
+                    digitMul -= 9;
+                }
+                sum += digitMul;
+            })
+        }
+        return sum % 10 === 0 && sum > 0;
+    } else {
+        return true;
+    }
 }
 
 export {
-  get,
-  isIdValid
+    get,
+    isIdValid
 } 
