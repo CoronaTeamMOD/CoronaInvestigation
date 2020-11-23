2ff253296cf572b6149371d6702e2534f6f1419f
Daniel Aizenberg <dani.aizenberg47@gmail.com>
2020-11-23 15:09:25 +0200

    remove conditional id

--- client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
+++ client/src/Utils/auxiliaryFunctions/auxiliaryFunctions.ts
@@ -10,8 +10,8 @@ const get = (obj: any, path: string, defaultValue = undefined) => {
 const isIdValid = (id: string | null | undefined) => {
     let sum = 0;
     if (Boolean(id)) {
-        if (id?.length === 9) {
-            Array.from(id)?.forEach((digit: string, index: number) => {
+        if (id.length === 9) {
+            Array.from(id).forEach((digit: string, index: number) => {
                 let digitMul = parseInt(digit) * ((index % 2) + 1);
                 if (digitMul > 9) {
                     digitMul -= 9;
