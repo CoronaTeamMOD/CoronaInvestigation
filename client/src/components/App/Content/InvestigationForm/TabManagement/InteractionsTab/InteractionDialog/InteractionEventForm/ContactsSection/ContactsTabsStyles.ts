e4b661b8b21b42f19ee0eaf9102e70c33c160951
sandymash <heyitssandym@gmail.com>
2020-11-26 13:51:04 +0200

    fix - stytles changes and files renamed

---
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsTabsStyles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    tab: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    tabs: {
+        display: 'flex'
+    }
+});
+
+export default useStyles;
