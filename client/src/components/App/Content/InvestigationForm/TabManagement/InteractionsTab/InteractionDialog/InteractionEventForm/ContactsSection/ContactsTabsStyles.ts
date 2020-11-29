3166cc00bce2fef60588281beb173de13a93f2ee
sandymash <heyitssandym@gmail.com>
2020-11-29 11:59:16 +0200

    fix(interaction form) - fixed styles - fixed button texts

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsTabsStyles.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsTabsStyles.ts
@@ -1,13 +1,32 @@
 import { makeStyles } from '@material-ui/styles';
+import {Theme} from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     tab: {
         flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        justifyContent: 'flex-start',
     },
     tabs: {
         display: 'flex'
+    },
+    icon: {
+        '& svg': {
+            paddingLeft: '0.5vw',
+            flip: false,
+        }
+    },
+    selected: {
+        color: theme.palette.primary.main,
+    },
+    collapse: {
+        '@media screen and (max-width: 1700px)': {
+            width: '84%'
+        },
+        width: '88%'
+    },
+    tabIndicator: {
+        width: '4px'
     }
-});
+}));
 
 export default useStyles;
