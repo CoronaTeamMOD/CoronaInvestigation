b1a28c932ecab18f8759187e383dcc143f3c826d
Itay Ben Moshe <itaybenm@gmail.com>
2020-12-06 20:09:14 +0200

    fix tooltip to display only when hover

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableFooter/FooterAction/FooterAction.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableFooter/FooterAction/FooterAction.tsx
@@ -10,17 +10,19 @@ const FooterAction: React.FC<Props> = ({ icon, displayTitle, onClick, disabled, 
     const classes = useStyle(isScreenWide)();
 
     return (
-        <Tooltip open={disabled} title={errorMessage}>
-            <IconButton
-                disabled={disabled}
-                onClick={onClick}
-                classes={{label: classes.button}}
-            >
-                {React.createElement(icon, { className: `${classes.icon} ${disabled ? classes.disabled: null}` })}
-                <Typography>{displayTitle}</Typography>
-            </IconButton>
+        <Tooltip title={errorMessage}>
+            <span>
+                <IconButton
+                    disabled={disabled}
+                    onClick={onClick}
+                    classes={{label: classes.button}}
+                >
+                    {React.createElement(icon, { className: `${classes.icon} ${disabled ? classes.disabled: null}` })}
+                    <Typography>{displayTitle}</Typography>
+                </IconButton>
+            </span>
         </Tooltip>
-    )
+    ) 
 };
 
 export default FooterAction;
