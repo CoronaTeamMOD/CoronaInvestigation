b0ad9c4326d7d524b551a69491bb8a3392bb7096
doronlnd84 <50694077+doronlnd84@users.noreply.github.com>
2020-12-07 11:39:41 +0200

    Fix clinicals margin (#905)
    * fix
    
    * fix
    
    Co-authored-by: Daniel Landau <daniellan@matrixbi.co.il>

--- client/src/components/App/Content/InvestigationForm/TabManagement/ClinicalDetails/ClinicalDetailsStyles.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ClinicalDetails/ClinicalDetailsStyles.ts
@@ -2,7 +2,7 @@ import {makeStyles} from '@material-ui/styles';
 
 export const useStyles = makeStyles({
     form: {
-        padding: '5vh',
+        padding: '10vh 5vw'
     },
     rowGridItem: {
         display: 'flex',
