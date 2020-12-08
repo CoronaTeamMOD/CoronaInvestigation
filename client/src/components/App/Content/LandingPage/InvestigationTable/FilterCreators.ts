f3b2713c8ad4c8f59c7910768f78811b6940829f
Beri <nevo.bonneh@gmail.com>
2020-12-08 14:21:25 +0200

    fix clearing investigations search bar

--- client/src/components/App/Content/LandingPage/InvestigationTable/FilterCreators.ts
+++ client/src/components/App/Content/LandingPage/InvestigationTable/FilterCreators.ts
@@ -3,30 +3,43 @@ import InvestigationsFilterByFields from 'models/enums/InvestigationsFilterByFie
 const filterCreators: { [T in InvestigationsFilterByFields]: ((values: any) => Exclude<any, void>) } = {
     [InvestigationsFilterByFields.STATUS]: (values: string[]) => {
         return values.length > 0 ?
-            {investigationStatus: {in: values}}
+            { investigationStatus: { in: values } }
             :
-            {investigationStatus: null};
+            { investigationStatus: null };
     },
     [InvestigationsFilterByFields.DESK_ID]: (values: number[]) => {
         return values.length > 0 ?
-            {deskId: {in: values}}
+            { deskId: { in: values } }
             :
-            {deskId: null};
+            { deskId: null };
     },
     [InvestigationsFilterByFields.FULL_NAME]: (values: string) => {
         return Boolean(values) ?
-            {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {fullName: {includes: values}}}} :
-            {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {fullName: {includes: ""}}}};
+            { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { fullName: { equalTo: values } } } } :
+            { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { fullName: { includes: "" } } } };
     },
     [InvestigationsFilterByFields.NUMERIC_PROPERTIES]: (values: string) => {
-      return Boolean(values) ?
-          {or: [{epidemiologyNumber: {equalTo: Number(values)}},
-              {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {primaryPhone: {includes: values}}}},
-              {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {identityNumber: {includes: values}}}}]} :
-          {or: [{epidemiologyNumber: {equalTo: 0}},
-                  {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {primaryPhone: {includes: ""}}}},
-                  {investigatedPatientByInvestigatedPatientId: {covidPatientByCovidPatient: {identityNumber: {includes: ""}}}}]}
+        return Boolean(values) ?
+            {
+                or: [{ epidemiologyNumber: { equalTo: Number(values) } },
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { primaryPhone: { includes: values } } } },
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { identityNumber: { includes: values } } } }]
+            } :
+            {
+                or: [{ epidemiologyNumber: { equalTo: 0 } },
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { primaryPhone: { includes: "" } } } },
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { identityNumber: { includes: "" } } } }]
+            }
+    },
+    [InvestigationsFilterByFields.DEFAULT_FILTER]: () => {
+        return {
+            investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { fullName: { includes: "" } } },
+            or: [
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { primaryPhone: { includes: "" } } } },
+                { investigatedPatientByInvestigatedPatientId: { covidPatientByCovidPatient: { identityNumber: { includes: "" } } } }]
+        }
     }
+
 };
 
 export default filterCreators;
--- client/src/models/enums/InvestigationsFilterByFields.ts
+++ client/src/models/enums/InvestigationsFilterByFields.ts
@@ -2,7 +2,8 @@ enum InvestigationsFilterByFields {
     STATUS = 'STATUS',
     DESK_ID = 'DESK_ID',
     FULL_NAME = 'FULL_NAME',
-    NUMERIC_PROPERTIES = 'NUMERIC_PROPERTIES'
+    NUMERIC_PROPERTIES = 'NUMERIC_PROPERTIES',
+    DEFAULT_FILTER = 'DEFAULT_FILTER'
 }
 
 export default InvestigationsFilterByFields;
