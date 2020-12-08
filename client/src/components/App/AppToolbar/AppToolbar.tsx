928b2096407686d7c5b4c78281ecfd5cb872d547
Naaman Tsur <naaman71@gmail.com>
2020-12-08 14:38:42 +0200

    fix() remove classes prop

--- client/src/components/App/AppToolbar/AppToolbar.tsx
+++ client/src/components/App/AppToolbar/AppToolbar.tsx
@@ -50,7 +50,7 @@ const AppToolbar: React.FC = (): JSX.Element => {
             navButtonsWhitelist.allowedUserTypes.includes(user.userType) &&
             navButtonsWhitelist.allowedRoutes.includes(location.pathname) &&
             <div className={classes.navButtons}>
-              <StatePersistentNavLink classes={classes} exact to={landingPageRoute}>
+              <StatePersistentNavLink exact to={landingPageRoute}>
                 <Home className={classes.menuIcon} />
                 <Typography className={classes.menuTypo}> עמוד הבית</Typography>
               </StatePersistentNavLink>
