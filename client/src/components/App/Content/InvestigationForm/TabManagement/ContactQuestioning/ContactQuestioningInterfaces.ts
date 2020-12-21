dab6ca629093c3026ed262a9f55ce740a478708c
y-golde <yon.golde@gmail.com>
2020-12-21 12:44:02 +0200

    removed 'any' from onsubmit

--- client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactQuestioningInterfaces.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactQuestioningInterfaces.ts
@@ -4,6 +4,11 @@ import InteractedContact from 'models/InteractedContact';
 import FamilyRelationship from 'models/FamilyRelationship';
 import InteractedContactFields from 'models/enums/InteractedContact';
 
+export interface FormInputs {
+    form : InteractedContact[]
+}
+
+
 export interface useContactQuestioningParameters {
     id: number;
     allContactedInteractions: InteractedContact[];
@@ -23,29 +28,9 @@ export interface useContactQuestioningOutcome {
         interactedContactId: number
     ) => boolean;
     checkAllContactsForDuplicateIds: () => boolean;
-    onSubmit: (data: any) => void;
+    onSubmit: (data: FormInputs) => void;
     parsePerson: (
         person: InteractedContact,
         index: number
     ) => InteractedContact;
-}
-
-export interface FormInputs {
-    [InteractedContactFields.CONTACT_STATUS]: number;
-    [InteractedContactFields.IDENTIFICATION_TYPE]: number;
-    [InteractedContactFields.IDENTIFICATION_NUMBER]: string;
-    [InteractedContactFields.BIRTH_DATE]: Date;
-    [InteractedContactFields.PHONE_NUMBER]: string;
-    [InteractedContactFields.ADDITIONAL_PHONE_NUMBER]: string;
-    [InteractedContactFields.FAMILY_RELATIONSHIP]: number;
-    [InteractedContactFields.RELATIONSHIP]: string;
-    [InteractedContactFields.CONTACTED_PERSON_CITY]: string;
-    [InteractedContactFields.DOES_NEED_HELP_IN_ISOLATION]: boolean;
-    [InteractedContactFields.DOES_NEED_ISOLATION]: boolean;
-    [InteractedContactFields.DOES_FEEL_GOOD]: boolean;
-    [InteractedContactFields.DOES_HAVE_BACKGROUND_DISEASES]: boolean;
-    [InteractedContactFields.DOES_LIVE_WITH_CONFIRMED]: boolean;
-    [InteractedContactFields.REPEATING_OCCURANCE_WITH_CONFIRMED]: boolean;
-    [InteractedContactFields.DOES_WORK_WITH_CROWD]: boolean;
-    [InteractedContactFields.OCCUPATION]: string;
 }
