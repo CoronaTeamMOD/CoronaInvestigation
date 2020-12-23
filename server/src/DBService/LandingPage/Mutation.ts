f938c20c9a93f1f3c2985b739d89ce2db52e9d41
Elad Yashar <eladyashar1104@gmail.com>
2020-12-23 10:33:52 +0200

    fix(group-actions-for-county): change desk

--- server/src/DBService/LandingPage/Mutation.ts
+++ server/src/DBService/LandingPage/Mutation.ts
@@ -9,8 +9,8 @@ export const CHANGE_DESK_ID = gql`
 `;
 
 export const UPDATE_DESK_BY_GROUP_ID = gql`
-mutation updateInvestigatorByGroupId($desk: Int!, $selectedGroups: [UUID!]!, $reason: String) {
-  updateDeskByGroupId(input: {desk: $desk, selectedGroups: $selectedGroups, reason: $reason}) {
+mutation updateInvestigatorByGroupId($desk: Int!, $selectedGroups: [UUID!]!, $userCounty: Int!, $reason: String) {
+  updateDeskByGroupId(input: {desk: $desk, selectedGroups: $selectedGroups, userCounty: $userCounty, reason: $reason}) {
     clientMutationId
   }
 }
