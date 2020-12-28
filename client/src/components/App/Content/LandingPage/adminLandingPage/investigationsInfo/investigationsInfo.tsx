a395d28a03f143073faad97d9a10a44657ab0d6b
Roei Rom <roeirom6@gmail.com>
2020-12-28 20:30:07 +0200

    feat(InvestigationInfo.tsx): move states and axios requests to the son instead of the father

--- client/src/components/App/Content/LandingPage/adminLandingPage/investigationsInfo/investigationsInfo.tsx
+++ client/src/components/App/Content/LandingPage/adminLandingPage/investigationsInfo/investigationsInfo.tsx
@@ -1,11 +1,13 @@
-import React, { useMemo } from 'react';
+import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import React, { useMemo, useState, useEffect } from 'react';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import { Card, CardContent, Grid, IconButton, Typography } from '@material-ui/core';
 
 import { landingPageRoute } from 'Utils/Routes/Routes';
 import InvestigationChart from 'models/InvestigationChart';
 import InvestigationStatistics from 'models/InvestigationStatistics';
+import { setIsLoading } from 'redux/IsLoading/isLoadingActionCreators';
 
 import useStyles from './investigationsInfoStyles';
 import InvestigationBarChart from './InvestigationBarChart/InvestigationBarChart';
@@ -30,10 +32,28 @@ const convertorsToGraph: { [T in keyof InvestigationStatistics]: Omit<Investigat
     }
 }
 
-const InvestigationsInfo: React.FC<Props> = ({ investigationsStatistics, investigationsCount }): JSX.Element => {
+const InvestigationsInfo: React.FC = (): JSX.Element => {
     const classes = useStyles();
 
     let history = useHistory();
+
+    const [allInvestigationsCount, setAllInvestigationsCount] = useState<number>(0);
+    const [investigationsStatistics, setInvestigationsStatistics] = useState<InvestigationStatistics>({
+        inProcessInvestigations: 0,
+        inactiveInvestigations: 0,
+        newInvestigations: 0,
+        unassignedInvestigations: 0
+    })
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.post<InvestigationStatistics & {allInvestigations: number}>('/landingPage/investigationStatistics')
+        .then((response) => {
+            const { data: {allInvestigations, ...statistics} } = response;
+            setAllInvestigationsCount(allInvestigations);
+            setInvestigationsStatistics(statistics);
+        }).finally(() => setIsLoading(false));
+    }, [])
 
     const investigationsGraphData = useMemo<InvestigationChart[]>(() => {
         const returnedArray:InvestigationChart[] = [];
@@ -65,7 +85,7 @@ const InvestigationsInfo: React.FC<Props> = ({ investigationsStatistics, investi
                         }
                     </Grid>
                     <Grid item xs={12} className={classes.investigationAmountContainer}>
-                        <Typography className={classes.investigationAmountText}><b>{investigationsCount}</b></Typography>
+                        <Typography className={classes.investigationAmountText}><b>{allInvestigationsCount}</b></Typography>
                         <Typography className={classes.allInvestigationsText}><b>חקירות בסך הכל</b></Typography>
                         <IconButton onClick={() => history.push(landingPageRoute)}>
                             <NavigateBeforeIcon className={classes.navigateIcon} />
@@ -75,11 +95,6 @@ const InvestigationsInfo: React.FC<Props> = ({ investigationsStatistics, investi
             </CardContent>
         </Card>
     )
-}
-
-interface Props {
-    investigationsCount: number;
-    investigationsStatistics: InvestigationStatistics;
 }
 
 export default InvestigationsInfo;
