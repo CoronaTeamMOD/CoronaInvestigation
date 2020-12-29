5d7badab91f5d324f1aba78a95c0ccf6bc502df4
Yonatan Golde <68457306+y-golde@users.noreply.github.com>
2020-12-29 11:04:49 +0200

    Fix/check authorization before investigation info (#1029)
    * added middleware to /staticInfo
    
    * cleanup
    
    * cleanup
    
    * added logging
    
    * renamed middleware
    
    * added return to error case
    
    * separated fetch to function
    
    * removed unnecessary EpidemiologyNumber param from /staticInfo
    
    * changed error handeling in fetchInvestigationCreatorDetails
    
    * moved http codes to constants
    
    * changed logger investigation ref
    
    * changed it to epidemiologyNumber constant
    
    * formatting and removing constant in fetch
    
    * added case where status is ok but result has errors

---
+++ server/src/middlewares/HandleInvestigationRequest.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from 'express';
+
+import logger from '../Logger/Logger';
+import UserType from '../Models/User/UserType';
+import { Severity } from '../Models/Logger/types';
+import { graphqlRequest } from '../GraphqlHTTPRequest';
+import { GET_INVESTIGATION_CREATOR } from '../DBService/InvestigationInfo/Query';
+
+const UNAUTHORIZED_STATUS = 401;
+const INTERNAL_SERVER_ERROR_STATUS = 500;
+
+export const handleInvestigationRequest = async (request: Request, response: Response, next: NextFunction) => {
+    const { user } = response.locals;
+    const epidemiologynumber = parseInt(response.locals.epidemiologynumber);
+
+    const investigationMiddlewareLogger = logger.setup({
+        workflow: 'InvestigationMiddleware',
+        investigation: epidemiologynumber,
+    });
+
+    if (isNaN(epidemiologynumber)) {
+        investigationMiddlewareLogger.info('client sent investigation request with no epidemiology number', Severity.MEDIUM);
+        return response.status(UNAUTHORIZED_STATUS).json({ error: 'no epidemiology number supplied' });
+    }
+
+    investigationMiddlewareLogger.info('getting current investigation details from DB', Severity.LOW);
+    const { investigationGroup, id , err} = await fetchInvestigationCreatorDetails(response.locals , epidemiologynumber);
+    if(err) {
+        investigationMiddlewareLogger.info(`error in requesting the graphql API: ${err}`, Severity.HIGH);
+        response.sendStatus(INTERNAL_SERVER_ERROR_STATUS);
+    }
+    investigationMiddlewareLogger.info('got investigation details', Severity.LOW);
+
+    if (user.userType === UserType.ADMIN || user.userType === UserType.SUPER_ADMIN) {
+        if (user.countyByInvestigationGroup.districtId === investigationGroup) {
+            investigationMiddlewareLogger.info('user is admin and investigation is in his county', Severity.LOW);
+            return next();
+        }
+        investigationMiddlewareLogger.info('user is admin but investigation is not in his county , returning 401', Severity.HIGH);
+        return response.status(UNAUTHORIZED_STATUS).json({ error: "unauthorized user - investigation is not in user's investigation group" });
+    } else {
+        if (user.id === id) {
+            investigationMiddlewareLogger.info('user is creator of the investigation', Severity.LOW);
+            return next();
+        }
+        investigationMiddlewareLogger.info('user is not admin nor creator of the investigation , returning 401', Severity.HIGH);
+        return response.status(UNAUTHORIZED_STATUS).json({ error: 'unauthorized user - user is not creator' });
+    }
+};
+
+const fetchInvestigationCreatorDetails = async (locals : any , epidemiologynumber : number) : Promise<{investigationGroup : number , id : string , err? : any}>=> {
+    return await graphqlRequest(GET_INVESTIGATION_CREATOR, locals, { epidemiologynumber })
+    .then((result: any) => {
+        return (result.data && !result.errors) 
+        ? result.data?.investigationByEpidemiologyNumber?.userByCreator
+        : { err : result.errors };
+    })
+    .catch((err) => {
+        return { err }
+    });
+}
