5c448c817b27a2dd4771fc9c85f3a5a360c0c3cd
y-golde <yon.golde@gmail.com>
2020-12-29 09:07:09 +0200

    moved http codes to constants

--- server/src/middlewares/HandleInvestigationRequest.ts
+++ server/src/middlewares/HandleInvestigationRequest.ts
@@ -6,6 +6,10 @@ import { Severity } from '../Models/Logger/types';
 import { graphqlRequest } from '../GraphqlHTTPRequest';
 import { GET_INVESTIGATION_CREATOR } from '../DBService/InvestigationInfo/Query';
 
+const UNAUTHORIZED_STATUS = 401;
+const INTERNAL_SERVER_ERROR_STATUS = 500;
+
+
 export const handleInvestigationRequest = async (request: Request, response: Response, next: NextFunction) => {
     const { user } = response.locals;
     const epidemiologynumber = parseInt(response.locals.epidemiologynumber);
@@ -17,14 +21,14 @@ export const handleInvestigationRequest = async (request: Request, response: Res
 
     if (isNaN(epidemiologynumber)) {
         InvestigationMiddlewareLogger.info('client sent investigation request with no epidemiology number', Severity.MEDIUM);
-        return response.status(401).json({ error: 'no epidemiology number supplied' });
+        return response.status(UNAUTHORIZED_STATUS).json({ error: 'no epidemiology number supplied' });
     }
 
     InvestigationMiddlewareLogger.info('getting current investigation details from DB', Severity.LOW);
     const { investigationGroup, id , err} = await fetchInvestigationCreatorDetails(response.locals , epidemiologynumber);
     if(err) {
         InvestigationMiddlewareLogger.info(`error in requesting the graphql API: ${err}`, Severity.HIGH);
-        response.sendStatus(500);
+        response.sendStatus(INTERNAL_SERVER_ERROR_STATUS);
     }
     InvestigationMiddlewareLogger.info('got investigation details', Severity.LOW);
 
@@ -34,14 +38,14 @@ export const handleInvestigationRequest = async (request: Request, response: Res
             return next();
         }
         InvestigationMiddlewareLogger.info('user is admin but investigation is not in his county , returning 401', Severity.HIGH);
-        return response.status(401).json({ error: "unauthorized user - investigation is not in user's investigation group" });
+        return response.status(UNAUTHORIZED_STATUS).json({ error: "unauthorized user - investigation is not in user's investigation group" });
     } else {
         if (user.id === id) {
             InvestigationMiddlewareLogger.info('user is creator of the investigation', Severity.LOW);
             return next();
         }
         InvestigationMiddlewareLogger.info('user is not admin nor creator of the investigation , returning 401', Severity.HIGH);
-        return response.status(401).json({ error: 'unauthorized user - user is not creator' });
+        return response.status(UNAUTHORIZED_STATUS).json({ error: 'unauthorized user - user is not creator' });
     }
 };
 
