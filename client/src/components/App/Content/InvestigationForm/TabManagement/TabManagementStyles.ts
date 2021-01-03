7cd08a44a215c66a902ac786441906dc5528ad20
Daniel Landau <daniellan@matrixbi.co.il>
2021-01-03 17:56:34 +0200

    remove redundant class

--- client/src/components/App/Content/InvestigationForm/TabManagement/TabManagementStyles.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/TabManagementStyles.ts
@@ -2,9 +2,6 @@ import { makeStyles } from '@material-ui/styles';
 import theme from 'styles/theme';
 
 const useStyles = makeStyles({
-    card: {
-        height: '70vh',
-    },
     displayedTab: {
         height: '90%',
         overflow: 'auto'
