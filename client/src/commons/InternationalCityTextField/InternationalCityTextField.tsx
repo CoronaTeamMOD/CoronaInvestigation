ba69887bb7ecb8923506c543eb5a5400ed63e60e
MayaSasson <mayokit22@gmail.com>
2021-01-04 14:11:35 +0200

    fix: using alphabet text field

--- client/src/commons/InternationalCityTextField/InternationalCityTextField.tsx
+++ client/src/commons/InternationalCityTextField/InternationalCityTextField.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import * as yup from 'yup';
+
+import AlphabetWithDashTextField from 'commons/AlphabetWithDashTextField/AlphabetWithDashTextField';
 
 import InternationalCityTextFieldType from './InternationalCityTextFieldTypes';
-import TypePreventiveTextField from '../TypingPreventionTextField/TypingPreventionTextField';
-
-const errorMessage = 'השדה יכול להכיל אותיות, רווחים ומקפים';
-const maxLengthErrorMessage = 'השדה יכול להכיל 50 אותיות בלבד';
-
-const internationalCity = yup
-  .string()
-  .matches(/^[a-zA-Z\u0590-\u05fe-\s]*$/, errorMessage)
-  .max(50, maxLengthErrorMessage);
 
 const InternationalCityTextField: InternationalCityTextFieldType = (props) => {
   return (
-    <TypePreventiveTextField
+    <AlphabetWithDashTextField
         {...props}
         value={props.value || ''}
-        validationSchema={internationalCity}
     />
   );
 };
