e4ef499c412898cd7731113ce31a369cedb4f3d8
y-golde <yon.golde@gmail.com>
2021-01-05 14:35:54 +0200

    changed popper design

--- client/src/components/App/Content/LandingPage/InvestigationTable/SelfInvestigationIcon/useSelfInvestigationIcon.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/SelfInvestigationIcon/useSelfInvestigationIcon.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Grid } from '@material-ui/core';
 import { AssignmentInd, AssignmentTurnedIn, AssignmentReturned } from '@material-ui/icons';
 
 import SelfInvestigaionStatuses , {getInvestigationStatusName} from 'models/enums/selfInvestigationStatuses';
 
 import useStyles from './selfInvestigationIconStyles';
+
 
 const contactTitle = 'תחקור עצמי';
 const lastUpdatedTime = 'זמן עדכון אחרון ע"י החולה';
@@ -35,7 +37,10 @@ const useSelfInvestigationIcon = (props: Props) => {
         return (
             <div>
                 <div><b>{lastUpdatedTime} : </b>{date.toLocaleDateString()}</div>
-                <div><b>{statusTitle} : </b>{getInvestigationStatusName(status)}</div>
+                <Grid item container>
+                    <Grid item xs={6} style={{textAlign : 'right'}}><b>{statusTitle} : </b></Grid>
+                    <Grid item xs={6} style={{textAlign : 'left'}}>{getInvestigationStatusName(status)}</Grid>
+                </Grid>
             </div>
         )
     }
