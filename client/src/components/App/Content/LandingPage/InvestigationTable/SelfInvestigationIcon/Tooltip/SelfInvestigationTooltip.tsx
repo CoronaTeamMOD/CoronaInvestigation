ab8844d39bc32806d07a31365e81da742b9b2765
eladyashar <eladyashar1104@gmail.com>
2021-01-06 18:29:55 +0200

    fix(time-format-self-investigation): changed to 24 houres (#1107)

--- client/src/components/App/Content/LandingPage/InvestigationTable/SelfInvestigationIcon/Tooltip/SelfInvestigationTooltip.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/SelfInvestigationIcon/Tooltip/SelfInvestigationTooltip.tsx
@@ -17,7 +17,7 @@ const SelfInvestigationTooltip = (props: Props) => {
 
     return (
         <div>
-            <div><b>{lastUpdatedTime} : </b>{format(date, "hh:mm:ss dd/MM/yy")}</div>
+            <div><b>{lastUpdatedTime} : </b>{format(date, "HH:mm:ss dd/MM/yy")}</div>
             <Grid item container>
                 <Grid item xs={6} style={{textAlign : 'right'}}><b>{statusTitle} : </b></Grid>
                 <Grid item xs={6} style={{textAlign : 'left'}}>{Boolean(statusNames[status]) ? statusNames[status] : 'לא ידוע' }</Grid>
