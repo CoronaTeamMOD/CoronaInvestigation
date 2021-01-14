fa28d2cfa9c97e46b1ba7b2c6227d3610941925f
Roy Maniv <roymaniv@gmail.com>
2021-01-14 15:26:49 +0200

    fix(LastUpdateMessage): fixed update message refresh rate

---
+++ client/src/models/enums/Times.ts
@@ -0,0 +1,11 @@
+enum Times {
+    now = 'פחות מדקה',
+    seconds = 'שניות',
+    minutes = 'דקות',
+    hours = 'שעות',
+    days = 'ימים',
+    months = 'חודשים',
+    years = 'שנים'
+};
+
+export default Times;
