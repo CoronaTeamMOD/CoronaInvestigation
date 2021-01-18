5a255578552f061b7dab1e3c1b0e4ba0db0f3fbd
Noam6421 <noam6421@gmail.com>
2021-01-18 10:33:47 +0200

    Fix styles, spacing, labels

--- client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightFormStyles.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightFormStyles.tsx
@@ -1,8 +1,12 @@
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
-    flightInputDate: {
-        marginRight: '6vw',
+    inputRow: {
+        alignItems: 'center',
+        minWidth: '45vw',
+        '&.MuiGrid-item': {
+            paddingLeft: 0
+        }
     },
 });
 
