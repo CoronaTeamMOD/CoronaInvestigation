19d620fdec4eb138d3d68c7aaa0203f2047c6e61
Roei Rom <roeirom6@gmail.com>
2021-01-20 19:00:06 +0200

    fix(AddressForm): fix the address form in personal info tab

--- client/src/components/App/Content/InvestigationForm/TabManagement/NewPersonalInfoTab/InstitutionComponent/InstitutionComponent.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/NewPersonalInfoTab/InstitutionComponent/InstitutionComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import { Controller, useFormContext } from 'react-hook-form';
@@ -11,6 +11,14 @@ const InstitutionComponent: React.FC<Props> = ({ fieldName, placeholder, options
 
     const methods = useFormContext<PersonalInfoTabState>();
 
+    const selectedInstitution = methods.watch(fieldName);
+
+    const inputValue = options.find(option => option.id === selectedInstitution)?.subOccupation;
+
+    // const inputValue = useMemo<string | undefined>(() => {
+    //     return options.find(option => option.id === selectedInstitution)?.subOccupation;
+    // }, [selectedInstitution])
+
     return (
         <Controller
             name={fieldName}
@@ -18,11 +26,20 @@ const InstitutionComponent: React.FC<Props> = ({ fieldName, placeholder, options
             render={(props) => (
                 <Autocomplete
                     options={options}
-                    getOptionLabel={(option) => option.subOccupation + (option.street ? ('/' + option.street) : '')}
-                    value={props.value?.id || ''}
+                    defaultValue={props.value}
+                    getOptionLabel={(option) => {
+                        console.log('option: ',option, '\noptions: ', options)
+                        return option.subOccupation ? (option.subOccupation + (option.street ? ('/' + option.street) : '')) : inputValue as string;
+                    }}
+                    getOptionSelected={(option, value) => {
+                        return option.id === value;
+                    }}
+                    value={props.value || ''}
                     onChange={(event, newValue) => {
                         props.onChange(newValue ? newValue.id : '')
                     }}
+                    inputValue={inputValue}
+                    loading={options.length === 0}
                     renderInput={(params) =>
                         <TextField
                             {...params}
