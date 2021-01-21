ae549c53811f34d9d403dfa4082fafe95d35054a
BARNA-LAP-HZ\barna <bar.nahum8@gmail.com>
2021-01-21 15:37:57 +0200

    change text

--- client/src/models/enums/FilterRulesDescription.ts
+++ client/src/models/enums/FilterRulesDescription.ts
@@ -4,7 +4,7 @@ enum FilterRulesDescription {
     UNASSIGNED = 'לא משויכות',
     INACTIVE = 'מוקצות לחוקרים לא פעילים',
     UNALLOCATED = 'ממתינות להקצאה',
-    UNUSUAL_COMPLETED_NO_CONTACT = 'הושלמו עם 0 מגעים',
+    UNUSUAL_COMPLETED_NO_CONTACT = 'הושלמו ללא מגעים',
     UNUSUAL_IN_PROCESS = 'בטיפול מעל 4 שעות',
     TRANSFER_REQUEST = 'ממתינות להעברה',
     WAITING_FOR_DETAILS = 'ממתינות להשלמת פרטים',
