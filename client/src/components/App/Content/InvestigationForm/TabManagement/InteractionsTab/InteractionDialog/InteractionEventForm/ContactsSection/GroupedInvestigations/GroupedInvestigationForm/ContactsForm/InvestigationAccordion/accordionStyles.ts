f5d22693e5b5f53e4de893a4a679808e520db306
y-golde <yon.golde@gmail.com>
2021-01-24 10:56:52 +0200

    added accordion padding

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/accordionStyles.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/accordionStyles.ts
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(() => ({
     accordion: {
+        margin: '0.5vh 0',
         border: '1px solid black',
         boxShadow: 'none',
         borderRadius: '1rem'
