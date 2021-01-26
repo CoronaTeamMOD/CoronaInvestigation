0520818350e55579e23390ac5fe28717b7a95cb6
y-golde <yon.golde@gmail.com>
2021-01-26 08:25:19 +0200

    removed family contacts

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/AccordionContent/ContactsTable/TableRows/TableRows.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/AccordionContent/ContactsTable/TableRows/TableRows.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { TableBody , TableRow , TableCell, Checkbox } from '@material-ui/core';
 
 import formatDate from 'Utils/DateUtils/formatDate';
+import useInvolvedContact from 'Utils/vendor/useInvolvedContact';
 import ContactEvent from 'models/GroupedInvestigationContacts/ContactEvent';
 
 import useStyles from './tableRowsStyles';
@@ -10,7 +11,8 @@ import useTableRows from './useTableRows';
 const TableRows = (props: Props) => {
     const { events , selectedRows , setSelectedRows, existingIds} = props;
     const classes = useStyles();
-    
+    const { isInvolvedThroughFamily } = useInvolvedContact();
+
     const { handleCheckboxToggle } = useTableRows({selectedRows , setSelectedRows});
 
     return (
@@ -20,47 +22,52 @@ const TableRows = (props: Props) => {
                     const { nodes } = event.contactedPeopleByContactEvent;
 
                     return nodes.map((person) => {
-                        const {id} = person;
+                        const {involvedContactByInvolvedContactId} = person;
+                        const isFamily = involvedContactByInvolvedContactId && isInvolvedThroughFamily(involvedContactByInvolvedContactId.involvementReason);
                         
-                        const {
-                            firstName,
-                            lastName,
-                            identificationType,
-                            identificationNumber,
-                            birthDate,
-                            phoneNumber,
-                            additionalPhoneNumber
-                        } = person.personByPersonInfo;
-                        const isolationCity = person.addressByIsolationAddress?.cityByCity?.displayName;
+                        if(!isFamily) {
+                            const {id} = person;
+                            
+                            const {
+                                firstName,
+                                lastName,
+                                identificationType,
+                                identificationNumber,
+                                birthDate,
+                                phoneNumber,
+                                additionalPhoneNumber
+                            } = person.personByPersonInfo;
+                            const isolationCity = person.addressByIsolationAddress?.cityByCity?.displayName;
 
-                        const isRowSelected = selectedRows.indexOf(id) !== -1;
-                        const isRowDisabled = existingIds.indexOf(identificationNumber) !== -1;
-                        const rowClass = isRowDisabled 
-                                            ? classes.disabled
-                                            : isRowSelected 
-                                                ? classes.selected
-                                                : ''
+                            const isRowSelected = selectedRows.indexOf(id) !== -1;
+                            const isRowDisabled = existingIds.indexOf(identificationNumber) !== -1;
+                            const rowClass = isRowDisabled 
+                                                ? classes.disabled
+                                                : isRowSelected 
+                                                    ? classes.selected
+                                                    : ''
 
-                        return (
-                            <TableRow key={id} className={rowClass}>
-                                <TableCell>
-                                    <Checkbox
-                                        disabled={isRowDisabled}
-                                        color='primary'
-                                        checked={isRowSelected}
-                                        onClick={() => handleCheckboxToggle(id)}
-                                    />
-                                </TableCell>
-                                <TableCell>{firstName}</TableCell>
-                                <TableCell>{lastName}</TableCell>
-                                <TableCell>{identificationType}</TableCell>
-                                <TableCell>{identificationNumber}</TableCell>
-                                <TableCell>{formatDate(new Date(birthDate))}</TableCell>
-                                <TableCell>{phoneNumber}</TableCell>
-                                <TableCell>{additionalPhoneNumber}</TableCell>
-                                <TableCell>{isolationCity}</TableCell>
-                            </TableRow>
-                        )
+                            return (
+                                <TableRow key={id} className={rowClass}>
+                                    <TableCell>
+                                        <Checkbox
+                                            disabled={isRowDisabled}
+                                            color='primary'
+                                            checked={isRowSelected}
+                                            onClick={() => handleCheckboxToggle(id)}
+                                        />
+                                    </TableCell>
+                                    <TableCell>{firstName}</TableCell>
+                                    <TableCell>{lastName}</TableCell>
+                                    <TableCell>{identificationType}</TableCell>
+                                    <TableCell>{identificationNumber}</TableCell>
+                                    <TableCell>{formatDate(new Date(birthDate))}</TableCell>
+                                    <TableCell>{phoneNumber}</TableCell>
+                                    <TableCell>{additionalPhoneNumber}</TableCell>
+                                    <TableCell>{isolationCity}</TableCell>
+                                </TableRow>
+                            )
+                        }
                     })
                 })
             }
