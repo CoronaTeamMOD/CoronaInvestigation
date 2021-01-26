5712543bd9a494103abaacd7df60c58c86786cfc
Noam6421 <noam6421@gmail.com>
2021-01-26 17:04:24 +0200

    added trash icon to delte flight

--- client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightsForm.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightsForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import { Grid, IconButton } from '@material-ui/core';
 import { Controller , useFormContext } from 'react-hook-form';
 
 import useFormStyles from 'styles/formStyles';
@@ -38,126 +39,152 @@ const FlightsForm = (props: any) => {
 
   return (
 		<Grid className={formClasses.form} container justify='flex-start'>
-			<FormRowWithInput testId='flightStartingPoint' fieldName='מוצא:'>
-				<AirportInput
-					country={exposureAndFlightsData[fieldsNames.originCountry]}
-					countryFieldName={fieldsNames.originCountry}
-					city={exposureAndFlightsData[fieldsNames.originCity]}
-					cityFieldName={fieldsNames.originCity}
-					airport={exposureAndFlightsData[fieldsNames.originAirport]}
-					airportFieldName={fieldsNames.originAirport}
-					index={index}
-					handleChangeExposureDataAndFlightsField={handleChangeExposureDataAndFlightsField}
-				/>
-			</FormRowWithInput>
-
-			<FormRowWithInput testId='flightDestination' fieldName='יעד:'>
-				<AirportInput
-					country={exposureAndFlightsData[fieldsNames.destinationCountry]}
-					countryFieldName={fieldsNames.destinationCountry}
-					city={exposureAndFlightsData[fieldsNames.destinationCity]}
-					cityFieldName={fieldsNames.destinationCity}
-					airport={exposureAndFlightsData[fieldsNames.destinationAirport]}
-					airportFieldName={fieldsNames.destinationAirport}
-					index={index}
-					handleChangeExposureDataAndFlightsField={handleChangeExposureDataAndFlightsField}
-				/>
-			</FormRowWithInput>
-
-			<FormRowWithInput fieldName='תאריך טיסה:'>
-				<Grid className={classes.inputRow} item container xs={9} justify='flex-start' alignItems='center' spacing={1}>
-					<Grid item>
-						<Controller
-							control={control}
-							name={`exposures[${index}].${fieldsNames.flightStartDate}`}
-							defaultValue={exposureAndFlightsData[fieldsNames.flightStartDate]}
-							render={(props) => {
-								return (
-									<DatePick
-										{...props}
-										maxDateMessage={''}
-										invalidDateMessage={''}
-										maxDate={new Date()}
-										testId='flightFromDate'
-										labelText={getDateLabel(startDateError, true)}
-										error={Boolean(startDateError)}
-										onChange={(newDate: Date) => {
-											props.onChange(newDate);
-											handleChangeExposureDataAndFlightsField(fieldsNames.flightStartDate, newDate);
-										}}
-									/>
-								);
-							}}
+		
+			<Grid container justify='space-between' xs={12}>
+                <Grid item xs={11}>
+					<FormRowWithInput testId='flightStartingPoint' fieldName='מוצא:'>
+						<AirportInput
+							country={exposureAndFlightsData[fieldsNames.originCountry]}
+							countryFieldName={fieldsNames.originCountry}
+							city={exposureAndFlightsData[fieldsNames.originCity]}
+							cityFieldName={fieldsNames.originCity}
+							airport={exposureAndFlightsData[fieldsNames.originAirport]}
+							airportFieldName={fieldsNames.originAirport}
+							index={index}
+							handleChangeExposureDataAndFlightsField={handleChangeExposureDataAndFlightsField}
 						/>
-					</Grid>
-					<Grid item>
-						<Controller
-							control={control}
-							name={`exposures[${index}].${fieldsNames.flightEndDate}`}
-							defaultValue={exposureAndFlightsData[fieldsNames.flightEndDate]}
-							render={(props) => {
-								return (
-									<DatePick
-										{...props}
-										maxDateMessage={''}
-										invalidDateMessage={''}
-										maxDate={new Date()}
-										testId='flightToDate'
-										labelText={getDateLabel(endDateError, false)}
-										error={Boolean(endDateError)}
-										onChange={(newDate: Date) => {
-											props.onChange(newDate);
-											handleChangeExposureDataAndFlightsField(fieldsNames.flightEndDate, newDate);
-										}}
-									/>
-								);
-							}}
+					</FormRowWithInput>
+                </Grid>
+                <Grid item xs={1} alignItems='center' justify='flex-start'>
+					<IconButton onClick={()=>{}}>
+							<Delete />
+					</IconButton>
+                </Grid>
+            </Grid>
+
+			<Grid container justify='space-between' xs={12}>
+                <Grid item xs={11} className={classes.deleteRow}>
+					<FormRowWithInput testId='flightDestination' fieldName='יעד:'>
+						<AirportInput
+							country={exposureAndFlightsData[fieldsNames.destinationCountry]}
+							countryFieldName={fieldsNames.destinationCountry}
+							city={exposureAndFlightsData[fieldsNames.destinationCity]}
+							cityFieldName={fieldsNames.destinationCity}
+							airport={exposureAndFlightsData[fieldsNames.destinationAirport]}
+							airportFieldName={fieldsNames.destinationAirport}
+							index={index}
+							handleChangeExposureDataAndFlightsField={handleChangeExposureDataAndFlightsField}
 						/>
-					</Grid>
+					</FormRowWithInput>
 				</Grid>
-			</FormRowWithInput>
+			</Grid>
+			
+			<Grid container justify='space-between' xs={12}>
+                <Grid item xs={11} className={classes.deleteRow}>
+					<FormRowWithInput fieldName='תאריך טיסה:'>
+						<Grid className={classes.inputRow} item container xs={9} justify='flex-start' alignItems='center' spacing={1}>
+							<Grid item>
+								<Controller
+									control={control}
+									name={`exposures[${index}].${fieldsNames.flightStartDate}`}
+									defaultValue={exposureAndFlightsData[fieldsNames.flightStartDate]}
+									render={(props) => {
+										return (
+											<DatePick
+												{...props}
+												maxDateMessage={''}
+												invalidDateMessage={''}
+												maxDate={new Date()}
+												testId='flightFromDate'
+												labelText={getDateLabel(startDateError, true)}
+												error={Boolean(startDateError)}
+												onChange={(newDate: Date) => {
+													props.onChange(newDate);
+													handleChangeExposureDataAndFlightsField(fieldsNames.flightStartDate, newDate);
+												}}
+											/>
+										);
+									}}
+								/>
+							</Grid>
+							<Grid item>
+								<Controller
+									control={control}
+									name={`exposures[${index}].${fieldsNames.flightEndDate}`}
+									defaultValue={exposureAndFlightsData[fieldsNames.flightEndDate]}
+									render={(props) => {
+										return (
+											<DatePick
+												{...props}
+												maxDateMessage={''}
+												invalidDateMessage={''}
+												maxDate={new Date()}
+												testId='flightToDate'
+												labelText={getDateLabel(endDateError, false)}
+												error={Boolean(endDateError)}
+												onChange={(newDate: Date) => {
+													props.onChange(newDate);
+													handleChangeExposureDataAndFlightsField(fieldsNames.flightEndDate, newDate);
+												}}
+											/>
+										);
+									}}
+								/>
+							</Grid>
+						</Grid>
+					</FormRowWithInput>
+				</Grid>
+			</Grid>
 
-			<FormRowWithInput fieldName='חברת תעופה:'>
-				<Controller
-					control={control}
-					name={`exposures[${index}].${fieldsNames.airline}`}
-					defaultValue={exposureAndFlightsData[fieldsNames.airline]}
-					render={(props) => {
-						return (
-							<AirelineTextField
-								{...props}
-								testId='airlineCompany'
-								onChange={(value) => {
-									props.onChange(value);
-								}}
-								placeholder='הזן חברת תעופה'
-							/>
-						);
-					}}
-				/>
-			</FormRowWithInput>
+			<Grid container justify='space-between' xs={12}>
+                <Grid item xs={11} className={classes.deleteRow}>
+					<FormRowWithInput fieldName='חברת תעופה:'>
+						<Controller
+							control={control}
+							name={`exposures[${index}].${fieldsNames.airline}`}
+							defaultValue={exposureAndFlightsData[fieldsNames.airline]}
+							render={(props) => {
+								return (
+									<AirelineTextField
+										{...props}
+										testId='airlineCompany'
+										onChange={(value) => {
+											props.onChange(value);
+										}}
+										placeholder='הזן חברת תעופה'
+									/>
+								);
+							}}
+						/>
+					</FormRowWithInput>
+				</Grid>
+			</Grid>
 
-			<FormRowWithInput fieldName='מספר טיסה:'>
-				<Controller
-					control={control}
-					name={`exposures[${index}].${fieldsNames.flightNumber}`}
-					defaultValue={exposureAndFlightsData[fieldsNames.flightNumber]}
-					render={(props) => {
-						return (
-							<FlightNumberTextField
-								{...props}
-								testId='airlineNumber'
-								onChange={(value) => {
-									props.onChange(value);
-                                    handleChangeExposureDataAndFlightsField(fieldsNames.flightNumber, value);
-								}}
-								label='מספר טיסה*'
-								placeholder='הזן מספר טיסה'
-							/>
-						);
-					}}
-				/>
-			</FormRowWithInput>
+			<Grid container justify='space-between' xs={12}>
+                <Grid item xs={11} className={classes.deleteRow}>
+					<FormRowWithInput fieldName='מספר טיסה:'>
+						<Controller
+							control={control}
+							name={`exposures[${index}].${fieldsNames.flightNumber}`}
+							defaultValue={exposureAndFlightsData[fieldsNames.flightNumber]}
+							render={(props) => {
+								return (
+									<FlightNumberTextField
+										{...props}
+										testId='airlineNumber'
+										onChange={(value) => {
+											props.onChange(value);
+											handleChangeExposureDataAndFlightsField(fieldsNames.flightNumber, value);
+										}}
+										label='מספר טיסה*'
+										placeholder='הזן מספר טיסה'
+									/>
+								);
+							}}
+						/>
+					</FormRowWithInput>
+				</Grid>
+			</Grid>
 		</Grid>
   );
 };
