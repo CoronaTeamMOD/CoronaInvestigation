53bc56d92131c7d5ac532f240891962a4991155e
chen friedman <31721666+chenFriedman@users.noreply.github.com>
2021-01-31 19:51:40 +0200

    Fix/city is required in interrogation of contacts (#1258)
    * fix- city is required
    
    * cr fixes
    
    * fix - required city error changed to be part of contact questioning clinical schema + enable save with city error
    
    * fix - disable error for complited Interrogation of contacts
    
    * pr fixes
    
    * space fixes
    
    * using ContactStatusCodes enum

--- client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 import { ALPHANUMERIC_TEXT_REGEX } from 'commons/Regex/Regex';
 import InteractedContactFields from 'models/enums/InteractedContact';
+import ContactStatusCodes from 'models/enums/ContactStatusCodes';
 
 export const contactQuestioningClinical = {
     [InteractedContactFields.FAMILY_RELATIONSHIP]: yup.number().nullable(),
@@ -9,4 +10,12 @@ export const contactQuestioningClinical = {
     [InteractedContactFields.DOES_NEED_HELP_IN_ISOLATION]: yup
         .boolean()
         .nullable(),
+    [InteractedContactFields.ISOLATION_ADDRESS]: yup.object()
+    .when(InteractedContactFields.CONTACT_STATUS, {
+        is: ContactStatusCodes.COMPLETED,
+        then: yup.object().nullable(),
+        otherwise: yup.object().shape({
+                 'city': yup.string().nullable().required('שגיאה: שדה חובה')
+             })
+    })
 };
