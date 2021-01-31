37856a342d1168e2716525b46f1071e4cac3893e
y-golde <yon.golde@gmail.com>
2021-01-31 12:33:29 +0200

    moved all of the components to InteractionGridItem

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/ContactDateCard/InteractionCard/PlacesAdditionalGrids/HospitalEventGrid.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/ContactDateCard/InteractionCard/PlacesAdditionalGrids/HospitalEventGrid.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import {Grid, Typography} from '@material-ui/core';
 
-import useFormStyles from 'styles/formStyles';
-import FormInput from 'commons/FormInput/FormInput';
 import InteractionEventDialogData from 'models/Contexts/InteractionEventDialogData';
 
 import AddressGrid from '../AddressGrid/AddressGrid';
+import InteractionGridItem from './InteractionGridItem';
 import BusinessContactGrid from '../BusinessContactGrid/BusinessContactGrid';
 
 const HospitalEventGrid : React.FC<Props> = (props: Props) : JSX.Element => {
-
     const { interaction } = props;
-
-    const formClasses = useFormStyles();
 
     return (
         <>
-            <Grid container justify='flex-start' className={formClasses.formRow}>
-                <FormInput xs={6} fieldName='שם בית חולים'>
-                    <Typography variant='caption'>
-                        {interaction.placeName}
-                    </Typography>
-                </FormInput>
-            
-                <FormInput xs={3} fieldName='מחלקה'>
-                    <Typography variant='caption'>
-                        {interaction.hospitalDepartment}
-                    </Typography>
-                </FormInput>
-            </Grid>
+            <InteractionGridItem 
+                containerSize={6}
+                labelLengthMD={3}
+                labelLengthLG={2}
+                title='שם בית חולים'
+                content={interaction.placeName}
+            />
+            <InteractionGridItem 
+                containerSize={6}
+                labelLengthMD={3}
+                labelLengthLG={2}
+                title='מחלקה'
+                content={interaction.hospitalDepartment}
+            />
             <AddressGrid interaction={interaction} />
             <BusinessContactGrid interaction={interaction}/>
         </>
