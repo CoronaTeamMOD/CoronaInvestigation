0046047edc36a079747cc67d2d77ac1ff428371d
Noam6421 <noam6421@gmail.com>
2021-02-09 09:14:59 +0200

    .

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorAllocationDialog.test.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorAllocationDialog.test.tsx
@@ -3,6 +3,9 @@ import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 
 import MockFormProvider from 'Utils/Testing/MockFormProvider';
+import mockSelectors from 'Utils/Testing/GroupedInvestigationForm/mockSelectors';
+import { testEvents, testPersonalDetails } from 'Utils/Testing/GroupedInvestigationForm/state';
+import TypePreventiveTextField from 'commons/TypingPreventionTextField/TypingPreventionTextField';
 
 import InvestigatorAllocationDialog from './InvestigatorAllocationDialog';
 
@@ -17,6 +20,7 @@ const contentProps = {
 };
 
 describe('<InvestigatorAllocationDialog />', () => {
+    mockSelectors();
     let wrapper = mount(
         <MockFormProvider >
             <InvestigatorAllocationDialog 
@@ -29,9 +33,89 @@ describe('<InvestigatorAllocationDialog />', () => {
         expect(wrapper.exists()).toBeTruthy();
     })
 
+    it('renders Buttons' , () => {
+        const searchBar = wrapper.find(TableSearchBar);
+
+        expect(searchBar.exists()).toBeTruthy();
+    })
+
     it('renders InvestigatorsTable' , () => {
         const investigatorsTable = wrapper.find(InvestigatorsTable);
 
         expect(investigatorsTable.exists()).toBeTruthy();
     })
+
+    it('renders SelectedRowsMessage' , () => {
+        const selectedRowsMessage = wrapper.find(SelectedRowsMessage);
+
+        expect(selectedRowsMessage.exists()).toBeTruthy();
+    });
+
+    it('triggers search on input', () => {
+        const query = 'מוטי';
+        act(() => {
+            wrapper.find(TypePreventiveTextField).at(0).props().onChange(query);
+        })
+        wrapper.update();
+
+        expect(wrapper.find(TypePreventiveTextField).at(0).props().value).toBe(query);
+        const tableRow = wrapper.find('tr#person-row-666');
+
+        expect(tableRow.exists()).toBeTruthy();
+        expect(tableRow).toHaveLength(1);
+
+        const noResultsMessage = wrapper.find('h5#errorMessage');
+        expect(noResultsMessage.exists()).toBeFalsy();
+    });
+
+    it('triggers search on button click', () => {
+        const query = 'yonatan';
+
+        act(() => {
+            wrapper.find(TypePreventiveTextField).at(0).props().onChange(query);
+        });
+        act(() => {
+            wrapper.find('button#searchIconButton').simulate('click');
+        });
+        wrapper.update();
+
+        expect(wrapper.find(TypePreventiveTextField).at(0).props().value).toBe(query);
+        const tableRow = wrapper.find('tr#person-row-666');
+
+        expect(tableRow.exists()).toBeFalsy();
+
+        const noResultsMessage = wrapper.find('h5#errorMessage');
+        expect(noResultsMessage.exists()).toBeTruthy();
+        expect(noResultsMessage.text()).toBe('אין תוצאות מתאימות');
+    })
+
+    describe('searches by: ' , () => {
+        const { identificationNumber , firstName , lastName, phoneNumber} = testPersonalDetails;
+        const searchableValues = [ identificationNumber, firstName, lastName, phoneNumber];
+        const searchWrapper = mount(
+            <MockFormProvider >
+                <AccordionContent 
+                    {...contentProps}
+                />
+            </MockFormProvider>
+        )
+
+        searchableValues.forEach((value) => {
+            it(`searches: ${value}` , () => {
+                act(() => {
+                    searchWrapper.find(TypePreventiveTextField).at(0).props().onChange(value);
+                });
+                act(() => {
+                    searchWrapper.find('button#searchIconButton').simulate('click');
+                });
+                searchWrapper.update();
+
+                expect(searchWrapper.find(TypePreventiveTextField).at(0).props().value).toBe(value);
+                const tableRow = searchWrapper.find('tr#person-row-666');
+
+                expect(tableRow.exists()).toBeTruthy();
+                expect(tableRow).toHaveLength(1);
+            })
+        });
+    });
 });
