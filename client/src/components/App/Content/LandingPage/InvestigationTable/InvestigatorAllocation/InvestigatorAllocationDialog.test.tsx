f12047b5ed5f4775f2424acf120bbb252a04fa17
Noam6421 <noam6421@gmail.com>
2021-02-10 14:20:49 +0200

    cleaning && canacel button clickes test

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorAllocationDialog.test.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorAllocationDialog.test.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { mount, ReactWrapper } from 'enzyme';
 import { act } from 'react-dom/test-utils';
+import { mount, ReactWrapper } from 'enzyme';
 
-
-import InvestigatorAllocationDialog, { investigatorAllocationTitle } from './InvestigatorAllocationDialog';
-import InvestigatorOption from 'models/InvestigatorOption';
-import InvestigatorsTable from './InvestigatorsTable/InvestigatorsTable';
 import { investigators } from 'Utils/Testing/InvestigatorAllocation/state/index';
 
+import InvestigatorsTable from './InvestigatorsTable/InvestigatorsTable';
+import InvestigatorAllocationDialog, { investigatorAllocationTitle } from './InvestigatorAllocationDialog';
+
 const fetchInvestigatorsSpy = jest.fn(() => Promise.resolve(investigators));
+const handleCloseDialogSpy = jest.fn();
+const allocateInvestigationToInvestigatorSpy = jest.fn();
+const onSuccessSpy = jest.fn(() => Promise.resolve({ isConfirmed: true, isDenied: false, isDismissed: false }));
 
 const contentProps = {
     isOpen: true,
-    handleCloseDialog:  jest.fn(),
+    handleCloseDialog: handleCloseDialogSpy,
     fetchInvestigators: fetchInvestigatorsSpy,
-    allocateInvestigationToInvestigator: jest.fn(),
+    allocateInvestigationToInvestigator: allocateInvestigationToInvestigatorSpy,
     groupIds: [],
     epidemiologyNumbers: [],
-    onSuccess:  jest.fn(() => Promise.resolve({ isConfirmed: true, isDenied: false, isDismissed: false })),
+    onSuccess: onSuccessSpy
 };
-
 
 let wrapper: ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
 const fulshPromises = () => new Promise(setImmediate);
@@ -34,7 +35,7 @@ const loadWrapper = async () => {
         await fulshPromises();
     })
     wrapper.update();
-}
+};
 
 describe('<InvestigatorAllocationDialog />', () => {
     beforeAll(loadWrapper);
@@ -49,13 +50,26 @@ describe('<InvestigatorAllocationDialog />', () => {
         expect(title.exists()).toBeTruthy();
         expect(title.text()).toBe(investigatorAllocationTitle);
     });
+    
+    describe('Cancel Btn:', () => {
+        beforeAll(loadWrapper);
+        afterEach(loadWrapper);
+        const cancelButtonSelector = 'button#cancel-button';
 
-    it('renders cancel button' , () => {
-        const cancelButton = wrapper.find('button#cancel-button');
-        expect(cancelButton.exists()).toBeTruthy();
+        it('renders' , () => {
+            expect(wrapper.find(cancelButtonSelector).exists()).toBeTruthy();
+        });
+
+        it('runs closeDialog when clicked' , () => {
+            act(() => {
+                wrapper.find(cancelButtonSelector).simulate('click');  
+            });
+            wrapper.update();    
+            expect(handleCloseDialogSpy).toHaveBeenCalledTimes(1);
+        });
     });
 
-    describe('Submit Btn :', () => {
+    describe('Submit Btn:', () => {
         beforeAll(loadWrapper);
         afterEach(loadWrapper);
         const submitButtonSelector = 'button#submit-button';
@@ -79,7 +93,7 @@ describe('<InvestigatorAllocationDialog />', () => {
         });
     });
 
-    describe('Tooltip : ' , () => {
+    describe('Tooltip:' , () => {
         beforeAll(loadWrapper);
         afterEach(loadWrapper);
         const tooltipSelector = 'span#tool-tip';
@@ -103,80 +117,10 @@ describe('<InvestigatorAllocationDialog />', () => {
             expect(wrapper.find(tooltipSelector).props().title).toBeFalsy();
         });   
     })
-    
-    
- 
 
     it('renders InvestigatorsTable' , () => {
         const investigatorsTable = wrapper.find(InvestigatorsTable);
         expect(investigatorsTable.exists()).toBeTruthy();
     });
 
-    // it('triggers search on input', () => {
-    //     const query = 'מוטי';
-    //     act(() => {
-    //         wrapper.find(TypePreventiveTextField).at(0).props().onChange(query);
-    //     })
-    //     wrapper.update();
-
-    //     expect(wrapper.find(TypePreventiveTextField).at(0).props().value).toBe(query);
-    //     const tableRow = wrapper.find('tr#person-row-666');
-
-    //     expect(tableRow.exists()).toBeTruthy();
-    //     expect(tableRow).toHaveLength(1);
-
-    //     const noResultsMessage = wrapper.find('h5#errorMessage');
-    //     expect(noResultsMessage.exists()).toBeFalsy();
-    // });
-
-    // it('triggers search on button click', () => {
-    //     const query = 'yonatan';
-
-    //     act(() => {
-    //         wrapper.find(TypePreventiveTextField).at(0).props().onChange(query);
-    //     });
-    //     act(() => {
-    //         wrapper.find('button#searchIconButton').simulate('click');
-    //     });
-    //     wrapper.update();
-
-    //     expect(wrapper.find(TypePreventiveTextField).at(0).props().value).toBe(query);
-    //     const tableRow = wrapper.find('tr#person-row-666');
-
-    //     expect(tableRow.exists()).toBeFalsy();
-
-    //     const noResultsMessage = wrapper.find('h5#errorMessage');
-    //     expect(noResultsMessage.exists()).toBeTruthy();
-    //     expect(noResultsMessage.text()).toBe('אין תוצאות מתאימות');
-    // })
-
-    // describe('searches by: ' , () => {
-    //     const { identificationNumber , firstName , lastName, phoneNumber} = testPersonalDetails;
-    //     const searchableValues = [ identificationNumber, firstName, lastName, phoneNumber];
-    //     const searchWrapper = mount(
-    //         <MockFormProvider >
-    //             <AccordionContent 
-    //                 {...contentProps}
-    //             />
-    //         </MockFormProvider>
-    //     )
-
-    //     searchableValues.forEach((value) => {
-    //         it(`searches: ${value}` , () => {
-    //             act(() => {
-    //                 searchWrapper.find(TypePreventiveTextField).at(0).props().onChange(value);
-    //             });
-    //             act(() => {
-    //                 searchWrapper.find('button#searchIconButton').simulate('click');
-    //             });
-    //             searchWrapper.update();
-
-    //             expect(searchWrapper.find(TypePreventiveTextField).at(0).props().value).toBe(value);
-    //             const tableRow = searchWrapper.find('tr#person-row-666');
-
-    //             expect(tableRow.exists()).toBeTruthy();
-    //             expect(tableRow).toHaveLength(1);
-    //         })
-    //     });
-    // });
 });
