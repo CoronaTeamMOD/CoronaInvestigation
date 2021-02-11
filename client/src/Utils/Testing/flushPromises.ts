8d951a71eb056732bd7b3547a0b67eeb63c13203
y-golde <yon.golde@gmail.com>
2021-02-11 13:27:22 +0200

    added documentation to flushPromises

--- client/src/Utils/Testing/flushPromises.ts
+++ client/src/Utils/Testing/flushPromises.ts
@@ -1,3 +1,17 @@
 const flushPromises = () => new Promise(setImmediate);
 
+/*
+ * Flush promises resolves all existing promises
+ * example : 
+ * 
+ * await act( async () => {
+ *      anAsyncAction();
+ *      await flushPromises();
+ * });
+ * wrapper.update();
+ * 
+ * here we're activating an asynchronos action but not necessarily waiting for it to be resolved
+ * by adding flushPromises we can make sure the action gets resloved and the wrapper will be updated appropriately
+ * 
+ */
 export default flushPromises;
