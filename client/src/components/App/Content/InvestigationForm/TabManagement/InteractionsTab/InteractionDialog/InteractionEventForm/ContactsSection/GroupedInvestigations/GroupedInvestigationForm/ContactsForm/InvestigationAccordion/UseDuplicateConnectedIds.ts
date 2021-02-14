470e62360edd16ce0a40c3758dba047dcedde2f0
y-golde <yon.golde@gmail.com>
2021-02-14 10:30:04 +0200

    formatting

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/UseDuplicateConnectedIds.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/UseDuplicateConnectedIds.ts
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useSelector } from 'react-redux';
 
 import StoreStateType from 'redux/storeStateType';
@@ -6,10 +5,11 @@ import ConnectedInvestigation from 'models/GroupedInvestigationContacts/Connecte
 
 const UseDuplicateConnectedIds = (epiNumber : number) => {
     const investigations = useSelector<StoreStateType , ConnectedInvestigation[]>(state => state.groupedInvestigations.investigations.investigationsByGroupId.nodes);
-
     const indexOfCurrentInvestigation = investigations.map(investigation => investigation.epidemiologyNumber).indexOf(epiNumber);
     const investigationsToSearch = investigations.slice(0 , indexOfCurrentInvestigation);
+
     let connectedInvestigationsIds : string[] = []
+    
     investigationsToSearch.forEach(investigation => {
         investigation.contactEventsByInvestigationId.nodes.forEach(event => {
             event.contactedPeopleByContactEvent.nodes.forEach(person => {
