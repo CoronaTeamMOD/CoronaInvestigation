f8b55475237afcfafc352c23787a954e2ffcdd1c
y-golde <yon.golde@gmail.com>
2021-02-14 10:26:02 +0200

    added useDuplicateConnectedIds

---
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/GroupedInvestigations/GroupedInvestigationForm/ContactsForm/InvestigationAccordion/UseDuplicateConnectedIds.ts
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+import StoreStateType from 'redux/storeStateType';
+import ConnectedInvestigation from 'models/GroupedInvestigationContacts/ConnectedInvestigation';
+
+const UseDuplicateConnectedIds = (epiNumber : number) => {
+    const investigations = useSelector<StoreStateType , ConnectedInvestigation[]>(state => state.groupedInvestigations.investigations.investigationsByGroupId.nodes);
+
+    const indexOfCurrentInvestigation = investigations.map(investigation => investigation.epidemiologyNumber).indexOf(epiNumber);
+    const investigationsToSearch = investigations.slice(0 , indexOfCurrentInvestigation);
+    let connectedInvestigationsIds : string[] = []
+    investigationsToSearch.forEach(investigation => {
+        investigation.contactEventsByInvestigationId.nodes.forEach(event => {
+            event.contactedPeopleByContactEvent.nodes.forEach(person => {
+                const {identificationNumber , identificationType} = person.personByPersonInfo;
+            
+                if(Boolean(identificationNumber) && Boolean(identificationType)) {
+                    connectedInvestigationsIds.push(identificationNumber);
+                }
+            });
+        });
+    });
+
+    return connectedInvestigationsIds;
+}
+
+export default UseDuplicateConnectedIds
