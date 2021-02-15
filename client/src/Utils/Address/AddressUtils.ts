0ca9122ef4cfb194ba5931e7b7ae9d2d688ea793
Chen Friedman <chen.friedman11@gmail.com>
2021-02-15 12:43:31 +0200

    AddressUtils, auxiliaryFunctions, symptomsUtils tests

--- client/src/Utils/Address/AddressUtils.ts
+++ client/src/Utils/Address/AddressUtils.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {Severity} from '../../models/Logger';
+import { Severity } from '../../models/Logger';
 import Street from '../../models/Street';
 import logger from '../../logger/logger';
 
@@ -9,16 +9,18 @@ export const getStreetByCity = (cityId: string, callback: (streets: Streets) => 
     const getStreetByCityLogger = logger.setup('Getting streets of city');
     getStreetByCityLogger.info(`launching request to server with parameter ${cityId}`, Severity.LOW);
     axios.get('/addressDetails/city/' + cityId + '/streets').then(result => {
-            if (result?.data) {
-                getStreetByCityLogger.info('got data from the server', Severity.LOW);
-                const streets: Streets = new Map();
-                result.data.forEach((street: Street) => {
-                    streets.set(street.id, street)
-                });
-                callback(streets);
-            } else {
-                getStreetByCityLogger.error(`got errors in server result: ${JSON.stringify(result)}`, Severity.HIGH);
-            }
+        if (result?.data) {
+            getStreetByCityLogger.info('got data from the server', Severity.LOW);
+            const streets: Streets = new Map();
+            result.data.forEach((street: Street) => {
+                streets.set(street.id, street)
+            });
+            callback(streets);
+        } else {
+            getStreetByCityLogger.error(`got errors in server result: ${JSON.stringify(result)}`, Severity.HIGH);
         }
-    )
+    })
+        .catch((err) => {
+            getStreetByCityLogger.error(`got errors in server result: ${err}`, Severity.MEDIUM);
+        })
 };
