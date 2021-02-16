8e4c71d1d9c2683f4f13272214b7d0f7b6a4ac8c
Chen Friedman <chen.friedman11@gmail.com>
2021-02-16 16:07:01 +0200

    added describe to passpord and id

--- client/src/Utils/auxiliaryFunctions/AuxiliaryFunctions.test.tsx
+++ client/src/Utils/auxiliaryFunctions/AuxiliaryFunctions.test.tsx
@@ -1,35 +1,41 @@
 import { isPassportValid, isIdValid } from 'Utils/auxiliaryFunctions/auxiliaryFunctions';
 
 describe('auxiliaryFunctions', () => {
-    it('should accept ID number' , () => {
-        expect(isIdValid('999616717')).toBeTruthy
-    })
+    
+    describe('ID number: ', () => {
+        it('should accept' , () => {
+            expect(isIdValid('999616717')).toBeTruthy
+        })
 
-    it('shouldnt accept ID number - wrong number' , () => {
-        expect(isIdValid('123123123')).toBe(false)
-    })
+        it('shouldnt accept - wrong number' , () => {
+            expect(isIdValid('123123123')).toBe(false)
+        })
 
-    it('shouldnt accept ID number - short number' , () => {
-        expect(isIdValid('99961671')).toBe(false)
-    })
+        it('shouldnt accept - short number' , () => {
+            expect(isIdValid('99961671')).toBe(false)
+        })
 
-    it('shouldnt accept ID number - long number' , () => {
-        expect(isIdValid('9996167179')).toBe(false)
-    })
+        it('shouldnt accept - long number' , () => {
+            expect(isIdValid('9996167179')).toBe(false)
+        })
+    });
 
-    it('should accept ID number' , () => {
-        expect(isPassportValid('3005739')).toBeTruthy
-    })
+    describe('Passport number: ', () => {
 
-    it('shouldnt accept ID number - wrong number' , () => {
-        expect(isPassportValid('123123123')).toBe(false)
-    })
+        it('should accept' , () => {
+            expect(isPassportValid('3005739')).toBeTruthy
+        })
 
-    it('shouldnt accept ID number - long number' , () => {
-        expect(isPassportValid('30057391')).toBe(false)
-    })
+        it('shouldnt accept - wrong number' , () => {
+            expect(isPassportValid('123123123')).toBe(false)
+        })
 
-    it('shouldnt accept Passport number - short number' , () => {
-        expect(isPassportValid('300573')).toBe(false)
-    })
+        it('shouldnt accept - long number' , () => {
+            expect(isPassportValid('30057391')).toBe(false)
+        })
+
+        it('shouldnt accept - short number' , () => {
+            expect(isPassportValid('300573')).toBe(false)
+        })
+    });
 })
