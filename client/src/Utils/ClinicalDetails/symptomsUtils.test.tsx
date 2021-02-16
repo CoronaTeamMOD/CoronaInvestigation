03b4a62fb2dceb01570680b79dfdf59684a11a59
Chen Friedman <chen.friedman11@gmail.com>
2021-02-16 15:48:27 +0200

    check tests in git

--- client/src/Utils/ClinicalDetails/symptomsUtils.test.tsx
+++ client/src/Utils/ClinicalDetails/symptomsUtils.test.tsx
@@ -5,20 +5,23 @@ import { getDatesToInvestigate } from 'Utils/ClinicalDetails/symptomsUtils';
         const investigationStartDate = new Date();
         let validationDate = new Date();
         let symptomsStartDate = new Date();
-        const expectedDatesToInvestigate = eachDayOfInterval({start: subDays(validationDate, 7), end: investigationStartDate}).sort(compareDesc);
+
         beforeAll(async () => {
             validationDate = subDays(validationDate, 3);
             symptomsStartDate= subDays(symptomsStartDate, 5);
         });
             it('HaveSymptoms, symptomsStartDateand and validationDate', async () => {
-                expect(getDatesToInvestigate(true, symptomsStartDate, validationDate)).toEqual(eachDayOfInterval({start: subDays(symptomsStartDate, 4), end: investigationStartDate}).sort(compareDesc));
+                const haveSymptomsExpectedDatesToInvestigate = eachDayOfInterval({start: subDays(symptomsStartDate, 4), end: investigationStartDate}).sort(compareDesc)
+                expect(getDatesToInvestigate(true, symptomsStartDate, validationDate)).toEqual(haveSymptomsExpectedDatesToInvestigate);
             })
 
             it('HaveSymptoms and validationDate', async () => {
+                const expectedDatesToInvestigate = eachDayOfInterval({start: subDays(validationDate, 7), end: investigationStartDate}).sort(compareDesc);
                 expect( getDatesToInvestigate(true, null, validationDate)).toEqual(expectedDatesToInvestigate);
             });
 
             it('asymptomatic with validationDate', async () => {
+                const expectedDatesToInvestigate = eachDayOfInterval({start: subDays(validationDate, 7), end: investigationStartDate}).sort(compareDesc);
                 expect(getDatesToInvestigate(false, null, validationDate)).toEqual(expectedDatesToInvestigate);
             });
 
