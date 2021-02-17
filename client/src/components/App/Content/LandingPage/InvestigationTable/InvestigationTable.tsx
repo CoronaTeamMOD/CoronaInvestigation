e70a6ae5b280286d9df40e7e470cca797ba5febd
y-golde <yon.golde@gmail.com>
2021-02-17 16:19:38 +0200

    re-ordered allocation structure

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTable.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTable.tsx
@@ -150,12 +150,12 @@ const InvestigationTable: React.FC = (): JSX.Element => {
     }
 
     const allocateInvestigationToInvestigator = async (groupIds: string[], epidemiologyNumbers: number[], investigatorToAllocate: InvestigatorOption) => {
-        if (epidemiologyNumbers) {
+        if (groupIds.length && groupIds[0]) {
+            await changeGroupsInvestigator(groupIds, investigatorToAllocate, '')
+        }
+        if (epidemiologyNumbers.length > 0) {
             await changeInvestigationsInvestigator(epidemiologyNumbers, investigatorToAllocate);
-        } else {
-            if (groupIds.length && groupIds[0]) {
-            await changeGroupsInvestigator(groupIds, investigatorToAllocate)
-        }}
+        }
         groupIds[0] && groupIds.forEach((groupId: string) => fetchInvestigationsByGroupId(groupId));
         fetchTableData();
     }
@@ -455,6 +455,7 @@ const InvestigationTable: React.FC = (): JSX.Element => {
                     className={classes.pagination}
                 />
             </Grid>
+            {console.log(selectedRow)}
             <InvestigatorAllocationDialog
                 isOpen={isInvestigatorAllocationDialogOpen}
                 handleCloseDialog={() => setIsInvestigatorAllocationDialogOpen(false)}
