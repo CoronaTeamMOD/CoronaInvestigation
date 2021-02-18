a21ab608837baa003d8991a9bdcff57429f284a0
y-golde <yon.golde@gmail.com>
2021-02-18 16:25:16 +0200

    added new-new loading icon

--- client/src/commons/LoadingSpinner/LoadingSpinner.tsx
+++ client/src/commons/LoadingSpinner/LoadingSpinner.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { CircularProgress, Backdrop } from '@material-ui/core';
+import {Backdrop } from '@material-ui/core';
 
 import StoreStateType from 'redux/storeStateType';
 
 import useStyles from './LoadingSpinnerStyles';
 
 const LoadingSpinner: React.FC = () => {
-
     const isLoading = useSelector<StoreStateType, boolean>(state => state.isLoading);
 
     const classes = useStyles();
 
     return (
-      <Backdrop open={true} className={classes.container}>
-        <div className={classes.wrapper}>
-          <div className={classes.loader}>
-            <div className={classes.inner}>
-              
-            </div>
-          </div>
-        </div>
+      <Backdrop open={isLoading} className={classes.container}>
+        <div className={classes.ball} />
+        <div className={classes.ball} />
+        <div className={classes.ball} />
+        <div className={classes.ball} />
+        <div className={classes.ball} />
       </Backdrop>
     );
 }
--- client/src/commons/LoadingSpinner/LoadingSpinnerStyles.ts
+++ client/src/commons/LoadingSpinner/LoadingSpinnerStyles.ts
@@ -1,12 +1,30 @@
 import { makeStyles } from '@material-ui/styles';
 
-import { primaryBackgroundColor } from 'styles/theme';
+import theme, { primaryBackgroundColor } from 'styles/theme';
 
+const speed = 1.5;
+const getBoucnesDealys = () => {
+  const delays: any = {};
+  for (let i = 1; i <= 5; i++) {
+    const nthString = `&:nth-child(${i})`
+    delays[nthString] = {animationDelay :  0.1*speed*(i-1) + 's'};
+  }
+  return delays;
+} 
 
 const useStyle = makeStyles({
-  '@keyframes loader': {
-    '100%': {
-      width: '100%'
+  '@keyframes bounce': {
+    '0%, 60%, 100%': {
+      background: 'rgba(44, 151, 185,0.25)',
+      transform: 'translateY(0)'
+    },
+    '20%': {
+      background: 'rgba(44, 151, 185,0.75)',
+      transform: 'translateY(13px)'
+    },
+    '40%': {
+      background: 'rgba(44, 151, 185,0.75)',
+      transform: 'translateY(-13px)'
     }
   },
   container: {
@@ -17,74 +35,14 @@ const useStyle = makeStyles({
     backgroundColor: primaryBackgroundColor,
     opacity: '0.8 !important',
   },
-  wrapper: {
-    width: '200px',
-    height: '60px',
-    background: '#2c2c2c',
-    borderRadius: '60px',
-    position: 'relative',
-    //top: '130px',
-    margin: '0 auto',
-    transform: 'rotate(-90Deg) scale(1.5)',
-    '&::before' : {
-      content: '""',
-      position: 'absolute',
-      background: 'inherit',
-      borderRadius: '50%',
-      width: '90px',
-      height: '90px',
-      left: '-10px',
-      top: '50%',
-      marginTop: '-44px'
-    },
-    '&::after': {
-      content: '""',
-      height: '100%',
-      width: '10px',
-      background: '#2c2c2c',
-      position: 'absolute',
-      left: '28px',
-      top: '0',
-    }
-  },
-  loader: {
-    width: '95%',
-    height: '50%',
-    position: 'absolute',
-    left: '0',
-    top: '50%',
-    marginTop: '-15px',
-    background: '#404040',
-    borderRadius: 'inherit',
-    zIndex: 100
-  },
-  inner: {
-    width: '0%',
-    height: '100%',
-    background: '#2b8095',
-    animation: '$loader 5s ease-out infinite',
-    borderRadius: 'inherit',
-    '&::after': {
-      content: '""',
-      position: 'absolute',
-      width: '60px',
-      height: '60px',
-      borderRadius: '50%',
-      background: '#2b8095',
-      left: '-3px',
-      top: '50%',
-      marginTop: '-30px'
-    },
-    '&::before': {
-      content: '""',
-      position: 'absolute',
-      height: '100%',
-      background: '#2b8095',
-      width: '20px',
-      top: 0,
-      left: '25px',
-      zIndex: 200
-    }
+  ball: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '50%',
+    backgroundColor: theme.palette.primary.main,
+    margin: '10px',
+    animation: `${speed}s $bounce infinite`,
+    ...getBoucnesDealys(),
   }
 });
 
