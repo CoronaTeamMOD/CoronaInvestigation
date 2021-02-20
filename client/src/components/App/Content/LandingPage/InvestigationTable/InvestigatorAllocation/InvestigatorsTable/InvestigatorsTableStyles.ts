0dc782a29830030f4736934854785931ea3c5caf
Chen Friedman <chen.friedman11@gmail.com>
2021-02-20 22:21:56 +0200

    style w/o comments

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorsTable/InvestigatorsTableStyles.ts
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigatorAllocation/InvestigatorsTable/InvestigatorsTableStyles.ts
@@ -8,13 +8,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     cursor: 'pointer'
   },
-    activeSortIcon: {
-      '&$active': {
-          '&& $icon': {
-              color: theme.palette.primary.dark,
-              fontSize: 'x-large'
-          }
+  activeSortIcon: {
+    '&$active': {
+      '&& $icon': {
+        color: theme.palette.primary.dark,
+        fontSize: 'x-large'
       }
+    }
   },
   icon: {},
   active: {},
@@ -22,22 +22,3 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export default useStyles;
-
-// import { makeStyles } from '@material-ui/styles';
-
-// const useStyles = (isWide: boolean) => makeStyles({
-//     tableRow: {
-//         '&$selected, &$selected:hover': {
-//           backgroundColor: 'rgb(202, 222, 234)'
-//         },
-//         cursor: 'pointer'
-//     },
-//     selected: {},
-//     tableContainer: {
-//       width: tableWidth,
-//       height: isWide ? '67vh' : '61vh',
-//       marginBottom: '2vh'
-//   }
-// });
-
-// export default useStyles;
