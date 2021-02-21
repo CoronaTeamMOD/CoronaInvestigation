73f181bd6771139c8323b01cfd54f04dd297363d
Yonatan Golde <68457306+y-golde@users.noreply.github.com>
2021-02-21 08:36:40 +0200

    Feat/new loading icon (#1342)
    * first animation - stable!
    
    * changed icon widths
    
    * added new-new loading icon

--- client/src/commons/LoadingSpinner/LoadingSpinnerStyles.ts
+++ client/src/commons/LoadingSpinner/LoadingSpinnerStyles.ts
@@ -1,8 +1,32 @@
 import { makeStyles } from '@material-ui/styles';
 
-import { primaryBackgroundColor } from 'styles/theme';
+import theme, { primaryBackgroundColor } from 'styles/theme';
+
+const speed = 1.5;
+const getBoucnesDealys = () => {
+  const delays: any = {};
+  for (let i = 1; i <= 5; i++) {
+    const nthString = `&:nth-child(${i})`
+    delays[nthString] = {animationDelay :  0.1*speed*(i-1) + 's'};
+  }
+  return delays;
+} 
 
 const useStyle = makeStyles({
+  '@keyframes bounce': {
+    '0%, 60%, 100%': {
+      background: 'rgba(44, 151, 185,0.25)',
+      transform: 'translateY(0)'
+    },
+    '20%': {
+      background: 'rgba(44, 151, 185,0.75)',
+      transform: 'translateY(13px)'
+    },
+    '40%': {
+      background: 'rgba(44, 151, 185,0.75)',
+      transform: 'translateY(-13px)'
+    }
+  },
   container: {
     display: 'flex',
     justifyContent: 'center',
@@ -11,6 +35,15 @@ const useStyle = makeStyles({
     backgroundColor: primaryBackgroundColor,
     opacity: '0.8 !important',
   },
+  ball: {
+    width: '30px',
+    height: '30px',
+    borderRadius: '50%',
+    backgroundColor: theme.palette.primary.main,
+    margin: '10px',
+    animation: `${speed}s $bounce infinite`,
+    ...getBoucnesDealys(),
+  }
 });
 
 export default useStyle;
