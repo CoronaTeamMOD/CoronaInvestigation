d7203e0975992b9f048aec8129fff35f957cab29
y-golde <yon.golde@gmail.com>
2021-02-22 14:21:59 +0200

    added 'reset_state' action

--- client/src/redux/Investigation/investigationActionCreators.ts
+++ client/src/redux/Investigation/investigationActionCreators.ts
@@ -80,3 +80,9 @@ export const setDatesToInvestigateParams = (symptomsExistenceInfo?: SymptomsExis
         payload: { symptomsExistenceInfo, validationDate }
     })
 };
+
+export const resetInvestigationState = () => {
+    store.dispatch({
+        type: actionTypes.RESET_STATE,
+    })
+};
