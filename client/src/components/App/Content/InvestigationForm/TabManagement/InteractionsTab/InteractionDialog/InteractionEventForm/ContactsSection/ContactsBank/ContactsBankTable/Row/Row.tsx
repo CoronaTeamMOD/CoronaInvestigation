e1229ed975612b53bdbd5e1207063c2ac1751833
y-golde <yon.golde@gmail.com>
2021-02-23 12:33:40 +0200

    added form elements

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsBank/ContactsBankTable/Row/Row.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsBank/ContactsBankTable/Row/Row.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TableCell, TableRow } from '@material-ui/core';
+import { TableCell, TableRow, Checkbox, Select, MenuItem, TextField } from '@material-ui/core';
 
 import Contact from 'models/Contact';
 
@@ -7,14 +7,37 @@ const Row = (props: Props) => {
     const { contact } = props;
     return (
         <TableRow>
-            <TableCell />
+            <TableCell>
+                <Checkbox
+                    disabled={false}
+                    color='primary'
+                    checked={false}
+                    id={`person-checkbox-${contact.personInfo}`}
+                    onClick={() => {}}
+                />    
+            </TableCell> 
             <TableCell>{contact.firstName}</TableCell>
             <TableCell>{contact.lastName}</TableCell>
             <TableCell>{contact.identificationType}</TableCell>
             <TableCell>{contact.identificationNumber}</TableCell>
-            <TableCell>{contact.contactType === 1 ? 'הדוק' : 'לא הדוק'}</TableCell>
+            <TableCell>
+                <Select
+                   id="demo-simple-select"
+                   defaultValue={contact.contactType}
+                   onChange={() => {}} 
+                >
+                    <MenuItem value={1}>הדוק</MenuItem>
+                    <MenuItem value={2}>לא הדוק</MenuItem>
+                </Select>
+            </TableCell>
             <TableCell>{contact.phoneNumber}</TableCell>
-            <TableCell>{contact.extraInfo}</TableCell>
+            <TableCell>
+                <TextField
+                    id={`pesron-extraInfo-${contact.personInfo}`}
+                    defaultValue={contact.extraInfo}
+                    onChange={() => {}}
+                />
+            </TableCell>
         </TableRow>
     )
 }
