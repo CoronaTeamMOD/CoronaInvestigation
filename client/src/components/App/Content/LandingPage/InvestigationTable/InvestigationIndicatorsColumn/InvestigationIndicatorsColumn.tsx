ac30d7d86e79b06cf777cf17c2c7061f2f47828a
Chen Friedman <chen.friedman11@gmail.com>
2021-02-24 19:11:48 +0200

    get investigationComplexityReasons table and convert it to string

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationIndicatorsColumn/InvestigationIndicatorsColumn.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationIndicatorsColumn/InvestigationIndicatorsColumn.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReplyAll } from '@material-ui/icons';
-import { Tooltip , Box } from '@material-ui/core';
+import { Tooltip, Box } from '@material-ui/core';
+import axios from 'axios';
 
 import ComplexityIcon from 'commons/InvestigationComplexity/ComplexityIcon/ComplexityIcon';
 
@@ -9,14 +10,33 @@ import SelfInvestigationIcon from './SelfInvestigationIcon/SelfInvestigationIcon
 const complexInvestigationMessage = 'חקירה מורכבת';
 
 const InvestigationIndicatorsColumn = (props: Props) => {
-    const { isComplex, wasInvestigationTransferred, transferReason , isSelfInvestigated ,selfInvestigationStatus, selfInvestigationUpdateTime, complexityReasonsId} = props;
-    const complexInvestigationText = `${complexInvestigationMessage}: ${complexityReasonsId}`
+    const { isComplex, wasInvestigationTransferred, transferReason, isSelfInvestigated, selfInvestigationStatus, selfInvestigationUpdateTime, complexityReasonsId } = props;
+    const [allComplexReasons, setAllComplexReasons] = useState([]);
+
+    const getInvestigationComplexityReasons = () => {
+        axios.get('/investigationInfo/complexityReasons')
+            .then((result) => {
+                if (result?.data && result.headers['content-type'].includes('application/json')) {
+                    const allComplexReasons = (result.data).map((reason: { description: any; }) => reason.description)
+                    setAllComplexReasons(allComplexReasons)
+                }
+            })
+            .catch((err) => { })
+    }
+
+    useEffect(() => {
+        getInvestigationComplexityReasons();
+    }, []);
+
+    const investigationComplexityReasons = !!complexityReasonsId && complexityReasonsId.map((id) => !!id && allComplexReasons[id - 1]).toString()
+    const complexInvestigationText = investigationComplexityReasons ? `${complexInvestigationMessage}: ${investigationComplexityReasons}` : complexInvestigationMessage
+
     return (
         <Box display='flex' alignItems='center' flexWrap='nowrap'>
             <Box flex={1} marginX={0.5}>
                 {
                     isSelfInvestigated &&
-                    <SelfInvestigationIcon 
+                    <SelfInvestigationIcon
                         status={selfInvestigationStatus}
                         date={selfInvestigationUpdateTime}
                     />
@@ -28,12 +48,12 @@ const InvestigationIndicatorsColumn = (props: Props) => {
                 }
             </Box>
             <Box flex={1} marginX={0.5}>
-            {
-                wasInvestigationTransferred &&
-                <Tooltip title={transferReason === null ? '' : transferReason} placement='top' arrow>
-                    <ReplyAll color='primary' />
-                </Tooltip>
-            }
+                {
+                    wasInvestigationTransferred &&
+                    <Tooltip title={transferReason === null ? '' : transferReason} placement='top' arrow>
+                        <ReplyAll color='primary' />
+                    </Tooltip>
+                }
             </Box>
         </Box>
     )
@@ -44,9 +64,9 @@ interface Props {
     isComplex: boolean;
     complexityReasonsId: Array<number | null>;
     transferReason: string;
-    isSelfInvestigated : boolean;
-    selfInvestigationStatus : number;
-    selfInvestigationUpdateTime : Date;
+    isSelfInvestigated: boolean;
+    selfInvestigationStatus: number;
+    selfInvestigationUpdateTime: Date;
 };
 
 export default InvestigationIndicatorsColumn;
