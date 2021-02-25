5909aecd2ad0833f08382acf3e001b97de8ecf11
y-golde <yon.golde@gmail.com>
2021-02-25 10:05:33 +0200

    removed auto ellipsis

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableRow/CommentCell/CommentCell.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableRow/CommentCell/CommentCell.tsx
@@ -3,21 +3,32 @@ import { Typography, Button } from '@material-ui/core';
 
 import useStyles from './commentCellStyles';
 
+const MAX_CHARS_PER_ROWS = 31;
+const MAX_ROWS = 3;
+const ellipsis = '... ';
+const readMoreText = 'קרא עוד';
+
+const MAX_CHARS_BEFORE_CUT = MAX_ROWS * MAX_CHARS_PER_ROWS;
+
 const CommentCell = (props: Props) => {
     const { comment } = props;
     const [readMore, setReadMore] = useState<boolean>(false);
-    const clampLines = readMore ? 'unset' : 3;
+
     const classes = useStyles();
+    const isCommentOverflowing = comment && comment.length > MAX_CHARS_BEFORE_CUT;
+
 
     return (
         <>
-        <Typography className={classes.comment} style={{WebkitLineClamp : clampLines}}>
-            { comment }
+        <Typography className={classes.comment}>
+            { isCommentOverflowing 
+                ? <span>
+                    {comment.slice(0 , MAX_CHARS_BEFORE_CUT - ellipsis.length - readMoreText.length) + ellipsis}
+                    <a href='#' onClick={(e) => { e.preventDefault(); e.stopPropagation()}}>{readMoreText}</a>
+                </span>
+
+                : comment }
         </Typography>
-        {/* {comment && <Button onClick={(e) => {
-            e.stopPropagation();
-            setReadMore(true)
-        }}>עוד?</Button>} */}
         </>
     )
 }
--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableRow/CommentCell/commentCellStyles.ts
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTableRow/CommentCell/commentCellStyles.ts
@@ -3,12 +3,10 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles(() => ({
     comment : {
         fontSize: '0.9rem',
-        WebkitBoxOrient: 'vertical',
-        display: '-webkit-box',
         whiteSpace: 'normal',
         overflow: 'hidden',
         marginRight: 0,
-        minWidth: '150px',
+        minWidth: '200px',
     }
 }));
 
