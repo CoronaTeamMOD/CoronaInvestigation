9b389e3015e98a2cbd163326f2a622c9bc718be4
y-golde <yon.golde@gmail.com>
2021-02-28 14:57:46 +0200

    finished row tests - stable

--- client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsBank/ContactsBankTable/Row/Row.test.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/InteractionsTab/InteractionDialog/InteractionEventForm/ContactsSection/ContactsBank/ContactsBankTable/Row/Row.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import { Table, TableBody, Select, TextField } from '@material-ui/core';
 
 import MockFormProvider from 'Utils/Testing/MockFormProvider';
@@ -7,7 +8,6 @@ import { person } from 'Utils/Testing/ContactsBankForm/state';
 import MockBankFormProvider from 'Utils/Testing/ContactsBankForm/MockBankFormProvider';
 
 import Row from './Row';
-
 
 const getWrapper = (props : WrapperProps) => {
     const { setContactBankSpy, existingEventPersonInfos } = props;
@@ -41,7 +41,7 @@ describe('<Row />', () => {
 
     const checkboxSelector = 'input#person-checkbox-1234';
     const contactTypeSelector = Select;
-    const extraInfoTypeSelectors = TextField;
+    const extraInfoTypeSelector = TextField;
 
     it('renders', () => {
         expect(wrapper.exists()).toBeTruthy();
@@ -59,29 +59,87 @@ describe('<Row />', () => {
         expect(disabledWrapper.find(contactTypeSelector).exists()).toBeTruthy();
         expect(disabledWrapper.find(contactTypeSelector).prop('disabled')).toBeTruthy();
 
-        expect(disabledWrapper.find(extraInfoTypeSelectors).exists()).toBeTruthy();
-        expect(disabledWrapper.find(extraInfoTypeSelectors).prop('disabled')).toBeTruthy();
+        expect(disabledWrapper.find(extraInfoTypeSelector).exists()).toBeTruthy();
+        expect(disabledWrapper.find(extraInfoTypeSelector).prop('disabled')).toBeTruthy();
     });
 
     describe('Checkbox: ', () => {
-        it.todo('renders');
+        const setContactBankSpy = jest.fn();
+        const wrapper = getWrapper({setContactBankSpy});
+
+        it('renders', () => {
+            expect(wrapper.find(checkboxSelector).exists()).toBeTruthy();
+            expect(wrapper.find(checkboxSelector).prop('disabled')).toBeFalsy();
+        });
     
-        it.todo('clicks');
+        it('clicks', () => {
+            expect(setContactBankSpy).not.toHaveBeenCalled();
+
+            act(() => {
+                wrapper.find(checkboxSelector).simulate('click');
+            });
+            wrapper.update();
+
+            expect(setContactBankSpy).toHaveBeenCalled();
+            const expectedMap = new Map([[1234 , {checked : true , contactType : 1 , extraInfo : ''}]]);
+            expect(setContactBankSpy).toHaveBeenCalledWith(expectedMap);
+        });
     }); 
 
     describe('Contact Type Select: ', () => {
-        it.todo('renders');
-    
-        it.todo('default value');
+        const setContactBankSpy = jest.fn();
+        const wrapper = getWrapper({setContactBankSpy});
 
-        it.todo('changes');
+        it('renders', () => {
+            expect(wrapper.find(contactTypeSelector).exists()).toBeTruthy();
+            expect(wrapper.find(contactTypeSelector).prop('disabled')).toBeFalsy();
+        });
+    
+        it('default value', () => {
+            expect(wrapper.find(contactTypeSelector).prop('defaultValue')).toBe(1);
+        });
+
+        it('changes', () => {
+            expect(setContactBankSpy).not.toHaveBeenCalled();
+
+            act(() => {
+                //@ts-ignore
+                wrapper.find(contactTypeSelector).props().onChange({ target : { value : 2 } });
+            });
+            wrapper.update();
+
+            expect(setContactBankSpy).toHaveBeenCalled();
+            const expectedMap = new Map([[1234 , {checked : false , contactType : 2 , extraInfo : ''}]]);
+            expect(setContactBankSpy).toHaveBeenCalledWith(expectedMap);
+        });
     });
 
     describe('extra info textfield', () => {
-        it.todo('renders');
+        const setContactBankSpy = jest.fn();
+        const wrapper = getWrapper({setContactBankSpy});
 
-        it.todo('default value');
+        it('renders', () => {
+            expect(wrapper.find(extraInfoTypeSelector).exists()).toBeTruthy();
+            expect(wrapper.find(extraInfoTypeSelector).prop('disabled')).toBeFalsy();
+        });
+    
+        it('default value', () => {
+            expect(wrapper.find(extraInfoTypeSelector).prop('defaultValue')).toBe('');
+        });
 
-        it.todo('changes');
+        it('changes', () => {
+            expect(setContactBankSpy).not.toHaveBeenCalled();
+
+            const testExtraInfo = 'test';
+            act(() => {
+                //@ts-ignore
+                wrapper.find(extraInfoTypeSelector).props().onChange({ target : { value : testExtraInfo } });
+            });
+            wrapper.update();
+
+            expect(setContactBankSpy).toHaveBeenCalled();
+            const expectedMap = new Map([[1234 , {checked : false , contactType : 1 , extraInfo : testExtraInfo}]]);
+            expect(setContactBankSpy).toHaveBeenCalledWith(expectedMap);
+        });
     });
 });
