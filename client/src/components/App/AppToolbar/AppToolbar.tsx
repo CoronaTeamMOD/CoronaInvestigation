83b231ab816e11d9d87e05cb5f2c4114439513fd
Noam6421 <noam6421@gmail.com>
2021-03-02 11:27:37 +0200

    initial ui adding

--- client/src/components/App/AppToolbar/AppToolbar.tsx
+++ client/src/components/App/AppToolbar/AppToolbar.tsx
@@ -5,7 +5,7 @@ import { NavLink, NavLinkProps, useLocation, useHistory } from 'react-router-dom
 import { AppBar, Toolbar, Typography, Tooltip, IconButton, Select, MenuItem } from '@material-ui/core';
 
 import County from 'models/County';
-import UserType from 'models/enums/UserType';
+import UserTypeCodes from 'models/enums/UserTypeCodes';
 import StoreStateType from 'redux/storeStateType';
 import IsActiveToggle from 'commons/IsActiveToggle/IsActiveToggle';
 import { setDisplayedCounty } from 'redux/User/userActionCreators';
@@ -16,124 +16,155 @@ import useAppToolbar from './useAppToolbar';
 
 const toggleMessage = 'מה הסטטוס שלך?';
 const navButtonsWhitelist = {
-  allowedUserTypes: [UserType.ADMIN, UserType.SUPER_ADMIN],
-  allowedRoutes: [landingPageRoute, adminLandingPageRoute, usersManagementRoute]
+    allowedUserTypes: [UserTypeCodes.ADMIN, UserTypeCodes.SUPER_ADMIN],
+    allowedRoutes: [landingPageRoute, adminLandingPageRoute, usersManagementRoute]
 };
 
 const StatePersistentNavLink = (props: NavLinkProps) => {
-  const classes = useStyles();
-  const history = useHistory();
+    const classes = useStyles();
+    const history = useHistory();
 
-  const handleNavClick: NavLinkProps['onClick'] = (event) => {
-    event.preventDefault(); // prevent state reset on reroute
-    history.push(props.to as string, history.location.state);
-  };
+    const handleNavClick: NavLinkProps['onClick'] = (event) => {
+        event.preventDefault(); // prevent state reset on reroute
+        history.push(props.to as string, history.location.state);
+    };
 
-  return (
-    <NavLink {...props} location={history.location}
-      onClick={handleNavClick}
-      activeClassName={classes.activeItem} className={classes.menuItem}>
-      {props.children}
-    </NavLink>
-  )
+    return (
+        <NavLink {...props} location={history.location}
+            onClick={handleNavClick}
+            activeClassName={classes.activeItem} className={classes.menuItem}
+        >
+            {props.children}
+        </NavLink>
+    );
 };
 
 const AppToolbar: React.FC = (): JSX.Element => {
-  const { user, isActive, logout, setUserActivityStatus } = useAppToolbar();
+    const { user, isActive, logout, setUserActivityStatus } = useAppToolbar();
 
-  const displayedCounty = useSelector<StoreStateType, number>(state => state.user.displayedCounty);
-  const districtCounties = useSelector<StoreStateType, County[]>(state => state.county.districtCounties);
-  const countyDisplayName = useSelector<StoreStateType, string>(state => state.user.data.countyByInvestigationGroup.displayName);
+    const displayedCounty = useSelector<StoreStateType, number>(state => state.user.displayedCounty);
+    const districtCounties = useSelector<StoreStateType, County[]>(state => state.county.districtCounties);
+    const countyDisplayName = useSelector<StoreStateType, string>(state => state.user.data.countyByInvestigationGroup.displayName);
 
-  const classes = useStyles();
-  const location = useLocation();
-  const userName = user.authorityByAuthorityId?.authorityName ? 
-                          user.userName +" (" + user.authorityByAuthorityId.authorityName + ")"  : user.userName;
+    const classes = useStyles();
+    const location = useLocation();
+    const userName = user.authorityByAuthorityId?.authorityName ? 
+                            user.userName +" (" + user.authorityByAuthorityId.authorityName + ")"  : user.userName;
 
-  return (
-    <AppBar className={classes.appBar} position='static'>
-      <Toolbar>
-        <div className={classes.rightSection}>
-          <StatePersistentNavLink exact to={indexRoute} >
-            <img alt='logo' src='./assets/img/logo.png' width={48} height={48} />
-            <Typography variant='h4' className={classes.title}>אבן יסוד</Typography>
-          </StatePersistentNavLink>
-          {
-            navButtonsWhitelist.allowedUserTypes.includes(user.userType) &&
-            navButtonsWhitelist.allowedRoutes.includes(location.pathname) &&
-            <div className={classes.navButtons}>
-              <StatePersistentNavLink exact to={indexRoute} 
-                isActive={(match, location) => 
-                  (location.pathname === adminLandingPageRoute || location.pathname  === landingPageRoute)}>
-                <Home className={classes.menuIcon} />
-                <Typography className={classes.menuTypo}> עמוד הבית</Typography>
-              </StatePersistentNavLink>
-              <StatePersistentNavLink exact to={usersManagementRoute}>
-                <SupervisorAccount className={classes.menuIcon} />
-                <Typography className={classes.menuTypo}> ניהול משתמשים</Typography>
-              </StatePersistentNavLink>
-            </div>
-          }
-        </div>
-        <div className={classes.userSection}>
-          {isActive !== null &&
-            <Tooltip title={toggleMessage} arrow>
-              <IsActiveToggle
-                value={isActive}
-                onToggle={setUserActivityStatus}
-                exclusive
-              />
-            </Tooltip>
-          }
-          <Tooltip title='התנתקות מהמערכת' arrow>
-            <IconButton color='inherit' onClick={logout}>
-              <ExitToApp />
-            </IconButton>
-          </Tooltip>
-          <Typography className={classes.greetUserText}>
-            שלום, {userName}
-          </Typography>
-          {
-            user.userType === UserType.SUPER_ADMIN ?
-              <Select
-                className={classes.countySelect}
-                value={displayedCounty}
-                onChange={(event) => setDisplayedCounty(event.target.value as number)}
-                classes={{icon: classes.countySelect}}
-                disableUnderline
-                MenuProps={{
-                  anchorOrigin: {
-                      vertical: 'bottom',
-                      horizontal: 'left'
-                  },
-                  transformOrigin: {
-                    vertical: 'top',
-                    horizontal: 'left'
-                  },
-                  getContentAnchorEl: null
-                }}
-                renderValue={(value) => 
-                  <Typography>נפת <b>{districtCounties.find(county => county.id === value)?.displayName}</b></Typography>
-                }
-              >
-                {
-                  districtCounties.map(county => 
-                    <MenuItem key={county.id} value={county.id}>
-                        {`נפת  ${county.displayName}`}
-                    </MenuItem>
-                  )
-                }
-              </Select>
-            :
-              countyDisplayName &&
-              <Typography>
-                הינך מחובר/ת לנפת <b>{countyDisplayName}</b>
-              </Typography>
-          }
-        </div>
-      </Toolbar>
-    </AppBar>
-  );
-}
+    return (
+        <AppBar className={classes.appBar} position='static'>
+            <Toolbar>
+                <div className={classes.rightSection}>
+                    <StatePersistentNavLink exact to={indexRoute} >
+                        <img alt='logo' src='./assets/img/logo.png' width={48} height={48} />
+                        <Typography variant='h4' className={classes.title}>אבן יסוד</Typography>
+                    </StatePersistentNavLink>
+                    {
+                    navButtonsWhitelist.allowedUserTypes.includes(user.userType) &&
+                    navButtonsWhitelist.allowedRoutes.includes(location.pathname) &&
+                        <div className={classes.navButtons}>
+                            <StatePersistentNavLink exact to={indexRoute} 
+                                isActive={(match, location) => 
+                                (location.pathname === adminLandingPageRoute || location.pathname  === landingPageRoute)}
+                            >
+                                <Home className={classes.menuIcon} />
+                                <Typography className={classes.menuTypo}> עמוד הבית</Typography>
+                            </StatePersistentNavLink>
+                            <StatePersistentNavLink exact to={usersManagementRoute}>
+                                <SupervisorAccount className={classes.menuIcon} />
+                                <Typography className={classes.menuTypo}> ניהול משתמשים</Typography>
+                            </StatePersistentNavLink>
+                        </div>
+                    }
+                </div>
+                <div className={classes.userSection}>
+                    {/* {user.isDeveloper &&
+                        <Select
+                            className={classes.select}
+                            value={user.userType}
+                            onChange={(event) => setUserType(event.target.value as number)}
+                            classes={{icon: classes.select}}
+                            disableUnderline
+                            MenuProps={{
+                            anchorOrigin: {
+                                vertical: 'bottom',
+                                horizontal: 'left'
+                            },
+                            transformOrigin: {
+                                vertical: 'top',
+                                horizontal: 'left'
+                            },
+                            getContentAnchorEl: null
+                            }}
+                            renderValue={(value) => 
+                                <Typography>נפת <b>{districtCounties.find(county => county.id === value)?.displayName}</b></Typography>
+                            }
+                        >
+                            {
+                            districtCounties.map(county => 
+                                <MenuItem key={county.id} value={county.id}>
+                                    {`נפת  ${county.displayName}`}
+                                </MenuItem>
+                            )
+                            }
+                        </Select>
+                    } */}
+                    {isActive !== null &&
+                        <Tooltip title={toggleMessage} arrow>
+                        <IsActiveToggle
+                            value={isActive}
+                            onToggle={setUserActivityStatus}
+                            exclusive
+                        />
+                        </Tooltip>
+                    }
+                    <Tooltip title='התנתקות מהמערכת' arrow>
+                        <IconButton color='inherit' onClick={logout}>
+                            <ExitToApp />
+                        </IconButton>
+                    </Tooltip>
+                    <Typography className={classes.greetUserText}>
+                        שלום, {userName}
+                    </Typography>
+                    {user.userType === UserTypeCodes.SUPER_ADMIN ?
+                        <Select
+                            className={classes.select}
+                            value={displayedCounty}
+                            onChange={(event) => setDisplayedCounty(event.target.value as number)}
+                            classes={{icon: classes.select}}
+                            disableUnderline
+                            MenuProps={{
+                            anchorOrigin: {
+                                vertical: 'bottom',
+                                horizontal: 'left'
+                            },
+                            transformOrigin: {
+                                vertical: 'top',
+                                horizontal: 'left'
+                            },
+                            getContentAnchorEl: null
+                            }}
+                            renderValue={(value) => 
+                                <Typography>נפת <b>{districtCounties.find(county => county.id === value)?.displayName}</b></Typography>
+                            }
+                        >
+                        {
+                            districtCounties.map(county => 
+                                <MenuItem key={county.id} value={county.id}>
+                                    {`נפת  ${county.displayName}`}
+                                </MenuItem>
+                            )
+                        }
+                    </Select>
+                    : countyDisplayName &&
+                        <Typography>
+                            הינך מחובר/ת לנפת <b>{countyDisplayName}</b>
+                        </Typography>
+                    }
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default AppToolbar;
