613869c02b9a4c1058092ceb19a81d78e46e468f
Chen Friedman <chen.friedman11@gmail.com>
2021-03-03 13:38:58 +0200

    add another 4 complexity reasons to db

--- db scripts/create_new_columns_in_investigation_and_investigation_complexity_reasons.sql
+++ db scripts/create_new_columns_in_investigation_and_investigation_complexity_reasons.sql
@@ -1,5 +1,16 @@
 ALTER TABLE investigation
 ADD COLUMN complexity_reasons_id integer[];
-
 ALTER TABLE investigation_complexity_reasons
-ADD COLUMN reason_id SERIAL NOT NULL
+ADD COLUMN reason_id SERIAL NOT NULL;
+ 
+insert into public.investigation_complexity_reasons
+values(10, 'חולה חוזר');
+
+insert into public.investigation_complexity_reasons
+values(11, 'מחוסן');
+
+insert into public.investigation_complexity_reasons
+values(12, 'מוטציה');
+
+insert into public.investigation_complexity_reasons
+values(13, 'חזר מחו"ל');
