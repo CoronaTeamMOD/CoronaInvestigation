4d82be677b20f5d8cc68db402acebff0113bce2c
Chen Friedman <chen.friedman11@gmail.com>
2021-03-04 16:19:46 +0200

    update redix after every change in redux

--- client/src/Utils/ComplexityReasons/ComplexityReasonsFunctions.ts
+++ client/src/Utils/ComplexityReasons/ComplexityReasonsFunctions.ts
@@ -1,15 +1,19 @@
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
+import StoreStateType from 'redux/storeStateType';
 import { PersonalInfoDbData } from 'models/Contexts/PersonalInfoContextData';
 import { Exposure } from 'commons/Contexts/ExposuresAndFlights';
 import logger from 'logger/logger';
 import { Severity } from 'models/Logger';
+import { setComplexReasonsId } from 'redux/Investigation/investigationActionCreators';
 
 interface DBExposure extends Omit<Exposure, 'exposureAddress'> {
     exposureAddress: string|null;
 }
 
-const updateInvestigationReasonId = (epidemiologyNumber: number, newComplexityReasonId: number) => {
+
+const updateInvestigationReasonId = (epidemiologyNumber: number, newComplexityReasonId: number, complexityReasonsId: (number|null)[]) => {
     const updateInvestigationComplexityReasonIdLogger = logger.setup('updateInvestigationComplexityReasonIdLogger');
     updateInvestigationComplexityReasonIdLogger.info('launching updateInvestigationComplexityReasonIdLogger request', Severity.LOW);
         
@@ -17,6 +21,8 @@ const updateInvestigationReasonId = (epidemiologyNumber: number, newComplexityRe
         epidemiologyNumberInput: epidemiologyNumber,
         newComplexityReasonId: newComplexityReasonId})
         .then(() => {
+            const currentComplexityReasonsId : (number|null)[] = complexityReasonsId.concat(newComplexityReasonId)
+            setComplexReasonsId(currentComplexityReasonsId)
             updateInvestigationComplexityReasonIdLogger.info('updateInvestigationComplexityReasonIdLogger to db successfully', Severity.LOW)
         })
         .catch((err) => {
@@ -24,13 +30,17 @@ const updateInvestigationReasonId = (epidemiologyNumber: number, newComplexityRe
         })
 }
 
-const removeInvestigationReasonId = (epidemiologyNumber: number, oldComplexityReasonId: number) => {
+const removeInvestigationReasonId = (epidemiologyNumber: number, oldComplexityReasonId: number, complexityReasonsId: (number|null)[]) => {
     const deleteInvestigationComplexityReasonIdLogger = logger.setup('deleteInvestigationComplexityReasonIdLogger');
     deleteInvestigationComplexityReasonIdLogger.info('launching deleteInvestigationComplexityReasonIdLogger request', Severity.LOW);
     axios.post('/investigationInfo/deleteComplexityReason', {
         epidemiologyNumberInput: epidemiologyNumber,
         oldComplexityReasonId: oldComplexityReasonId})
         .then(() => {
+            let currentComplexityReasonsId : (number|null)[] = complexityReasonsId
+            const index = currentComplexityReasonsId.indexOf(oldComplexityReasonId);
+            index > -1 && currentComplexityReasonsId.splice(index, 1);
+            setComplexReasonsId(currentComplexityReasonsId)
             deleteInvestigationComplexityReasonIdLogger.info('deleteInvestigationComplexityReasonIdLogger to db successfully', Severity.LOW)
         })
         .catch((err) => {
@@ -41,37 +51,37 @@ const removeInvestigationReasonId = (epidemiologyNumber: number, oldComplexityRe
 
 export const checkUpdateInvestigationPersonalReasonId = (personalInfoData: PersonalInfoDbData, epidemiologyNumber: number, complexityReasonsId: (number|null)[]) => {
     if (personalInfoData.insuranceCompany === null || personalInfoData.insuranceCompany === `אף אחד מהנ"ל` && !(complexityReasonsId.includes(6))) {
-        updateInvestigationReasonId(epidemiologyNumber, 6);
+        updateInvestigationReasonId(epidemiologyNumber, 6, complexityReasonsId);
     }
     if (personalInfoData.role === 4  && !(complexityReasonsId.includes(7))) {
-        updateInvestigationReasonId(epidemiologyNumber, 7);
+        updateInvestigationReasonId(epidemiologyNumber, 7, complexityReasonsId);
     }
     if (personalInfoData.relevantOccupation === "מערכת החינוך"  && !(complexityReasonsId.includes(8))) {
-        updateInvestigationReasonId(epidemiologyNumber, 8);
+        updateInvestigationReasonId(epidemiologyNumber, 8, complexityReasonsId);
     }
     if (personalInfoData.relevantOccupation === "מערכת הבריאות"  && !(complexityReasonsId.includes(9))) {
-        updateInvestigationReasonId(epidemiologyNumber, 9);
+        updateInvestigationReasonId(epidemiologyNumber, 9, complexityReasonsId);
     }
 
     if (personalInfoData.insuranceCompany !== null && personalInfoData.insuranceCompany !== `אף אחד מהנ"ל` && complexityReasonsId.includes(6)) {
-        removeInvestigationReasonId(epidemiologyNumber, 6);
+        removeInvestigationReasonId(epidemiologyNumber, 6, complexityReasonsId);
     }
     if (personalInfoData.role !== 4  && complexityReasonsId.includes(7)) {
-        removeInvestigationReasonId(epidemiologyNumber, 7);
+        removeInvestigationReasonId(epidemiologyNumber, 7, complexityReasonsId);
     }
     if (personalInfoData.relevantOccupation !== "מערכת החינוך"  && complexityReasonsId.includes(8)) {
-        removeInvestigationReasonId(epidemiologyNumber, 8);
+        removeInvestigationReasonId(epidemiologyNumber, 8, complexityReasonsId);
     }
     if (personalInfoData.relevantOccupation !== "מערכת הבריאות"  && complexityReasonsId.includes(9)) {
-        removeInvestigationReasonId(epidemiologyNumber, 9);
+        removeInvestigationReasonId(epidemiologyNumber, 9, complexityReasonsId);
     }
 }
 
 export const checkUpdateInvestigationExposureReasonId = (filteredExposures: (Exposure | DBExposure)[], epidemiologyNumber: number, complexityReasonsId: (number|null)[]) => {
     if (filteredExposures.length > 0  && !(complexityReasonsId.includes(13))) {
-        updateInvestigationReasonId(epidemiologyNumber, 13)
+        updateInvestigationReasonId(epidemiologyNumber, 13, complexityReasonsId);
     }
     if (filteredExposures.length == 0  && complexityReasonsId.includes(13)) {
-        removeInvestigationReasonId(epidemiologyNumber, 13)
+        removeInvestigationReasonId(epidemiologyNumber, 13, complexityReasonsId);
     }
 }
