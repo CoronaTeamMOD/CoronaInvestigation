aad0966651c9a4dfd138ed48047d028f4fba0ef2
Noam6421 <noam6421@gmail.com>
2021-03-08 16:26:51 +0200

    .

--- server/src/DBService/Districts/Query.ts
+++ server/src/DBService/Districts/Query.ts
@@ -2,7 +2,7 @@ import { gql } from 'postgraphile';
 
 export const GET_ALL_DISTRICRS = gql`
 query allDistricts {
-    allDistricts(filter: {orderBy: DISPLAY_NAME_ASC) {
+    allDistricts(orderBy: DISPLAY_NAME_ASC) {
     nodes {
       id
       displayName
