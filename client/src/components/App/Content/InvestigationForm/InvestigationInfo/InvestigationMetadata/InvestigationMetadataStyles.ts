a888eb1e49abe3c33372f1860157dd7e1ff624f8
Chen Friedman <chen.friedman11@gmail.com>
2021-03-16 14:33:31 +0200

    align

--- client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigationMetadata/InvestigationMetadataStyles.ts
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InvestigationMetadata/InvestigationMetadataStyles.ts
@@ -5,7 +5,7 @@ const useStyles = makeStyles({
         display: 'flex',
         flexWrap: 'wrap',
         whiteSpace: 'pre',
-        padding: '1vh 0 1vh 1.5vh',
+        padding: '1vh 0 1vh 2.5vh',
         background: '#E5E5E5',
         margin: '2vh 2vh 0 2vh',
     },
