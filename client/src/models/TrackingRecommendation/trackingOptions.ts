d78407f826153941f778cc23bbfa49fb2c77c13b
y-golde <yon.golde@gmail.com>
2021-03-17 09:49:25 +0200

    fixed typo

--- client/src/models/TrackingRecommendation/trackingOptions.ts
+++ client/src/models/TrackingRecommendation/trackingOptions.ts
@@ -2,6 +2,6 @@ export const otherSubReason = 99;
 export const defaultTrackingReason = 0;
 export const trackingOptions = [
     { id: defaultTrackingReason , displayName: 'ללא המלצה לאיתור מגעים ממוכן'},
-    { id: 1 , displayName: 'המלצה לאיתר מגעים ממוכן'},
+    { id: 1 , displayName: 'המלצה לאיתור מגעים ממוכן'},
     { id: 2 , displayName: 'אין צורך באיתור מגעים ממוכן'}
 ];
