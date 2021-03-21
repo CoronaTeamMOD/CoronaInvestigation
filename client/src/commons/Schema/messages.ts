d7c2d86c6f76c57620cc77ae1a2b399f9fff172d
Noam6421 <noam6421@gmail.com>
2021-03-21 17:57:52 +0200

    new common schema messages file for setting up text for schema messages in one place

---
+++ client/src/commons/Schema/messages.ts
@@ -0,0 +1,2 @@
+export const requiredText = 'שדה זה הוא חובה';
+export const invalidDateText = 'תאריך לא תקין';
--- client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoTabValidationSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoTabValidationSchema.ts
@@ -1,14 +1,13 @@
 import * as yup from 'yup';
 
 import Occupations from 'models/enums/Occupations';
+import { requiredText } from 'commons/Schema/messages';
 import PersonalInfoDataContextFields from 'models/enums/PersonalInfoDataContextFields';
 import { PHONE_NUMBER_REGEX, NOT_REQUIRED_PHONE_NUMBER_REGEX } from 'commons/Regex/Regex';
 
 const occupationsWithInstitution = ['מערכת הבריאות', 'מערכת החינוך', 'כוחות הביטחון'];
 const occupationsWithoutExtraInfo = ['מערכת הבריאות', 'מערכת החינוך', 'כוחות הביטחון', 'לא עובד'];
-const requiredText = 'שגיאה: שדה חובה';
 const numberValidationText = 'שגיאה: מספר אינו תקין';
-const requiredSelectionText = '*שדה חובה';
 const maxClassNumberError = 'ניתן להזין עד המספר 50';
 
 const schema = yup.object().shape({
@@ -25,13 +24,13 @@ const schema = yup.object().shape({
         PersonalInfoDataContextFields.RELEVANT_OCCUPATION, 
         {
             is: 'מערכת החינוך',
-            then: yup.string().nullable().required(requiredSelectionText),
+            then: yup.string().nullable().required(requiredText),
             else: yup.string().nullable()
         }
     ),
     [PersonalInfoDataContextFields.INSTITUTION_NAME]: yup.string().when('relevantOccupation', (relevantOccupation: any, schema: any) => {
         return occupationsWithInstitution.find(element => element === relevantOccupation) ?
-            schema.nullable().required(requiredSelectionText) :
+            schema.nullable().required(requiredText) :
             schema.nullable()
     }),
     [PersonalInfoDataContextFields.OTHER_OCCUPATION_EXTRA_INFO]: yup.string().when('relevantOccupation', (relevantOccupation: any, schema: any) => {
@@ -41,7 +40,7 @@ const schema = yup.object().shape({
     }),
     [PersonalInfoDataContextFields.EDUCATION_OCCUPATION_CITY]: yup.string().when('relevantOccupation', (relevantOccupation: any, schema: any) => {
         return (Occupations.EDUCATION_SYSTEM === relevantOccupation) ?
-            schema.nullable().required(requiredSelectionText) :
+            schema.nullable().required(requiredText) :
             schema.nullable()
     }),
     [PersonalInfoDataContextFields.EDUCATION_CLASS_NUMBER]: yup.number().transform((value: any) => 
