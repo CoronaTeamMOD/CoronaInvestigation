537f506592025d4a0b92bda1b6e552443204a846
Noam6421 <noam6421@gmail.com>
2021-03-22 15:05:10 +0200

    phone number validation message

--- client/src/commons/Schema/messages.ts
+++ client/src/commons/Schema/messages.ts
@@ -1,2 +1,4 @@
 export const requiredText = 'שדה זה הוא חובה';
 export const invalidDateText = 'תאריך לא תקין';
+export const invalidPhoneText = 'מספר אינו תקין';
+
--- client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoTabValidationSchema.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/PersonalInfoTab/PersonalInfoTabValidationSchema.ts
@@ -1,19 +1,18 @@
 import * as yup from 'yup';
 
 import Occupations from 'models/enums/Occupations';
-import { requiredText } from 'commons/Schema/messages';
+import { requiredText, invalidPhoneText } from 'commons/Schema/messages';
 import PersonalInfoDataContextFields from 'models/enums/PersonalInfoDataContextFields';
 import { PHONE_NUMBER_REGEX, NOT_REQUIRED_PHONE_NUMBER_REGEX } from 'commons/Regex/Regex';
 
 const occupationsWithInstitution = ['מערכת הבריאות', 'מערכת החינוך', 'כוחות הביטחון'];
 const occupationsWithoutExtraInfo = ['מערכת הבריאות', 'מערכת החינוך', 'כוחות הביטחון', 'לא עובד'];
-const numberValidationText = 'שגיאה: מספר אינו תקין';
 const maxClassNumberError = 'ניתן להזין עד המספר 50';
 
 const schema = yup.object().shape({
-    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(PHONE_NUMBER_REGEX, numberValidationText),
-    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(NOT_REQUIRED_PHONE_NUMBER_REGEX, numberValidationText),
-    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().matches(NOT_REQUIRED_PHONE_NUMBER_REGEX, numberValidationText),
+    [PersonalInfoDataContextFields.PHONE_NUMBER]: yup.string().nullable().required(requiredText).matches(PHONE_NUMBER_REGEX, invalidPhoneText),
+    [PersonalInfoDataContextFields.ADDITIONAL_PHONE_NUMBER]: yup.string().nullable().matches(NOT_REQUIRED_PHONE_NUMBER_REGEX, invalidPhoneText),
+    [PersonalInfoDataContextFields.CONTACT_PHONE_NUMBER]: yup.string().nullable().matches(NOT_REQUIRED_PHONE_NUMBER_REGEX, invalidPhoneText),
     [PersonalInfoDataContextFields.INSURANCE_COMPANY]: yup.string().nullable().required(requiredText),
     address: yup.object().shape({
         [PersonalInfoDataContextFields.CITY]: yup.string().nullable().required(requiredText),
