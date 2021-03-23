79fb43de7a50a22158c083340b5ec36147464efe
Chen Friedman <chen.friedman11@gmail.com>
2021-03-24 00:13:00 +0200

    fix - uniqe-filter-complexity-reasons

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationIndicatorsColumn/InvestigationIndicatorsColumn.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationIndicatorsColumn/InvestigationIndicatorsColumn.tsx
@@ -13,7 +13,8 @@ const complexInvestigationMessage = 'חקירה מורכבת';
 const InvestigationIndicatorsColumn = (props: Props) => {
     const { isComplex, wasInvestigationTransferred, transferReason, isSelfInvestigated, selfInvestigationStatus, selfInvestigationUpdateTime, complexityReasonsId } = props;
     const allComplexReasons = useSelector<StoreStateType, (number|null)[]>(state => state.complexReasons);
-    const investigationComplexityReasons = (complexityReasonsId) && complexityReasonsId.map((id) => (id) && allComplexReasons[id - 1]).toString()
+    const investigationComplexityReasons = (complexityReasonsId) && complexityReasonsId.map((id) => (id) && allComplexReasons[id - 1]).filter((value, index, self) => self.indexOf(value) === index).toString()
+    console.log(complexityReasonsId, investigationComplexityReasons)
     const complexInvestigationText = investigationComplexityReasons ? `${complexInvestigationMessage}: ${investigationComplexityReasons}` : `${complexInvestigationMessage}: אחר`;
 
     return (
