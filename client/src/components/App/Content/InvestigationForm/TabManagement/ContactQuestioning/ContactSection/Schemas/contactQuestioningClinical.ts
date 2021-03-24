44060cd647a6fd8c6ce27f70ea3ce7d0f7ac96af
Yonatan Golde <68457306+y-golde@users.noreply.github.com>
2021-03-24 11:01:49 +0200

    added validation exception on isolationAddress (#1452)
    * added validation exception on isolationAddress
    
    * removed schema on street and housenum
    
    * removed from useContactFields

--- client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 import { ALPHANUMERIC_TEXT_REGEX } from 'commons/Regex/Regex';
+import ContactStatusCodes from 'models/enums/ContactStatusCodes';
 import InteractedContactFields from 'models/enums/InteractedContact';
-import ContactType from 'models/enums/ContactType';
 
 const errorMessage = 'שגיאה: שדה חובה'
 
@@ -13,13 +13,14 @@ export const contactQuestioningClinical = {
         .boolean()
         .nullable(),
     [InteractedContactFields.ISOLATION_ADDRESS]: yup.object()
-    .when(InteractedContactFields.DOES_NEED_ISOLATION, {
-        is: true,
-        then: yup.object().shape({
-            'city': yup.string().nullable().required(errorMessage),
-            'street': yup.string().nullable().required(errorMessage),
-            'houseNum': yup.string().nullable().required(errorMessage)
-        }),
-        otherwise: yup.object().nullable()
-    })
+    .when([InteractedContactFields.DOES_NEED_ISOLATION , InteractedContactFields.CONTACT_STATUS], 
+        (needIsolation: boolean, contactStatus: number, schema: any) => {
+            return contactStatus === ContactStatusCodes.COMPLETED || !needIsolation
+                ? yup.object().nullable()
+                : yup.object().shape({
+                    'city': yup.string().nullable().required(errorMessage) //,
+                    // 'street': yup.string().nullable().required(errorMessage),
+                    // 'houseNum': yup.string().nullable().required(errorMessage)
+                })
+        })
 };
