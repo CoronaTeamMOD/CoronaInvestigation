493b04c067e117ab173d0cc1ab1b258283b3c26c
y-golde <yon.golde@gmail.com>
2021-04-13 12:16:09 +0300

    added exception for unreachable contacts

--- db scripts/close_isolate_contacts.sql
+++ db scripts/close_isolate_contacts.sql
@@ -7,7 +7,8 @@ CREATE OR REPLACE FUNCTION public.close_isolate_contacts(
 AS $BODY$
 UPDATE public.contacted_person SET contact_status = 5, completion_time = now()
 WHERE contact_event IN 
-	(SELECT id 
+	(
+	 SELECT id 
 	 FROM public.contact_event 
 	 WHERE investigation_id = epi_number) AND
 	 completion_time IS NULL AND (
@@ -15,9 +16,6 @@ WHERE contact_event IN
  		 (
  			 SELECT (id is not null) FROM public.person WHERE id = person_info AND
 		  	 first_name IS NOT NULL AND last_name IS NOT NULL AND phone_number IS NOT NULL
- 		 )
-	 )
+ 		 ) 
+	 ) AND contact_status NOT IN (6,7)
 $BODY$;
-
-ALTER FUNCTION public.close_isolate_contacts(integer)
-    OWNER TO coronai;
