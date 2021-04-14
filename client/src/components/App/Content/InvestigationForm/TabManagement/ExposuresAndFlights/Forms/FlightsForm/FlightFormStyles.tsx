2bf44e2873d95b8fba50d654bd1f937c3828edb1
Chen Friedman <chen.friedman11@gmail.com>
2021-04-14 13:30:31 +0300

    1730/fix/flight-label-is-too-long

--- client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightFormStyles.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Forms/FlightsForm/FlightFormStyles.tsx
@@ -6,7 +6,12 @@ const useStyles = makeStyles({
         minWidth: '45vw',
         '&.MuiGrid-item': {
             paddingLeft: 0
-        }
+        },
+        '& .MuiFormLabel-root': {
+            maxWidth: '100%',
+            textAlign: 'left',
+            marginTop: '-10px',
+        },
     },
 });
 
