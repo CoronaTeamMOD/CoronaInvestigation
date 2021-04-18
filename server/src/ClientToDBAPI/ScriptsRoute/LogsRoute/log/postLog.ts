d28085bfb5bb11d7bcfab50910b32ca72852f70c
y-golde <yon.golde@gmail.com>
2021-04-18 12:41:04 +0300

    added remaining todos

---
+++ server/src/ClientToDBAPI/ScriptsRoute/LogsRoute/log/postLog.ts
@@ -0,0 +1,5 @@
+// TODO: add post so that it would run folder->script at any given moment, then return details
+
+// TODO: method to run all contents of a folder
+
+// TODO: find a way to add how much time it took to run a query (at the server)
