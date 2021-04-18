f4fb544a5753bdfda9418e2798450a13d81cc554
y-golde <yon.golde@gmail.com>
2021-04-18 11:37:11 +0300

    cleanup

--- server/src/ClientToDBAPI/LogsRoute/log/getLog.ts
+++ server/src/ClientToDBAPI/LogsRoute/log/getLog.ts
@@ -4,7 +4,6 @@ import { Request , Response } from 'express';
 const PATH_TO_LOG_FILE = path.resolve(__dirname , '../../../ScriptRunner/Logs/log.csv');
 
 const getLog = (req : Request , res : Response) => {
-    console.log(PATH_TO_LOG_FILE);
     res.download(PATH_TO_LOG_FILE);
 }
 
