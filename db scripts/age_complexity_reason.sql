4ce70d00f605443da2fd41f06567bc2dedda9897
chen friedman <31721666+chenFriedman@users.noreply.github.com>
2021-04-20 20:20:03 +0300

    1633/fix/faster-age-complexity-reason-script (#1518)

--- db scripts/age_complexity_reason.sql
+++ db scripts/age_complexity_reason.sql
@@ -1,10 +1,8 @@
 UPDATE public.investigation
-SET complexity_reasons_id = 
-	CASE WHEN epidemiology_number IN ( SELECT epidemiology_number
-            FROM covid_patients ip
-            WHERE ip.age <= 14)
-            AND investigation_status != 100000001
-            AND complexity_code = 1
-            THEN array_append(complexity_reasons_id, 3)
-        ELSE complexity_reasons_id
-END
+SET complexity_reasons_id = array_append(complexity_reasons_id, 3)
+WHERE epidemiology_number IN ( SELECT epidemiology_number
+    FROM covid_patients ip
+    WHERE ip.age <= 14)
+    AND investigation_status != 100000001
+    AND complexity_code = 1
+    AND NOT( 3 = ANY (complexity_reasons_id))
