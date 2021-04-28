7852a8dc4f1a7cb56ceae06ed04c8ef53cb5d631
Yonatan Golde <68457306+y-golde@users.noreply.github.com>
2021-04-28 09:10:50 +0300

    clean - cache auth

---
+++ server/src/Cache/authentication/removeAuthCache.ts
@@ -0,0 +1,8 @@
+import cache from '../'
+import getCacheNameByUserID from './getCacheNameByUserID';
+
+const removeAuthCache = (userId : string) => {
+    cache.del(getCacheNameByUserID(userId));
+}
+
+export default removeAuthCache;
