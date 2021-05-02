87ba324a3ec5c17f22e185406ae35054b10785a9
Noam6421 <noam6421@gmail.com>
2021-05-02 15:38:18 +0300

    Fixed closing contacts after finishing investigation with contacts bank

--- server/src/ScriptRunner/Scripts/index/close_isolate_contacts.sql
+++ server/src/ScriptRunner/Scripts/index/close_isolate_contacts.sql
@@ -10,16 +10,22 @@ CREATE OR REPLACE FUNCTION public.close_isolate_contacts(
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
 UPDATE public.person_contact_details SET contact_status = 5, completion_time = now()
-WHERE contact_event IN 
+WHERE person_info IN (
+	SELECT person_info 
+	FROM public.contacted_person
+	WHERE contact_event IN 
 	(
-	 SELECT id 
-	 FROM public.contact_event 
-	 WHERE investigation_id = epi_number) AND
-	 completion_time IS NULL AND (
-		 (does_need_isolation IS true) OR
- 		 (
- 			 SELECT (id is not null) FROM public.person WHERE id = person_info AND
-		  	 first_name IS NOT NULL AND last_name IS NOT NULL AND phone_number IS NOT NULL
- 		 ) 
-	 ) AND contact_status NOT IN (6,7)
+	 	SELECT id 
+	 	FROM public.contact_event 
+	 	WHERE investigation_id = epi_number
+	)) 
+	AND contact_status NOT IN (6,7) 
+	AND completion_time IS NULL
+	AND (
+		(does_need_isolation IS true) OR
+		(
+			SELECT (id is not null) FROM public.person WHERE id = person_info AND
+			first_name IS NOT NULL AND last_name IS NOT NULL AND phone_number IS NOT NULL
+		) 
+	);
 $BODY$;
