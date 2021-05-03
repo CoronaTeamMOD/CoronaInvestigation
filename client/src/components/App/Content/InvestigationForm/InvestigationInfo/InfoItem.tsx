9f9794d7678d72e2549f79d2c7ffeace43ad390f
y-golde <yon.golde@gmail.com>
2021-05-03 09:07:40 +0300

    added space to infoItem field

--- client/src/components/App/Content/InvestigationForm/InvestigationInfo/InfoItem.tsx
+++ client/src/components/App/Content/InvestigationForm/InvestigationInfo/InfoItem.tsx
@@ -21,8 +21,8 @@ const InfoItem = ({ name, value, testId, size = 'regular' }: InfoItemProps) => {
     const variant = sizeVariantMap[size] as Variant;
     return (
         <Typography variant={variant} className={classes.typographyElement} test-id={testId}>
-           <bdi>{name}</bdi>: 
-            {value}
+           <bdi>{name}</bdi>:
+            {` ${value}`}
         </Typography>
     );
 };
