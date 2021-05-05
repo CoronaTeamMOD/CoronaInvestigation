b8f61fdabed3cf922d734875c1abed81bd2998c8
Noam Korkos <55889135+Noam6421@users.noreply.github.com>
2021-05-05 11:53:25 +0300

    fix removed requird from parametrs (#1578)

--- server/src/DBService/InvestigationInfo/Mutation.ts
+++ server/src/DBService/InvestigationInfo/Mutation.ts
@@ -41,7 +41,7 @@ export const COMMENT = gql`
 `;
 
 export const UPDATE_INVESTIGATED_PATIENT_RESORTS_DATA = gql`
-mutation updateInvestigatedPatientById ($wasInVacation: Boolean!, $wasInEvent: Boolean!, $wereConfirmedExposuresDesc: String $id: Int!) {
+mutation updateInvestigatedPatientById ($wasInVacation: Boolean, $wasInEvent: Boolean, $wereConfirmedExposuresDesc: String $id: Int!) {
     updateInvestigatedPatientById(input: {investigatedPatientPatch: {wasInVacation: $wasInVacation, wasInEvent: $wasInEvent, wereConfirmedExposuresDesc: $wereConfirmedExposuresDesc }, id: $id}) {
       clientMutationId
     }
