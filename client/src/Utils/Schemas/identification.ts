c196e38f27dfc4b856896dfdb8082ec2454731b2
Noam Korkos <55889135+Noam6421@users.noreply.github.com>
2021-05-06 10:35:16 +0300

    changed error message text on regex error of (#1588)
    identificationNumber

--- client/src/Utils/Schemas/identification.ts
+++ client/src/Utils/Schemas/identification.ts
@@ -2,23 +2,23 @@ import * as yup from 'yup';
 
 import { otherIdLength , idLength } from 'Utils/auxiliaryFunctions/auxiliaryFunctions';
 import { ID_BASIC_VALIDATION_REGEX, PALESTINE_ID_REGEX, PASSPORT_DASH_REGEX } from 'commons/Regex/Regex';
-import { alphaNumericSpecialCharsErrorMessage, max15LengthErrorMessage, max9LengthIdErrorMessage } from 'commons/Schema/messages';
+import { alphaNumericSlashErrorMessage, max15LengthErrorMessage, max9LengthIdErrorMessage, numericErrorMessage } from 'commons/Schema/messages';
 
 export const passportSchema = yup
     .string()
-    .matches(PASSPORT_DASH_REGEX, alphaNumericSpecialCharsErrorMessage)
+    .matches(PASSPORT_DASH_REGEX, alphaNumericSlashErrorMessage)
     .max(otherIdLength, max15LengthErrorMessage);
 
 export const idSchema = yup
     .string()
-    .matches(ID_BASIC_VALIDATION_REGEX, alphaNumericSpecialCharsErrorMessage)
+    .matches(ID_BASIC_VALIDATION_REGEX, numericErrorMessage)
     .max(idLength, max9LengthIdErrorMessage);
 
 export const palestineIdSchema = yup
     .string()
-    .matches(PALESTINE_ID_REGEX, alphaNumericSpecialCharsErrorMessage)
+    .matches(PALESTINE_ID_REGEX, numericErrorMessage)
     .max(idLength, max9LengthIdErrorMessage);
 
 export const otherIdSchema = yup
     .string()
-    .matches(PASSPORT_DASH_REGEX, alphaNumericSpecialCharsErrorMessage);
+    .matches(PASSPORT_DASH_REGEX, alphaNumericSlashErrorMessage);
--- client/src/commons/IdentificationTextField/IdentificationTextField.tsx
+++ client/src/commons/IdentificationTextField/IdentificationTextField.tsx
@@ -3,13 +3,13 @@ import * as yup from 'yup';
 
 import { NUMERIC_TEXT_REGEX, PASSPORT_DASH_REGEX } from 'commons/Regex/Regex';
 import TypePreventiveTextField from 'commons/TypingPreventionTextField/TypingPreventionTextField';
-import { alphaNumericSpecialCharsErrorMessage, max15LengthErrorMessage, max9LengthIdErrorMessage, numericErrorMessage } from 'commons/Schema/messages';
+import { alphaNumericSlashErrorMessage, max15LengthErrorMessage, max9LengthIdErrorMessage, numericErrorMessage } from 'commons/Schema/messages';
 
 const IdentificationTextField = (props : Props) => {
 
     const schema = props.isId 
         ? yup.string().matches(NUMERIC_TEXT_REGEX, numericErrorMessage).max(9, max9LengthIdErrorMessage)
-        : yup.string().matches(PASSPORT_DASH_REGEX, alphaNumericSpecialCharsErrorMessage).max(15, max15LengthErrorMessage);
+        : yup.string().matches(PASSPORT_DASH_REGEX, alphaNumericSlashErrorMessage).max(15, max15LengthErrorMessage);
     
     return (
         <TypePreventiveTextField
@@ -19,6 +19,7 @@ const IdentificationTextField = (props : Props) => {
         />
     );
 };
+
 interface Props{
     disabled?: boolean,
     testId?: string,
