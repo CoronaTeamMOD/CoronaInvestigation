882062f99f140b03167f613c4ff596de2eaeb927
Yonatan Golde <68457306+y-golde@users.noreply.github.com>
2021-05-11 06:54:02 -0700

    added ip to script runner (#1608)

--- server/src/ScriptRunner/src/Logger/index.ts
+++ server/src/ScriptRunner/src/Logger/index.ts
@@ -1,21 +1,22 @@
 import levels from './levels';
+import IP from '../LogUtils/ipAddress';
 import { writeToStream } from './writeStream';
 import getLogDateTime from '../LogUtils/getLogDateTime';
 
 const success = (message : string) => {
-    writeToStream([getLogDateTime(), levels.SUCCESS, message])
+    writeToStream([getLogDateTime(), IP, levels.SUCCESS, message])
 }
 
 const info = (message : string) => {
-    writeToStream([getLogDateTime(), levels.INFO, message])
+    writeToStream([getLogDateTime(), IP, levels.INFO, message])
 }
 
 const warn = (message : string) => {
-    writeToStream([getLogDateTime(), levels.WARN, message])
+    writeToStream([getLogDateTime(), IP, levels.WARN, message])
 }
 
 const error = (message : string) => {
-    writeToStream([getLogDateTime(), levels.ERROR, message])
+    writeToStream([getLogDateTime(), IP, levels.ERROR, message])
 }
 
 
