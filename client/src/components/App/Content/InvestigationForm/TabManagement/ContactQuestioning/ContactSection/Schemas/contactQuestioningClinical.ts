7ff366723fa67c40f747bfffdb13b960e1969fa8
chenFriedman <chen.friedman11@gmail.com>
2021-05-16 10:44:26 +0300

    1843 + 1842

--- client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactSection/Schemas/contactQuestioningClinical.ts
@@ -8,17 +8,33 @@ import InteractedContactFields from 'models/enums/InteractedContact';
 export const contactQuestioningClinical = {
     [InteractedContactFields.FAMILY_RELATIONSHIP]: yup.number().nullable(),
     [InteractedContactFields.RELATIONSHIP]: yup.string().nullable().matches(ALPHANUMERIC_TEXT_REGEX, 'השדה יכול להכיל רק אותיות ומספרים'),
-    [InteractedContactFields.DOES_NEED_HELP_IN_ISOLATION]: yup.boolean().nullable().required(requiredText),
-    [InteractedContactFields.DOES_NEED_ISOLATION]: yup.boolean().nullable().required(requiredText),
+    [InteractedContactFields.DOES_NEED_HELP_IN_ISOLATION]: yup.boolean()
+    .when(
+        [InteractedContactFields.CONTACT_STATUS],
+        (contactStatus: number) => {
+            return (contactStatus === ContactStatusCodes.CANT_REACH || contactStatus === ContactStatusCodes.DONT_COOPERATE )
+                ? yup.boolean().nullable()
+                : yup.boolean().nullable().required(requiredText);
+        }
+    ),
+    [InteractedContactFields.DOES_NEED_ISOLATION]: yup.boolean()
+    .when(
+        [InteractedContactFields.CONTACT_STATUS],
+        (contactStatus: number) => {
+            return (contactStatus === ContactStatusCodes.CANT_REACH || contactStatus === ContactStatusCodes.DONT_COOPERATE )
+                ? yup.boolean().nullable()
+                : yup.boolean().nullable().required(requiredText);
+        }
+    ),
     [InteractedContactFields.ISOLATION_ADDRESS]: yup.object()
     .when([InteractedContactFields.DOES_NEED_ISOLATION , InteractedContactFields.CONTACT_STATUS], 
         (needIsolation: boolean, contactStatus: number, schema: any) => {
-            return contactStatus === ContactStatusCodes.COMPLETED || !needIsolation
+            return (contactStatus === ContactStatusCodes.CANT_REACH || contactStatus === ContactStatusCodes.DONT_COOPERATE )
                 ? yup.object().nullable()
-                : yup.object().shape({
-                    'city': yup.string().nullable().required(requiredText) //,
-                    // 'street': yup.string().nullable().required(requiredText),
-                    // 'houseNum': yup.string().nullable().required(requiredText)
-                })
+                : contactStatus === ContactStatusCodes.COMPLETED || !needIsolation
+                    ? yup.object().nullable()
+                    : yup.object().shape({
+                        'city': yup.string().nullable().required(requiredText)
+                    })
         })
 };
