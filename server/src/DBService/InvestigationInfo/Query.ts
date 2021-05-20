9ba0baa7057f1c3d597644ea3c4507239bc8b84c
Noam6421 <noam6421@gmail.com>
2021-05-20 11:56:03 +0300

    add parentStatus info to subStatuses

--- server/src/DBService/InvestigationInfo/Query.ts
+++ server/src/DBService/InvestigationInfo/Query.ts
@@ -64,6 +64,7 @@ query GetAllSubStatuses($parentStatus: Int!) {
   allInvestigationSubStatuses(orderBy: DISPLAY_NAME_ASC, filter: {parentStatus: {equalTo: $parentStatus}}) {
     nodes {
       displayName
+      parentStatus
     }
   }
 }
