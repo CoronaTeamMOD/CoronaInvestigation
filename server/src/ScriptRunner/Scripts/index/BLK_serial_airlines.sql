c114130f728c27cc1579fa33a21c74f5af61d262
Noam6421 <noam6421@gmail.com>
2021-05-23 15:05:47 +0300

    change airlineID in DB to be serial seq

---
+++ server/src/ScriptRunner/Scripts/index/BLK_serial_airlines.sql
@@ -0,0 +1,13 @@
+-- SEQUENCE: public.airlines_id_seq
+
+-- DROP SEQUENCE public.airlines_id_seq;
+
+CREATE SEQUENCE public.airlines_id_seq
+    INCREMENT 1
+    START 77
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER TABLE public.airlines
+    ALTER COLUMN id SET DEFAULT nextval('airlines_id_seq'::regclass);
