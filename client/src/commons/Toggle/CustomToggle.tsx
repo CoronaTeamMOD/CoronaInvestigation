ab0eb50eb3897eb868b7104700218ae469f467b0
Noam Korkos <55889135+Noam6421@users.noreply.github.com>
2021-06-06 10:34:20 +0300

    1824/feat/add-irrelevant-to-green-pass-questions (#1686)
    * .
    
    * changed toggle to custom togglr &&
    now only requested qouesion have irrelevant answer

---
+++ client/src/commons/Toggle/CustomToggle.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ToggleButtonGroup, ToggleButton, ToggleButtonGroupProps } from '@material-ui/lab';
+
+import theme from 'styles/theme';
+
+import { useStyles } from './ToggleStyles';
+
+const CustomToggle: React.FC<Props> = (props: Props): JSX.Element => {
+
+    const classes = useStyles({});
+
+    const { options, value, disabled, ...rest } = props;
+
+    const activeButtonStyle = {
+        backgroundColor: theme.palette.primary.main, 
+        color: 'white'
+    };
+
+    return (
+        <ToggleButtonGroup value={value} exclusive {...rest}>
+            {options.map((option) => { 
+                return(
+                <ToggleButton 
+                    className={classes.trippleToggle} 
+                    disabled={disabled}
+                    style={value === option?.id ? activeButtonStyle : {}}
+                    value={option?.id}>
+                    {option?.displayName}
+                </ToggleButton>
+                )
+            })}
+        </ToggleButtonGroup>
+    );
+};
+
+export default CustomToggle;
+
+interface Option {
+    displayName: string;
+    id: number;
+};
+
+interface Props extends ToggleButtonGroupProps {
+    options: Option[];
+    disabled?: boolean;
+    value?: number;
+};
--- client/src/commons/Toggle/TripleToggle.tsx
+++ client/src/commons/Toggle/TripleToggle.tsx
@@ -6,29 +6,34 @@ import theme from 'styles/theme';
 import { useStyles } from './ToggleStyles';
 
 const TrippleToggle: React.FC<Props> = (props: Props): JSX.Element => {
+
     const classes = useStyles({});
 
     const { firstOption, secondOption, thirdOption, value, disabled, ...rest } = props;
+
     const activeButtonStyle = {
         backgroundColor: theme.palette.primary.main, 
         color: 'white'
-    }
+    };
 
     return (
         <ToggleButtonGroup value={value} exclusive {...rest}>
-            <ToggleButton className={classes.trippleToggle} 
+            <ToggleButton 
+                className={classes.trippleToggle} 
                 disabled={disabled}
                 style={value === firstOption?.id ? activeButtonStyle : {}}
                 value={firstOption?.id}>
                 {firstOption?.displayName}
             </ToggleButton>
-            <ToggleButton className={classes.trippleToggle}
+            <ToggleButton 
+                className={classes.trippleToggle}
                 disabled={disabled}
                 style={value === secondOption?.id ? activeButtonStyle : {}}
                 value={secondOption?.id}>
                 {secondOption?.displayName}
             </ToggleButton>
-            <ToggleButton className={classes.trippleToggle} 
+            <ToggleButton 
+                className={classes.trippleToggle} 
                 disabled={disabled}
                 style={value === thirdOption?.id ? activeButtonStyle : {}}
                 value={thirdOption?.id}>
