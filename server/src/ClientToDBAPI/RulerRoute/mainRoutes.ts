c4f8daa3558f2d45184bf877bbd0b24441ff8c43
Chen Friedman <chen.friedman11@gmail.com>
2021-06-14 12:25:13 +0300

    pr fix

--- server/src/ClientToDBAPI/RulerRoute/mainRoutes.ts
+++ server/src/ClientToDBAPI/RulerRoute/mainRoutes.ts
@@ -1,5 +1,5 @@
-import { Router, Request, Response } from 'express';
 import axios from 'axios';
+import { Router, Request, Response } from 'express';
 
 import { Severity } from '../../Models/Logger/types';
 import UseCache, { setToCache } from '../../middlewares/UseCache';
@@ -7,6 +7,7 @@ import logger, { invalidAPIResponseLog, launchingAPIRequestLog, validAPIResponse
 
 const rulerRoute = Router();
 const rulerApiUrl = `http://192.168.2.26:8888/Corona/RulerCheckColor`;
+const errorStatusCode = 500;
 
 rulerRoute.post('/rulerapi/', UseCache, (req: Request, res: Response) => {
     const rulerLogger = logger.setup({
@@ -14,40 +15,40 @@ rulerRoute.post('/rulerapi/', UseCache, (req: Request, res: Response) => {
         user: res.locals.user.id,
     });
 
-    const params: any = 
+    const params: any =
     {
-        "RulerCheckColorRequest":{     
-        "MOHHeader":{       
-            "ActivationID":"1",       
-            "CustID":"23",       
-            "AppID":"130",       
-            "SiteID":"2",       
-            "InterfaceID":"Ruler"
-        },
-        "Ids":[{
-                "IdType":3,
-                "IDnum":"??2563621",
-                "DOB":"24011971",
-                "Tel":"0542987778"
-                },
-                {
-                "IdType":2,
-                "IDnum":".T0901828",
-                "DOB":"24011971",
-                "Tel":"0542987778"
-                },
-                {
-                "IdType":2,
-                "IDnum":"?0901788",
-                "DOB":"24011971",
-                "Tel":"0542987778"
-                }
+        "RulerCheckColorRequest": {
+            "MOHHeader": {
+                "ActivationID": "1",
+                "CustID": "23",
+                "AppID": "130",
+                "SiteID": "2",
+                "InterfaceID": "Ruler"
+            },
+            "Ids": [{
+                "IdType": 3,
+                "IDnum": "??2563621",
+                "DOB": "24011971",
+                "Tel": "0542987778"
+            },
+            {
+                "IdType": 2,
+                "IDnum": ".T0901828",
+                "DOB": "24011971",
+                "Tel": "0542987778"
+            },
+            {
+                "IdType": 2,
+                "IDnum": "?0901788",
+                "DOB": "24011971",
+                "Tel": "0542987778"
+            }
             ]
         }
     }
-    const parameters = <JSON> params
+    const parameters = <JSON>params
     rulerLogger.info(launchingAPIRequestLog(parameters), Severity.LOW);
-    
+
     axios.post(rulerApiUrl, parameters).then((data) => {
         rulerLogger.info(validAPIResponseLog, Severity.LOW);
         setToCache(req.originalUrl, data.data);
@@ -55,7 +56,7 @@ rulerRoute.post('/rulerapi/', UseCache, (req: Request, res: Response) => {
     }
     ).catch((err: any) => {
         rulerLogger.error(invalidAPIResponseLog(err), Severity.HIGH);
-        res.send(err);
+        res.status(errorStatusCode).send(err);
     })
 });
 
