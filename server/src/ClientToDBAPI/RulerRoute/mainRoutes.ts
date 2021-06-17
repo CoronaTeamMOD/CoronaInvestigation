75fd5aef6e96467d98d80c094cc8b17817270f8a
Chen Friedman <chen.friedman11@gmail.com>
2021-06-17 11:45:26 +0300

    test ruler fix9 (JSON.stringify(parameters))

--- server/src/ClientToDBAPI/RulerRoute/mainRoutes.ts
+++ server/src/ClientToDBAPI/RulerRoute/mainRoutes.ts
@@ -47,11 +47,11 @@ rulerRoute.post('/rulerapi/', (req: Request, res: Response) => {
     
     rulerLogger.info(launchingAPIRequestLog(parameters), Severity.LOW);
 
-    axios.post(rulerApiUrl, parameters)
-        .then((data) => {
+    axios.post(rulerApiUrl, JSON.stringify(parameters))
+        .then((result) => {
             rulerLogger.info(validAPIResponseLog, Severity.LOW);
-            // setToCache(req.originalUrl, data.data);
-            res.send(data);
+            // setToCache(req.originalUrl, result.result);
+            res.send(result);
         }
         ).catch((err: any) => {
             rulerLogger.error(invalidAPIResponseLog(err), Severity.HIGH);
