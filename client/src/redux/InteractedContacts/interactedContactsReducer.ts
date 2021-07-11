0936a96d7ceeec45c7af2b6abf36f6f98ddc5eed
orna.hasson <orna.hasson@VISDEVWEB08>
2021-07-11 11:35:05 +0300

    Redux changes

--- client/src/redux/InteractedContacts/interactedContactsReducer.ts
+++ client/src/redux/InteractedContacts/interactedContactsReducer.ts
@@ -1,15 +1,25 @@
-import GroupedInteractedContact from 'models/ContactQuestioning/GroupedInteractedContact';
+import GroupedInteractedContact from '../../models/ContactQuestioning/GroupedInteractedContact';
 import * as Actions from './interactedContactsActionTypes';
+
+export class FormStateObject {
+    isValid: boolean;
+
+    constructor(isValid: boolean) {
+        this.isValid = isValid;
+    }
+}
 
 export interface InteractedContactsState {
     pending: boolean;
     interactedContacts: GroupedInteractedContact[];
+    formState: Map<number, FormStateObject>;
     error: any;
 }
 
-const initialState :InteractedContactsState= {
+const initialState: InteractedContactsState = {
     pending: false,
     interactedContacts: [],
+    formState: new Map(),
     error: null
 };
 
@@ -24,9 +34,26 @@ const interactedContactsReducer = (state = initialState, action: Actions.Interac
             return {
                 ...state,
                 pending: false,
-                interactedContacts: action.payload.interactedContacts
+                interactedContacts: action.payload.interactedContacts,
+                formState: action.payload.formState
             }
         case Actions.GET_INTERACTED_CONTACTS_ERROR:
+            return {
+                ...state,
+                pending: false,
+                error: action.error
+            }
+        case Actions.SET_INTERACTED_CONTACT_PENDING:
+            return {
+                ...state,
+                pending: true
+            }
+        case Actions.SET_INTERACTED_CONTACT_SUCCESS:
+            return {
+                ...state,
+                pending: false
+            }
+        case Actions.SET_INTERACTED_CONTACT_ERROR:
             return {
                 ...state,
                 pending: false,
