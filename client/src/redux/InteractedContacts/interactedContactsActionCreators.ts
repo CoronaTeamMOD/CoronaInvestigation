3f684bc68a05148191bb19095ec26400b8e27e87
yosef.knafo <yosef.knafo@VISDEVWEB09>
2021-07-12 13:00:53 +0300

    כ

--- client/src/redux/InteractedContacts/interactedContactsActionCreators.ts
+++ client/src/redux/InteractedContacts/interactedContactsActionCreators.ts
@@ -39,15 +39,23 @@ export const getInteractedContacts = (minimalDate?: Date): ThunkAction<void, Int
     }
 }
 
-
 export const setInteractedContact = (contact: GroupedInteractedContact, formState: FormState<GroupedInteractedContact>):
     ThunkAction<void, StoreStateType, unknown, actionTypes.InteractedContactAction> => async (dispatch, getState) => {
-        dispatch({
-            type: actionTypes.SET_INTERACTED_CONTACT_FORM_STATE,
-            payload: { 
-                formState: getState().interactedContacts.formState.set(contact.id, new FormStateObject(formState.isValid))
-             }
-        });
+        // dispatch({
+        //     type: actionTypes.SET_INTERACTED_CONTACT_FORM_STATE,
+        //     payload: { 
+        //         formState: getState().formState.set(contact.id, new FormStateObject(formState.isValid))
+        //      }
+        // });
+        const dispatchFunc=async ()=>{
+            dispatch({
+                type: actionTypes.SET_INTERACTED_CONTACT_FORM_STATE,
+                payload: { 
+                    formState: getState().interactedContacts.formState.set(contact.id, new FormStateObject(formState.isValid))
+                 }
+            });
+        }
+       await dispatchFunc();
     }
 
 export const updateInteractedContacts = (contacts: InteractedContact[]):
