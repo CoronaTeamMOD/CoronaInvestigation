3d385996dd9b4ca55731422cd3e9378f2cd1ae8c
BARNA-LAP-HZ\barna <bar.nahum8@gmail.com>
2021-07-13 13:52:43 +0300

    fix loaded when switching tabs

--- client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactQuestioning.tsx
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ContactQuestioning/ContactQuestioning.tsx
@@ -94,6 +94,7 @@ const ContactQuestioning: React.FC<Props> = ({ id, isViewMode }: Props): JSX.Ele
         }
         
         if (interactedContacts) {
+            loaded = SIZE_OF_CONTACTS
             loopWithSlice(0, SIZE_OF_CONTACTS);
         }
         
