ff8b60f76c4cb3cbc1f911a7f594f5a991a5dfd0
ornahasson <85481012+ornahasson@users.noreply.github.com>
2021-08-23 13:58:25 +0300

    55786/feat​/add new investigation status (#1784)
    * DB script
    
    * Add new invesigation sub statuses
    
    * on conflict
    
    Co-authored-by: orna.hasson <orna.hasson@VISDEVWEB08>

---
+++ server/src/ScriptRunner/Scripts/index/add_not_investigated_to__investigation_statuses.sql
@@ -0,0 +1,24 @@
+INSERT INTO investigation_status (display_name, id)
+VALUES ('לא נחקר',100000005)
+ON CONFLICT (id) DO NOTHING
+
+INSERT INTO investigation_sub_status (display_name, id, parent_status)
+SELECT 'מדיניות משרד הבריאות',100000008,100000005
+WHERE
+    NOT EXISTS (
+        SELECT id FROM investigation_sub_status WHERE id = 100000008
+    );
+
+INSERT INTO investigation_sub_status (display_name, id, parent_status)
+SELECT 'מדיניות לשכה',100000009,100000005
+WHERE
+    NOT EXISTS (
+        SELECT id FROM investigation_sub_status WHERE id = 100000009
+    );
+
+INSERT INTO investigation_sub_status (display_name, id, parent_status)
+SELECT 'חוסר בכ"א',100000010,100000005
+WHERE
+    NOT EXISTS (
+        SELECT id FROM investigation_sub_status WHERE id = 100000010
+    );
