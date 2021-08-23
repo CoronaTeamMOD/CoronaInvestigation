ff5b4281dcacfa160fe67df443832e92802723bf
orna.hasson <orna.hasson@VISDEVWEB08>
2021-08-23 13:35:11 +0300

    on conflict

--- server/src/ScriptRunner/Scripts/index/add_not_investigated_to__investigation_statuses.sql
+++ server/src/ScriptRunner/Scripts/index/add_not_investigated_to__investigation_statuses.sql
@@ -1,9 +1,6 @@
 INSERT INTO investigation_status (display_name, id)
-SELECT 'לא נחקר',100000005
-WHERE
-    NOT EXISTS (
-        SELECT id FROM investigation_status WHERE id = '100000005'
-    );
+VALUES ('לא נחקר',100000005)
+ON CONFLICT (id) DO NOTHING
 
 INSERT INTO investigation_sub_status (display_name, id, parent_status)
 SELECT 'מדיניות משרד הבריאות',100000008,100000005
