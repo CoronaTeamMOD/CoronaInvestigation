c140350060a8b333ab003e06c9b4c92b41109ce6
Chen Friedman <chen.friedman11@gmail.com>
2021-08-24 10:41:42 +0300

    fixed

--- server/src/ScriptRunner/Scripts/index/add_new_streets.sql
+++ server/src/ScriptRunner/Scripts/index/add_new_streets.sql
@@ -1,5 +1,7 @@
 INSERT INTO public.streets (id, display_name, city, mho_code)
-VALUES ('100000', 'ירושלים', '681', '208');
+VALUES ('100000', 'ירושלים', '681', '208')
+ON CONFLICT DO NOTHING;
 
 INSERT INTO public.streets (id, display_name, city, mho_code)
-VALUES ('100001', 'קרן קיימת לישראל', '6500', '515');
+VALUES ('100001', 'קרן קיימת לישראל', '6500', '515')
+ON CONFLICT DO NOTHING;
