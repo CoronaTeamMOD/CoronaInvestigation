cfb4e4b3b74a5fa1dec99d79ef431eb5e4aaec2a
Chen Friedman <chen.friedman11@gmail.com>
2021-08-24 10:19:56 +0300

    55841/feat/add-new-streets

---
+++ server/src/ScriptRunner/Scripts/index/add_new_streets.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.streets (id, display_name, city, mho_code)
+VALUES ('100000', 'ירושלים', '681', '208');
+
+INSERT INTO public.streets (id, display_name, city, mho_code)
+VALUES ('100001', 'קרן קיימת לישראל', '6500', '515');
