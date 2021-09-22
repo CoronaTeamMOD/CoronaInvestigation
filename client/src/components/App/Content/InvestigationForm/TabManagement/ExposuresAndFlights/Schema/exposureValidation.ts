c143d5ee6cdb8be27f733b1447b8aa0d94e0321a
Chen Friedman <chen.friedman11@gmail.com>
2021-09-22 12:08:52 +0300

    part of another bug

--- client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Schema/exposureValidation.ts
+++ client/src/components/App/Content/InvestigationForm/TabManagement/ExposuresAndFlights/Schema/exposureValidation.ts
@@ -9,10 +9,9 @@ const twoWeeksBeforeValidationDateText = 'תאריך לא יכול להיות י
 
 const exposureValidation = (validationDate : Date) : yup.Schema<any, object>  => {
     return (yup.object().shape({
-        // [fieldsNames.exposureSource]: yup.object().nullable().required(requiredText),
         [fieldsNames.date]: yup.date().nullable().required(requiredText)
-                                                 .max(validationDate , endDateBeforeValidationDateText)
-                                                 .min(subDays(new Date(validationDate) , 14), twoWeeksBeforeValidationDateText),
+            .max(validationDate , endDateBeforeValidationDateText)
+            .min(subDays(new Date(validationDate) , 14), twoWeeksBeforeValidationDateText),
         [fieldsNames.address]: yup.object().nullable(),
         [fieldsNames.placeType]: yup.string().nullable().required(requiredText),
         [fieldsNames.placeSubType]: yup.number().nullable().required(requiredText),
