9ebe977337e628e9592d2fa0db852d9df942c3d9
chen friedman <31721666+chenFriedman@users.noreply.github.com>
2021-11-10 15:05:34 +0200

    15416/feat/add-street-and-sub-occupation2 (#1833)
    * 15416/feat/add-street-and-sub-occupation2
    
    * duplicate

--- server/src/ScriptRunner/Scripts/index/add_streets.sql
+++ server/src/ScriptRunner/Scripts/index/add_streets.sql
@@ -14,3 +14,15 @@ ON CONFLICT DO NOTHING;
 INSERT INTO public.streets (id, display_name, city, mho_code)
 VALUES ('100005', 'איסר הראל', '1020', '138')
 ON CONFLICT DO NOTHING;
+
+INSERT INTO public.streets (id, display_name, city, mho_code)
+VALUES ('100006', 'הר שלמה', '1020', '227')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO public.sub_occupation(id, display_name, parent_occupation, city)
+VALUES ('7701105692', 'שכ הפרחים', 'מערכת החינוך', 'חריש')
+ON CONFLICT DO NOTHING;;
+
+INSERT INTO public.sub_occupation(
+id, display_name, parent_occupation, city, street)
+VALUES ('651166', 'גן ניצוצות ב''', 'מערכת החינוך', 'חריש', 'רקפת 17');
