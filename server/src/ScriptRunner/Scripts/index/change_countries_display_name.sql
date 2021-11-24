5775e7f8f274b1d511c02b751dca2c650ac25814
chenfriedman11 <chen.friedman11@gmail.com>
2021-11-24 15:20:55 +0200

    15679/fix/change-countries-display-name

---
+++ server/src/ScriptRunner/Scripts/index/change_countries_display_name.sql
@@ -0,0 +1,11 @@
+UPDATE public.countries
+SET display_name =  'גרמניה המזרחית'
+WHERE id = '502';
+
+UPDATE public.countries
+SET display_name =  'רשות פלשתינאית'
+WHERE id = '953';
+
+UPDATE public.countries
+SET display_name =  'מאלי'
+WHERE id = '212';
