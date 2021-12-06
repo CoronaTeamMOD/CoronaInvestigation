24a80d45250d6624ab5c7611b0529bd83f801d1f
orna.hasson <orna.hasson@VISDEVWEB08>
2021-12-06 12:25:40 +0200

    update last updator user value

--- server/src/ScriptRunner/Scripts/index/add_updator_user_column_to_investigation.sql
+++ server/src/ScriptRunner/Scripts/index/add_updator_user_column_to_investigation.sql
@@ -1,2 +1,8 @@
 ALTER TABLE investigation
 ADD COLUMN IF NOT EXISTS last_updator_user character varying COLLATE pg_catalog."default";
+
+ALTER TABLE public.investigation DROP CONSTRAINT IF EXISTS last_updator_user_fk;
+
+ALTER TABLE public.investigation ADD CONSTRAINT last_updator_user_fk 
+FOREIGN KEY (last_updator_user)
+REFERENCES public."user" (id) MATCH SIMPLE;
