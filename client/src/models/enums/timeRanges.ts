acbf072de64e7ea977638b5f2cb8ea4da5e242d7
Bar Nahum <74368177+barnahum8@users.noreply.github.com>
2021-12-26 13:48:37 +0200

    fix (#1880)
    Co-authored-by: Bar Nahum <bar.nahum@moh.co.il>

--- client/src/models/enums/timeRanges.ts
+++ client/src/models/enums/timeRanges.ts
@@ -6,37 +6,37 @@ const timeRanges: TimeRange[] = [
     {
         id: 10,
         displayName: 'ת. הגעת חקירה',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format(new Date(),'yyyy-MM-dd')
     },
     {
         id: 11,
         displayName: 'הכל',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format(new Date(),'yyyy-MM-dd')
     },
     {
         id: 1,
         displayName: 'יום אחרון',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format(subDays(new Date(), 1),'yyyy-MM-dd')
     },
     {
         id: 3,
         displayName: '3 ימים  אחרונים',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format(subDays(new Date(), 3),'yyyy-MM-dd')
     },
     {
         id: 7,
         displayName: '7 ימים אחרונים',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format(subDays(new Date(), 7),'yyyy-MM-dd')
     },
     {
         id: -1,
         displayName: 'בחירת תאריכים',
-        endDate: format(new Date(),'yyyy-MM-dd'),
+        endDate: format(new Date(),'yyyy-MM-dd HH:mm:ss'),
         startDate: format((new Date()),'yyyy-MM-dd')
     }
 ];
