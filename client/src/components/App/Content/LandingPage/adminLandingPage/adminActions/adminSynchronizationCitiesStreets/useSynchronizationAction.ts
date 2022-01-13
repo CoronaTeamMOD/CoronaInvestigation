d400b3ed53fd6961f9575e443efcbf5338d77376
Bar Nahum <bar.nahum@moh.co.il>
2022-01-13 13:31:59 +0200

    fix mail and add index

--- client/src/components/App/Content/LandingPage/adminLandingPage/adminActions/adminSynchronizationCitiesStreets/useSynchronizationAction.ts
+++ client/src/components/App/Content/LandingPage/adminLandingPage/adminActions/adminSynchronizationCitiesStreets/useSynchronizationAction.ts
@@ -12,16 +12,14 @@ const useSynchronizationAction = (props: Props) => {
 
     const failedCityUpdateEmailParameters = { 
         from: 'moh.automatic.mail@gmail.com',
-        // to: 'chenfr@matrixdna.ai, Rut.shpitzer@moh.gov.il', //ran
-        to: 'chenfr@matrixdna.ai',
+        to: 'barna@matrixdna.ai, Rut.shpitzer@moh.gov.il', //ran
         subject: 'יצירת קובץ ערים נכשלה',
         text: 'יצירת קובץ ערים נכשלה'
     }
 
     const failedStreetUpdateEmailParameters = { 
         from: 'moh.automatic.mail@gmail.com',
-        // to: 'chenfr@matrixdna.ai, Rut.shpitzer@moh.gov.il', //ran
-        to: 'chenfr@matrixdna.ai',
+        to: 'barna@matrixdna.ai, Rut.shpitzer@moh.gov.il', //ran
         subject: 'יצירת קובץ רחובות נכשלה',
         text: 'יצירת קובץ רחובות נכשלה'
     }
---
+++ server/src/ScriptRunner/Scripts/index/add_sttreet_index.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX if not exists streets_idx on streets_temp (display_name,city,mho_code);
