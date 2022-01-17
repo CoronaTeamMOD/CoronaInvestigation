f60820a322b817d20074de06bf8ca51c84f5be8e
orna.hasson <orna.hasson@VISDEVWEB08>
2022-01-17 15:29:05 +0200

    select all

--- client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTable.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/InvestigationTable.tsx
@@ -4,7 +4,7 @@ import {
     Paper, Table, TableRow, TableBody, TableCell, Typography,
     TableHead, TableContainer, TableSortLabel, Button,
     useMediaQuery, Collapse, IconButton, Grid,
-    Slide, Popover, Tooltip
+    Slide, Popover, Tooltip, Checkbox
 } from '@material-ui/core';
 import React, { useMemo, useState, useRef, useEffect } from 'react';
 import { KeyboardArrowDown, KeyboardArrowLeft, Refresh } from '@material-ui/icons';
@@ -40,6 +40,7 @@ export const rowsPerPage = 100;
 const refreshPromptMessage = 'שים לב, ייתכן כי התווספו חקירות חדשות';
 const emptyGroupText = 'שים לב, בסבירות גבוהה לחקירה זו קובצו חקירות ישנות שכבר לא קיימות במערכת'
 const resetSortButtonText = 'סידור לפי תעדוף';
+const selectAllText = 'בחר הכל';
 
 const InvestigationTable: React.FC = (): JSX.Element => {
     const isScreenWide = useMediaQuery('(min-width: 1680px)');
@@ -97,7 +98,7 @@ const InvestigationTable: React.FC = (): JSX.Element => {
     });
 
     const user = useSelector<StoreStateType, User>(state => state.user.data);
-
+    const [selectAll, setSelectAll] = useState<boolean>(false);
     const { countyDesks, displayedCounty } = useDesksUtils();
 
     const desksToTransfer: Desk[] = useMemo(() =>
@@ -234,7 +235,23 @@ const InvestigationTable: React.FC = (): JSX.Element => {
                 fetchTableData();
             }
         };
-
+    
+    const onSelectAllClick = () => {
+        if (!selectAll) {
+            const selectedRows = tableRows.map((row: InvestigationTableRowType) =>
+                convertToIndexedRow(row)
+            );
+            setCheckedIndexedRows(selectedRows);
+        }
+        else {
+            setCheckedIndexedRows([]);
+        }
+        setSelectAll(!selectAll);
+    }
+    const onSelectedInvsDialogClose = () => {
+        setSelectAll(false);
+        setCheckedIndexedRows([]);
+    }
     useEffect(() => {
         window.addEventListener('keydown', handleEscKey);
         return () => {
@@ -322,15 +339,12 @@ const InvestigationTable: React.FC = (): JSX.Element => {
                                             {
                                                 key === TableHeadersNames.multipleCheck &&
                                                 <>
-                                                    <Tooltip title={resetSortButtonText} placement='top' arrow>
-                                                        <Button
-                                                            color='primary'
-                                                            className={classes.sortResetButton}
-                                                            startIcon={<Refresh />}
-                                                            onClick={(event: any) => handleRequestSort(event, defaultOrderBy)}
-                                                        >
-                                                        </Button>
-                                                    </Tooltip>
+                                                    {
+                                                       (user.userType !== UserTypeCodes.INVESTIGATOR) &&
+                                                        <Tooltip title={selectAllText} placement='top' arrow>
+                                                            <Checkbox onClick={onSelectAllClick} color='primary' checked={selectAll} size='small' />
+                                                        </Tooltip>
+                                                    }
                                                     <Tooltip title={(isGroupedExpanded ? 'הסתר' : 'הצג') + ' ' + 'את כל החקירות המקושרות'} placement='top' arrow>
                                                         <IconButton onClick={
                                                             isGroupedExpanded ? collapseAllGroupedInvestigations : expandAllGroupedInvestigations
@@ -340,6 +354,16 @@ const InvestigationTable: React.FC = (): JSX.Element => {
                                                                 <KeyboardArrowLeft />}
                                                         </IconButton>
                                                     </Tooltip>
+                                                    <Tooltip title={resetSortButtonText} placement='top' arrow>
+                                                        <Button
+                                                            color='primary'
+                                                            className={classes.sortResetButton}
+                                                            startIcon={<Refresh />}
+                                                            onClick={(event: any) => handleRequestSort(event, defaultOrderBy)}
+                                                        >
+                                                        </Button>
+                                                    </Tooltip>
+                                                   
                                                 </>
                                             }
                                         </TableCell>
@@ -467,7 +491,7 @@ const InvestigationTable: React.FC = (): JSX.Element => {
                 <InvestigationTableFooter
                     checkedIndexedRows={checkedIndexedRows}
                     allDesks={desksToTransfer}
-                    onDialogClose={() => setCheckedIndexedRows([])}
+                    onDialogClose={onSelectedInvsDialogClose}
                     tableRows={tableRows}
                     fetchTableData={fetchTableData}
                     onDeskGroupChange={changeGroupsDesk}
