7eecd066a0d72f53d1111a8283565d5f144353c2
ornahasson <85481012+ornahasson@users.noreply.github.com>
2022-01-23 13:01:05 +0200

    script (#1912)
    Co-authored-by: orna.hasson <orna.hasson@VISDEVWEB08>

--- server/src/ScriptRunner/Scripts/index/add_chat_statuses_human_investigation_request.sql
+++ server/src/ScriptRunner/Scripts/index/add_chat_statuses_human_investigation_request.sql
@@ -1,7 +1,11 @@
 INSERT INTO public.chat_status(id, display_name)
-	VALUES (14,'בקשה דחופה לחקירה אנושית')
+	VALUES (14,'חקירה אנושית -קושי רפואי')
 	ON CONFLICT DO NOTHING;
 
 INSERT INTO public.chat_status(id, display_name)
-	VALUES (15,'עדיפות לחקירה אנושית')
+	VALUES (15,'חקירה אנושית')
 	ON CONFLICT DO NOTHING;
+
+UPDATE  public.chat_status
+SET display_name='חקירה אנושית -קושי בבידוד'
+WHERE id = 10;
