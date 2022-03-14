4d2daac3592590cec0511f724cf534f2608dde2a
orna.hasson <orna.hasson@VISDEVWEB08>
2022-03-14 09:10:36 +0200

    bug fixing

--- client/src/components/App/Content/LandingPage/InvestigationTable/Pagination/Pagination.tsx
+++ client/src/components/App/Content/LandingPage/InvestigationTable/Pagination/Pagination.tsx
@@ -7,6 +7,22 @@ const Pagination = (props: Props) => {
     const { page, count, onPageChange } = props;
     const classes = useStyles();
     const [currentPage, setCurrentPage] = useState<string | null>(page.toString());
+    
+    const changePage = (page: number ) => {
+        if (page > count ){
+            setCurrentPage(count.toString());
+            onPageChange(count);                             
+        }
+        else if (page < 1) {
+            setCurrentPage((1).toString());
+            onPageChange(1);
+        } 
+        else {
+            setCurrentPage(page.toString());
+            onPageChange(page);
+        }
+    }  
+
     return (
         <Grid container xs={12} spacing={1} direction='row' className={classes.paginationRow} >
             <Grid item xs='auto'>
@@ -16,14 +32,7 @@ const Pagination = (props: Props) => {
                         disabled={page == 1}
                         onMouseDown={() => {
                             let currentPageNumber = Number(currentPage)
-                            if (currentPageNumber > 1 && currentPageNumber <= count + 1) {
-                                setCurrentPage((currentPageNumber - 1).toString());
-                                onPageChange(currentPageNumber - 1);
-                            }
-                            else {
-                                setCurrentPage(currentPageNumber.toString());
-                                onPageChange(currentPageNumber);
-                            }
+                            changePage(currentPageNumber-1);
                         }}
                     ><NavigateNext className={classes.arrowIcon} /></button>
                 </Box>
@@ -40,12 +49,11 @@ const Pagination = (props: Props) => {
                         setCurrentPage(event.target.value);
                     }}
                     onKeyPress={event => {
-                        event.key === 'Enter' &&
-                            onPageChange(Number(currentPage))
+                         (event.key === 'Enter') && changePage(Number(currentPage))
                     }}
                     onBlur={() => {
                         if (currentPage != '')
-                            onPageChange(Number(currentPage))
+                            changePage(Number(currentPage))
                     }}
                     className={classes.pageInput}
                 />
@@ -55,15 +63,8 @@ const Pagination = (props: Props) => {
                     <button disabled={page == count}
                         className={classes.arrowButton}
                         onMouseDown={() => {
-                            let currentPageNumber = Number(currentPage)
-                            if (currentPageNumber >= 0 && currentPageNumber < count) {
-                                setCurrentPage((currentPageNumber + 1).toString());
-                                onPageChange(currentPageNumber + 1);
-                            }
-                            else {
-                                setCurrentPage(currentPageNumber.toString());
-                                onPageChange(currentPageNumber);
-                            }
+                            let currentPageNumber = Number(currentPage);
+                            changePage(currentPageNumber+1);
                         }}><NavigateBefore className={classes.arrowIcon} /></button>
                 </Box>
             </Grid>
