eb4bcb2de032b09313776f829dd5ad9c8937470f
Bar Nahum <bar.nahum@moh.co.il>
2022-04-11 11:40:22 +0300

    Revert "19089/feat/set complex color (#1956)"
    This reverts commit 8ee7c31a61740777f5faeda009e6fd76c4432134.

--- client/src/commons/InvestigationComplexity/ComplexityIcon/ComplexityIcon.tsx
+++ client/src/commons/InvestigationComplexity/ComplexityIcon/ComplexityIcon.tsx
@@ -3,13 +3,13 @@ import { Star } from '@material-ui/icons';
 import { SvgIconProps, Tooltip } from '@material-ui/core';
 
 import useStyles from './ComplexityIconStyles';
-const abroadText = 'חזר מחו"ל';
+const onlyAbroadText = 'חקירה מורכבת: חזר מחו"ל';
 
 const ComplexityIcon: React.FC<Props> = (props: Props) => {
     
     const { className, tooltipText, isVarient, ...otherProps } = props
     let colors = isVarient ? {fill: 'red'} : {};
-    colors = tooltipText.indexOf(abroadText) != -1 ? {fill: 'green'} : colors;
+    colors = tooltipText == onlyAbroadText ? {fill: 'green'} : colors;
     const classes = useStyles();
 
     return (
