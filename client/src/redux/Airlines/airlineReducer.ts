69770d3ebc05bee8146c079f7ea41efd9a59a7a0
Bar Nahum <bar.nahum@moh.co.il>
2022-04-17 11:18:59 +0300

    Revert "fix (#1992)"
    This reverts commit 68c9fe5ff61810f995acbec1477864f270d055ff.

--- client/src/redux/Airlines/airlineReducer.ts
+++ client/src/redux/Airlines/airlineReducer.ts
@@ -1,8 +1,8 @@
 import * as Actions from './airlineActionTypes';
 
-const initialState: Map<string, string> = new Map();
+const initialState: Map<String, string> = new Map();
 
-const airlineReducer = (state = initialState, action: Actions.airlinesAction) : Map<string, string> => {
+const airlineReducer = (state = initialState, action: Actions.airlinesAction) : Map<String, string> => {
     switch (action.type) {
         case Actions.SET_AIRLINES: {
             return action.payload.airlines
