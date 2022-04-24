4a5b0c583c25b28b92e2e9ca13085294a8e83a14
Bar Nahum <74368177+barnahum8@users.noreply.github.com>
2022-04-24 14:59:13 +0300

    remove (#1997)
    Co-authored-by: Bar Nahum <bar.nahum@moh.co.il>

--- server/src/ScriptRunner/src/Logger/index.ts
+++ server/src/ScriptRunner/src/Logger/index.ts
@@ -1,22 +1,22 @@
 import levels from './levels';
-import IP from '../LogUtils/ipAddress';
+//import IP from '../LogUtils/ipAddress';
 import { writeToStream } from './writeStream';
 import getLogDateTime from '../LogUtils/getLogDateTime';
 
 const success = (message : string) => {
-    writeToStream([getLogDateTime(), IP, levels.SUCCESS, message])
+    writeToStream([getLogDateTime(), levels.SUCCESS, message])
 }
 
 const info = (message : string) => {
-    writeToStream([getLogDateTime(), IP, levels.INFO, message])
+    writeToStream([getLogDateTime(), levels.INFO, message])
 }
 
 const warn = (message : string) => {
-    writeToStream([getLogDateTime(), IP, levels.WARN, message])
+    writeToStream([getLogDateTime(), levels.WARN, message])
 }
 
 const error = (message : string) => {
-    writeToStream([getLogDateTime(), IP, levels.ERROR, message])
+    writeToStream([getLogDateTime(), levels.ERROR, message])
 }
 
 
