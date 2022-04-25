b1604768ff84d65f0ad2bd45fa411e5539614c6a
ornahasson <85481012+ornahasson@users.noreply.github.com>
2022-04-25 14:42:47 +0300

    db script (#1972)
    * db script
    
    * add insert /  update trigger
    
    Co-authored-by: orna.hasson <orna.hasson@VISDEVWEB08>

---
+++ server/src/ScriptRunner/Scripts/index/history_investigation_tables.sql
@@ -0,0 +1,104 @@
+-- Creating table action_type
+
+CREATE TABLE IF NOT EXISTS public.action_type
+(
+    id integer NOT NULL,
+    display_name character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT action_type_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+-- Inserting data into table action_type
+
+INSERT INTO public.action_type(id, display_name)
+	VALUES (1,'יצירה')
+	ON CONFLICT DO NOTHING;
+
+INSERT INTO public.action_type(id, display_name)
+	VALUES (2,'עדכון')
+	ON CONFLICT DO NOTHING;
+
+-- Creating table investigation_history
+
+
+CREATE SEQUENCE IF NOT EXISTS public.investigation_history_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+
+CREATE TABLE IF NOT EXISTS public.investigation_history
+(
+    investigation_history_id  integer NOT NULL DEFAULT nextval('investigation_history_seq'::regclass),
+    epidemiology_number integer not null,
+    change_date timestamp with time zone NOT NULL,
+    changed_by character varying COLLATE pg_catalog."default" NOT NULL,
+    action_type_id integer,
+    changed_field character varying COLLATE pg_catalog."default",
+    old_value character varying COLLATE pg_catalog."default",
+    new_value character varying COLLATE pg_catalog."default",
+
+    CONSTRAINT investigation_history_pkey PRIMARY KEY (investigation_history_id),
+    CONSTRAINT epidemiology_number_fkey FOREIGN KEY (epidemiology_number)
+        REFERENCES public.investigation (epidemiology_number) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT changed_by_user_fkey FOREIGN KEY (changed_by)
+        REFERENCES public.user (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT action_type_id_fkey FOREIGN KEY (action_type_id)
+        REFERENCES public.action_type (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+
+--- investigation table trigger
+
+CREATE OR REPLACE FUNCTION update_history_investigation() RETURNS TRIGGER AS $$
+    BEGIN
+  		IF (TG_OP = 'INSERT') THEN          
+			 INSERT INTO public.investigation_history(change_date, changed_by, action_type_id, epidemiology_number, changed_field, old_value, new_value)
+			 VALUES (now(), NEW.last_updator_user , 1, NEW.epidemiology_number, null, null, null);
+			 RETURN NEW;
+        ELSEIF (TG_OP = 'UPDATE') THEN
+			   IF (NEW.investigation_status <> OLD.investigation_status) THEN
+				 INSERT INTO public.investigation_history(change_date, changed_by, action_type_id,epidemiology_number, changed_field, old_value, new_value)
+				 VALUES (now(), NEW.last_updator_user ,2, NEW.epidemiology_number, 'investigation_status',  OLD.investigation_status,  NEW.investigation_status);
+			  END IF;
+			  IF (NEW.creator <> OLD.creator) THEN
+				 INSERT INTO public.investigation_history(change_date, changed_by, action_type_id,epidemiology_number, changed_field, old_value, new_value)
+				 VALUES (now(), NEW.last_updator_user ,2, NEW.epidemiology_number, 'creator',  OLD.creator,  NEW.creator);
+			  END IF;
+			  IF (NEW.last_updator_user <> OLD.last_updator_user OR NEW.last_update_time::Date <> OLD.last_update_time::Date) THEN
+				 INSERT INTO public.investigation_history(change_date, changed_by, action_type_id,epidemiology_number, changed_field, old_value, new_value)
+				 VALUES (now(), NEW.last_updator_user, 2, NEW.epidemiology_number, null, null, null);
+			  END IF;
+			 RETURN NEW;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+
+
+DO $$
+BEGIN
+  IF NOT EXISTS(SELECT *
+    FROM information_schema.triggers
+    WHERE event_object_table = 'investigation'
+    AND trigger_name = 'investigation_change_trigger'
+  )
+  THEN
+    CREATE TRIGGER  investigation_change_trigger
+	AFTER INSERT OR UPDATE ON public.investigation
+    FOR EACH ROW EXECUTE FUNCTION update_history_investigation();
+  END IF;
+END;
+$$
+
