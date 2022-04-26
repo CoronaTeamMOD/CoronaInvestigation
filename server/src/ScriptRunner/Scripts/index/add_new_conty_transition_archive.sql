3806358e004fb825d4331e3efe44f54383a06379
chenfriedman11 <chen.friedman11@gmail.com>
2022-04-26 09:13:26 +0300

    pr fix

--- server/src/ScriptRunner/Scripts/index/add_new_conty_transition_archive.sql
+++ server/src/ScriptRunner/Scripts/index/add_new_conty_transition_archive.sql
@@ -1,10 +1,12 @@
 INSERT INTO public.counties(
 display_name, id, user_admin_id, district_id, self_investigator_id, is_displayed)
-VALUES ('ארכיון מעברים', '9992', 'admin.group9999', '9999', 'self_investigator.group9999',true);
+VALUES ('ארכיון מעברים', '9992', 'admin.group9999', '9999', 'self_investigator.group9999',true)
+ON CONFLICT DO NOTHING;
 
 INSERT INTO public."user"(
 user_name, id, investigation_group, user_type, is_developer)
-VALUES ('ארכיון מעברים', 'admin.group9992', '9992', 1, false);
+VALUES ('ארכיון מעברים', 'admin.group9992', '9992', 1, false)
+ON CONFLICT DO NOTHING;
 
 UPDATE public.counties
 SET user_admin_id='admin.group9992'
