790439a22a5c34cf3c06317aac9a93d3affb10af
chenfriedman11 <chen.friedman11@gmail.com>
2022-06-07 14:09:57 +0300

    try to find why refreshing the table beacme slower

--- server/src/DBService/InvestigationInfo/Query.ts
+++ server/src/DBService/InvestigationInfo/Query.ts
@@ -186,7 +186,7 @@ query getinvestigationReasonId ($epidemiologyNumber: Int!) {
 `;
 
 export const GET_BIRTHDATE = gql`
-query getinvestigationReasonId ($epidemiologyNumber: Int!) {
+query getBirthdate ($epidemiologyNumber: Int!) {
   covidPatientByEpidemiologyNumber(epidemiologyNumber: $epidemiologyNumber) {
     birthDate
   }
@@ -194,9 +194,23 @@ query getinvestigationReasonId ($epidemiologyNumber: Int!) {
 `;
 
 export const GET_VACCINE_DOSE_ID = gql`
-query getinvestigationReasonId ($epidemiologyNumber: Int!) {
+query getVaccineDoseId ($epidemiologyNumber: Int!) {
   investigationByEpidemiologyNumber(epidemiologyNumber: $epidemiologyNumber) {
     vaccineDoseId
+  }
+}
+`;
+
+export const GET_COMPLEXITY_REASONS_BIRTHDATE_AND_VACCINE_DOSE_ID = gql`
+query getinvestigationData ($epidemiologyNumber: Int!) {
+  investigationByEpidemiologyNumber(epidemiologyNumber: $epidemiologyNumber) {
+    complexityReasonsId
+    vaccineDoseId
+    investigatedPatientByInvestigatedPatientId {
+      covidPatientByCovidPatient {
+        birthDate
+      }
+    }
   }
 }
 `;
