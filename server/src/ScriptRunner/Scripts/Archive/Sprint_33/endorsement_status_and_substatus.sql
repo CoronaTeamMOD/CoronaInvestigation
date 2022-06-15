afe7a5043517571935fe5f686f7e8996bf2c2dcd
chenfriedman11 <chen.friedman11@gmail.com>
2022-06-15 11:46:48 +0300

    20562/task/endorsement_status_and_substatus

---
+++ server/src/ScriptRunner/Scripts/Archive/Sprint_33/endorsement_status_and_substatus.sql
@@ -0,0 +1,16 @@
+UPDATE investigation
+SET investigation_status = 100000008
+WHERE investigation_status = 100000000 AND investigation_sub_status = 'נפטר';
+
+UPDATE investigation
+	SET investigation_sub_status = 
+		CASE
+		 WHEN investigation_status = 100000008 AND investigation_sub_status = 'מדיניות לשכה'
+		 THEN 'מדיניות משרד הבריאות'
+		 WHEN investigation_status = 100000000 AND investigation_sub_status = 'חסר טלפון'
+		 THEN 'חסרים פרטי מטופל'
+		 WHEN investigation_status = 100000000 AND investigation_sub_status = 'טלפון לא זמין'
+		 THEN 'לא ניתן לאיתור'
+		 WHEN investigation_status = 100000000 AND investigation_sub_status = 'מטופל מבקש לצלצל אליו בזמן אחר'
+		 THEN 'חוסר שיתוף פעולה'
+		END
